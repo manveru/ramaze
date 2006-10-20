815f03c3b82807f1dc3aa56bbe569573a0140b87
manveru <m.fellinger@gmail.com>
2006-10-20 01:50:00 +0000

    added documentation and Gestalt.build, which takes a block instead of Gestalt.new
    darcs-hash:20061020015034-24576-94c04d6bc74188e46080fdaac9ce8ebb5a5a9c44.gz

--- lib/ramaze/gestalt.rb
+++ lib/ramaze/gestalt.rb
@@ -3,17 +3,51 @@ class BlankSlate
   instance_methods.each{|m| undef_method m unless m =~ /^__/}
 end
 
+=begin rdoc
+Example:
+
+  require 'ramaze'
+  require 'ramaze/gestalt'
+
+  def random_color
+    r = lambda{|n| rand(n).to_s(16) }
+    '#' + [255, 255, 255].map{r[255] + r[255] + r[255]
+  end
+
+  puts Ramaze::Gestalt.build{
+    html do
+      head do
+        title{"Hello World"}
+      end
+      body do
+        h1{"Hello, World!"}
+        div(:style => 'width:100%') do
+          10.times do
+            div(:style => "width:#{rand(100)}%;height:#{rand(100)}%;background:#{random_color}"){ '&nbsp;' }
+          end
+        end
+      end
+    end
+  }
+=end
+
 module Ramaze
   class Gestalt < BlankSlate
+    def self.build &block
+      self.new(&block).to_s
+    end
+
     def initialize &block
       @out = ''
       __instance_eval__(&block) if block_given?
     end
 
+    # proxy everything to _gestalt_handle
     def method_missing meth, *args, &block
       _gestalt_handle meth, *args, &block
     end
 
+    # A little hack to circumvent Kernel::p
     def p *args, &block
       _gestalt_handle :p, *args, &block
     end
