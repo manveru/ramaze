530547f6c11d9dc78afff332a797deb774c8b942
manveru <m.fellinger@gmail.com>
2006-10-26 10:27:00 +0000

    autoreload is now a method in Ramaze and i added a Global.autoreload hash that holds the autoreload-times for the hashes, additionally there is the new :benchmark mode
    darcs-hash:20061026102716-24576-63b3cde5609155ca72b923ef44c684d1dc795681.gz

--- lib/ramaze/snippets/kernel/autoreload.rb
+++ lib/ramaze/snippets/kernel/autoreload.rb
@@ -7,22 +7,22 @@ module Ramaze
   # You can also safely kill all threads (except for the main)
   # and it will just restart the autoreloader.
 
-  def autoreload interval = 10
+  def self.autoreload interval = 10
     gatherer = Thread.new do
       this = Thread.current
       cache = {}
-      this[:interval] = interval
-      joiner = lambda{|path, file| File.expand_path(File.join(path, file))}
+
       loop do
-        (this[:files] ||= []).dup.each do |file|
-          this[:files].delete(file) unless File.exist?(file)
-        end
-        $".each do |file|
+        files = []
+        $LOADED_FEATURES.map do |file|
           paths = $LOAD_PATH + ['']
-          correct_path = paths.find{|lp| File.exist?(joiner[lp, file])}
-          correct_file = joiner[correct_path, file]
-          this[:files] << correct_file unless this[:files].include?(correct_file)
+          correct_path = paths.find{|lp| File.exist?(File.join(lp, file))}
+          correct_file = File.expand_path(File.join(correct_path, file))
+
+          files << correct_file
         end
+
+        this[:files] = files
         sleep(this[:interval] ||= interval)
       end
     end
@@ -32,36 +32,32 @@ module Ramaze
       cache = {}
       this[:interval] = interval
       sleep 0.1 until gatherer[:files] # wait for the gatherer
+
       loop do
-        begin
-          gatherer[:files].each do |file|
+        gatherer[:files].each do |file|
+          begin
             current_time = File.mtime(file)
             if last_time = cache[file]
               unless last_time == current_time
-                begin
-                  print "reloading #{file} ... "
-                  load(file)
-                  puts "successfully."
-                  cache[file] = current_time
-                rescue Object => ex # catches errors when the load fails
-                  # in case mtime fails
-                  puts "failed."
-                end
+                Ramaze::Logger.info "autoreload #{file}"
+                load(file)
+                cache[file] = current_time
               end
             else
               cache[file] = current_time
             end
-            # sleep a total of reloader[:interval], 
+          rescue Object => ex # catches errors when the load fails
+            # in case mtime fails
+            unless ex.message =~ /No such file or directory/
+              puts ex
+              puts ex.backtrace
+            end
+            # sleep a total of reloader[:interval],
             # but spread it evenly on all files
-            sleep(this[:interval].to_f / gatherer[:files].size)
-          end
-        rescue Object => ex
-          unless ex.message =~ /no such file or directory/
-            puts ex
-            puts ex.backtrace
-          end
-        end
-      end
-    end
-  end
-end
+          end # begin
+          sleep(this[:interval].to_f / gatherer[:files].size)
+        end # each
+      end # loop
+    end # Thread.new
+  end # autoreload
+end # Ramaze
