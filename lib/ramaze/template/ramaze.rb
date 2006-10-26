7a3eb75c9df7504ac1740a46199090f883577f51
manveru <m.fellinger@gmail.com>
2006-10-26 10:28:00 +0000

    rewrote the ramaze-template
    darcs-hash:20061026102812-24576-22d819e851717aed3698279761dd1d8efc66996b.gz

--- lib/ramaze/template/ramaze.rb
+++ lib/ramaze/template/ramaze.rb
@@ -3,6 +3,34 @@ module Ramaze::Template
     class << self
       def handle_request request, action, *params
         controller = self.new
+        controller.__send__(action, *params)
+      end
+    end
+
+    private
+
+    def render template
+      file = File.join(Global.template_root, Global.mapping.invert[self.class], "#{template}.rmze")
+
+      if File.exist?(file)
+        transform(file, ivs)
+      else
+        ''
+      end
+    end
+
+    def redirect target
+      response.head['Location'] = target
+      response.code = 303
+      %{Please follow <a href="#{target}">#{target}</a>!}
+    end
+  end
+end
+
+=begin
+
+      def handle_request request, action, *params
+        controller = self.new
         controller.instance_eval do
           out controller.__send__(action, *params).to_s
           template_file = "templates/#{action}.html"
@@ -14,12 +42,28 @@ module Ramaze::Template
 
         return controller.out
       end
+
+      def transform template = '', ivs = ivs
+        self.new do
+          ivs.each{ |k, v| __instance_variable_set__(k, v) }
+          __instance_eval__(template)
+        end
+      end
     end
 
-    attr_accessor :out
+    private
+
+    def render template
+      template_transform(File.read("template/#{template}.rmze"), ivs)
+    end
+
+    def redirect target
+      Gestalt.new{html{body{a(:href => "/#{target}"){target.to_s}}}}
+    end
 
     def out str = ''
       (@out ||= '') << str
     end
   end
 end
+=end
