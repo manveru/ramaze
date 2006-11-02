2be41ca84b39f28533a3e3e8dd71873511858aef
manveru <m.fellinger@gmail.com>
2006-11-02 15:10:00 +0000

    added trinity, to be extracted
    darcs-hash:20061102151013-24576-ee5d9388a75d258ec1286f2e399921527894bdc2.gz

--- lib/ramaze/dispatcher.rb
+++ lib/ramaze/dispatcher.rb
@@ -1,6 +1,20 @@
 require 'timeout'
 
 module Ramaze
+  module Trinity
+    def response
+      Thread.current[:response]
+    end
+
+    def request
+      Thread.current[:request]
+    end
+
+    def session
+      Thread.current[:session]
+    end
+  end
+
   module Dispatcher
     class << self
       def handle orig_request, orig_response
