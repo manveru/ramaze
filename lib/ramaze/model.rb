8ad2aa6ccd0a0a9ba5129b104f2c029fb4334527
manveru <m.fellinger@gmail.com>
2006-11-02 15:12:00 +0000

    added a simple YAMLstore... to be replaced by the real thing
    darcs-hash:20061102151244-24576-dd45d3f9eb0ffb00ead035cfeca5eb1fdd819566.gz

---
+++ lib/ramaze/model.rb
@@ -0,0 +1,26 @@
+module Model
+  class YAMLDatabase
+    def initialize file = 'db.yaml'
+      @file = file
+      load
+    end
+
+    def load file = @file
+      @db = YAML.load_file(file)
+      p [:loaded, @db]
+    end
+
+    def save file = @file
+      File.open(file, 'w+') do |f|
+        f.print(YAML.dump(@db))
+      end
+      p [:saved, @db]
+    end
+
+    def method_missing(meth, *params, &block)
+      p [:method_missing, meth, params]
+      @db.send(meth, *params, &block)
+    end
+  end
+end
+
