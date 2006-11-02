10bf8b12b83a9b1dd30f808d7d3022114c508f47
manveru <m.fellinger@gmail.com>
2006-11-02 15:13:00 +0000

    default testmode is now :live instead of :silent
    darcs-hash:20061102151316-24576-e619addc8ed3470edb7221673b7f33fecfd13f9a.gz

--- lib/test/test_helper.rb
+++ lib/test/test_helper.rb
@@ -4,6 +4,6 @@ require 'spec'
 $:.unshift File.join(File.dirname(File.expand_path(__FILE__)), '..', 'lib')
 require 'ramaze'
 
-Ramaze::Global.mode = :silent
+Ramaze::Global.mode = :live
 Ramaze::Global.run_loose = true
 Ramaze::Global.error_page = false
