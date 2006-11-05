971f2d78d80e16c177e3a4469d9dc7a8027dc00e
manveru <m.fellinger@gmail.com>
2006-11-05 08:42:00 +0000

    fixing the amrita2 controller to find the correct path for templates (ann[:template_root] to override it)
    darcs-hash:20061105084238-24576-4cfe60620ab0812cf40c85aa98bc2dfac44487d8.gz

--- lib/ramaze/template/amrita2.rb
+++ lib/ramaze/template/amrita2.rb
@@ -10,16 +10,17 @@ module Ramaze::Template
       include Trinity
 
       def handle_request request, action, *params
-        template_file = File.expand_path((File.dirname($0) / 'template' / Global.mapping.invert[self] / action) << '.html')
-        template      = ::Amrita2::TemplateFile.new(template_file)
-        out           = ''
+        template_file = 
+        if template_root = ann[:template_root]
+          File.expand_path(File.join(template_root, action) << '.html')
+        else
+          File.expand_path((File.dirname($0) / 'template' / Global.mapping.invert[self] / action) << '.html')
+        end
 
+        template     = ::Amrita2::TemplateFile.new(template_file)
+        out = ''
         template.expand(out, self.new)
-        out 
-        p out
-        p response
-        response.out = out
-        p response
+        out
       rescue Object => ex
         puts ex
         Logger.error ex
