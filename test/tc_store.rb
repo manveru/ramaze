33621b2a681e202c94d8f183d729458b13f77abc
manveru <m.fellinger@gmail.com>
2006-11-28 12:33:00 +0000

    saner tc_store
    darcs-hash:20061128123321-24576-d6d799dca04ccfd29db1398542cfa659a906e6e0.gz

--- test/tc_store.rb
+++ test/tc_store.rb
@@ -5,13 +5,19 @@ require 'ramaze/store/default'
 include Ramaze
 
 context "initialize an Store" do
+  db = 'db.yaml'
+
   specify "Store::Default.new" do
-    Db = Store::Default.new
+    Db = Store::Default.new(db)
     Db.db.should.is_a?(YAML::Store)
   end
 
   specify "store and retrieve something" do
     (Db[:foo] = :bar).should == :bar
     Db[:foo].should == :bar
+  end
+
+  teardown do
+    FileUtils.rm db
   end
 end
