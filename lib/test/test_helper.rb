0e978f494a3293f628f9588f853ac29d490ad3c7
manveru <m.fellinger@gmail.com>
2006-11-28 11:54:00 +0000

    update to test_helper, now using get/post instead of test_get/test_post or request
    darcs-hash:20061128115456-24576-34d3bd1974a786b508f26fe1976c463eb4568da8.gz

--- lib/test/test_helper.rb
+++ lib/test/test_helper.rb
@@ -4,6 +4,23 @@ require 'spec'
 $:.unshift File.join(File.dirname(File.expand_path(__FILE__)), '..', 'lib')
 require 'ramaze'
 
-Ramaze::Global.mode = :live
+def get url = ''
+  url = "http://localhost:#{Ramaze::Global.port}" + "/#{url}".gsub(/^\/+/, '/')
+  result = open(url).read.strip
+  #p url => result
+  result
+end
+
+def post url = '', params = {}
+  url = "http://localhost:#{Ramaze::Global.port}" + "/#{url}".gsub(/^\/+/, '/')
+  uri = URI.parse(url)
+  res = Net::HTTP.post_form(uri, params)
+  result = res.body.to_s.strip
+  #p res => result
+  result
+end
+
+Ramaze::Global.mode = :debug
+Ramaze::Global.adpater = :mongrel
 Ramaze::Global.run_loose = true
 Ramaze::Global.error_page = false
