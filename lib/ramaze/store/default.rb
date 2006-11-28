82d886af27d3fcbb5593d4519c8ae29c51686696
manveru <m.fellinger@gmail.com>
2006-11-28 11:57:00 +0000

    added the default-store, this is just a very simple wrapper over YAML::Store and doesn't offer anything really... just to satisfy the tests
    darcs-hash:20061128115713-24576-8c079cf1798ae3af461602e5bde1b1010c8285db.gz

---
+++ lib/ramaze/store/default.rb
@@ -0,0 +1,31 @@
+require 'yaml/store'
+
+module Ramaze
+  module Store
+    class Default
+      attr_accessor :db
+
+      def initialize filename = 'db.yaml'
+        @db = YAML::Store.new(filename)
+      end
+
+      def method_missing(meth, *args, &block)
+        @db.transaction do
+          @db.send(meth, *args, &block)
+        end
+      end
+      
+      def [](key)
+        @db.transaction do
+          @db[key]
+        end
+      end
+
+      def []=(key, value)
+        @db.transaction do
+          @db[key] = value
+        end
+      end
+    end
+  end
+end
