619e2fc93a1acfa5cd3daf234ec54629daaa4e14
manveru <m.fellinger@gmail.com>
2006-11-30 06:52:00 +0000

    this is a fix in case some testcase just hangs, at least you will know which one does
    darcs-hash:20061130065233-24576-6651d828dcfa5e254dc6b8a6da55a2c4921dc7e8.gz

--- lib/test/all_tests.rb
+++ lib/test/all_tests.rb
@@ -7,11 +7,12 @@ failures = 0
 problematic = {}
 
 Dir['test/**/tc_*.rb'].each do |test_case|
+  print "rspec #{test_case.ljust(50)} "
   out = `ruby #{test_case}`
   out.split("\n").each do |line|
     if line =~ /(\d+) specifications?, (\d+) failures?/ 
       s, f = $1.to_i, $2.to_i
-      puts "rspec #{test_case.ljust(50)} [#{s.to_s.rjust(3)} specs]"
+      puts "[#{s.to_s.rjust(3)} specs]"
       problematic[test_case] = out unless f == 0
       specs    += s
       failures += f
