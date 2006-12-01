df5919f1b5b26cd3d242cb9aa720f27420ee0586
manveru <m.fellinger@gmail.com>
2006-12-01 12:43:00 +0000

    added a pretty good coverage of the helpers in test/tc_helper.rb
    darcs-hash:20061201124302-24576-2c68dbf9495fe6db2db51efe8af7150257a31899.gz

---
+++ test/tc_helper.rb
@@ -0,0 +1,81 @@
+require 'ramaze'
+require 'test/test_helper'
+include Ramaze
+
+class MainController < Template::Ramaze
+  def index
+    self.class.name
+  end
+
+  def redirection
+    redirect :target
+  end
+
+  def double_redirection
+    redirect :redirection
+  end
+
+  def link_redirection
+    redirect link_raw(self.class)
+  end
+
+  def target
+    "#{self.class.name} target"
+  end
+end
+
+class MinorController < Template::Ramaze
+  def index
+    self.class.name
+  end
+
+  def redirection
+    redirect :target
+  end
+
+  def double_redirection
+    redirect :redirection
+  end
+
+  def link_redirection
+    redirect link_raw(self.class)
+  end
+
+  def target
+    "#{self.class.name} target"
+  end
+end
+
+ramaze do
+  context "default Helper" do
+    
+    def lh
+      @lh ||= Class.new.extend(Ramaze::LinkHelper)
+    end
+
+    specify "testrun" do
+      get('/index').should == 'MainController'
+      get('/minor').should == 'MinorController'
+    end
+
+    specify "RedirectHelper" do
+      get('/redirection').should       == 'MainController target'
+      get('/minor/redirection').should == 'MinorController target'
+      get('/double_redirection').should       == 'MainController target'
+      get('/minor/double_redirection').should == 'MinorController target'
+    end
+
+    specify "LinkHelper" do
+      lh.link(:foo).should == %{<a href="foo">foo</a>}
+      lh.link(:foo, :bar).should == %{<a href="foo/bar">bar</a>}
+      lh.link(MinorController, :bar).should == %{<a href="/minor/bar">bar</a>}
+      lh.link(MainController, :bar).should == %{<a href="/bar">bar</a>}
+      lh.link('/foo/bar').should == %{<a href="/foo/bar">bar</a>}
+    end
+
+    specify "RedirectHelper and LinkHelper combined" do
+      get(lh.link_raw(MainController, :link_redirection)).should == "MainController"
+    end
+
+  end
+end
