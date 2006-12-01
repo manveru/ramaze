42deb0cde8c285236d346961c8efedecca6109f1
manveru <m.fellinger@gmail.com>
2006-12-01 13:57:00 +0000

    documentation for helpers
    darcs-hash:20061201135707-24576-f17d3acf391a7551a2984bea18c8b594816fb90c.gz

--- lib/ramaze/helper/link.rb
+++ lib/ramaze/helper/link.rb
@@ -1,5 +1,40 @@
 module Ramaze
+
+  # LinkHelper is included into the Template::Ramaze by default
+  #
+  # this helper tries to get along without any major magic, the only 'magic'
+  # thing is that it looks up controller-paths if you pass it a controller
+  # the text shown is always the last segmet of the finished link from split('/')
+  #
+  # usage is pretty much shown in test/tc_helper
+  # however, to give you some idea of how it actually works, some examples:
+  #
+  # link MainController, :foo                 #=> '<a href="/foo">foo</a>'
+  # link MinorController, :foo                #=> '<a href="/minor/foo">foo</a>'
+  # link MinorController, :foo, :bar          #=> '<a href="/minor/foo/bar">bar</a>'
+  # link MainController, :foo, :raw => true   #=> '/foo'
+  #
+  # link_raw MainController, :foo             #=> '/foo'
+  # link_raw MinorController, :foo            #=> '/minor/foo'
+  # link_raw MinorController, :foo, :bar      #=> '/minor/foo/bar'
+  #
+  # TODO:
+  #   - handling of no passed parameters
+  #   - setting of a custom link-title, possibly images as well
+  #   - setting of id or class
+  #   - taking advantae of Gestalt to build links
+  #   - lots of other minor niceties, for the moment i'm only concerned to keep
+  #     it as simple as possible
+  #
+
   module LinkHelper
+
+    # Usage:
+    #   link MainController, :foo                 #=> '<a href="/foo">foo</a>'
+    #   link MinorController, :foo                #=> '<a href="/minor/foo">foo</a>'
+    #   link MinorController, :foo, :bar          #=> '<a href="/minor/foo/bar">bar</a>'
+    #   link MainController, :foo, :raw => true   #=> '/foo'
+
     def link *to
       hash = to.last.is_a?(Hash) ? to.pop : {}
 
@@ -18,6 +53,11 @@ module Ramaze
         %{<a href="#{link}">#{title}</a>}
       end
     end
+
+    # Usage:
+    #   link_raw MainController, :foo             #=> '/foo'
+    #   link_raw MinorController, :foo            #=> '/minor/foo'
+    #   link_raw MinorController, :foo, :bar      #=> '/minor/foo/bar'
 
     def link_raw *to
       if to.last.is_a?(Hash)
--- lib/ramaze/helper/redirect.rb
+++ lib/ramaze/helper/redirect.rb
@@ -1,5 +1,28 @@
 module Ramaze
+
+  # RedirectHelper actually takes advantage of LinkHelper.link_raw to build the links
+  # it redirects to.
+  # It doesn't do much else than this:
+  #     setting a status-code of 303 and a head['Location'] = link
+  # returning some nice text for visitors who insist on ignoring those hints :P
+  #
+  # example of usage:
+  #   redirect MainController
+  #   redirect MainController, :foo
+  #   redirect 'foo/bar'
+  #
+  # TODO:
+  #   - setting custom status-code, it ignores any preset ones at the moment
+  #   - maybe some more options, like a delay
+  #
+
   module RedirectHelper
+
+    # Usage:
+    #   redirect MainController
+    #   redirect MainController, :foo
+    #   redirect 'foo/bar'
+
     def redirect *target
       target = target.join('/')
       response.head['Location'] = target
