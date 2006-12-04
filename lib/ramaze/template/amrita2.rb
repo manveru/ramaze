4db70e39c597d57169c216ec081015545ce5338e
manveru <m.fellinger@gmail.com>
2006-12-04 13:39:00 +0000

    rewrote Template::Amrita2 in the way the implementation of Erubis worked out, a lot cleaner and straight-forward. will have to minimize the overhead of find_file though.
    darcs-hash:20061204133904-24576-dc64ca8c729206cf2b54d46871db1dfeccbf1249.gz

--- lib/ramaze/template/amrita2.rb
+++ lib/ramaze/template/amrita2.rb
@@ -10,21 +10,35 @@ module Ramaze::Template
       include Trinity
 
       def handle_request request, action, *params
-        template_file = 
-        if template_root = trait[:template_root]
-          File.expand_path(File.join(template_root, action) << '.html')
-        else
-          File.expand_path((File.dirname($0) / 'template' / Global.mapping.invert[self] / action) << '.html')
-        end
 
-        template     = ::Amrita2::TemplateFile.new(template_file)
+        file = find_template(action)
+
+        return '' unless file
+
+        template = ::Amrita2::TemplateFile.new(file)
         out = ''
         template.expand(out, self.new)
         out
+
       rescue Object => ex
         puts ex
         Logger.error ex
         ''
+      end
+
+      def find_template action
+        path =
+          if template_root = trait[:template_root]
+            template_root / action
+          else
+            Global.template_root / Global.mapping.invert[self] / action
+          end
+        path = File.expand_path(File.dirname($0) / path)
+
+        exts = %w[html rhtml rmze xhtml]
+
+        files = exts.map{|ext| "#{path}.#{ext}"}
+        file = files.find{|file| File.file?(file)}
       end
     end
   end
