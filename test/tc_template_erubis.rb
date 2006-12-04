54af80a839cb440aab793dfdaeae98667ea9e011
manveru <m.fellinger@gmail.com>
2006-12-04 13:27:00 +0000

    added tc_template_erubis and it passes :)
    darcs-hash:20061204132715-24576-4d92d3f355c776fe2bbb0026ae84ddf1e658682f.gz

---
+++ test/tc_template_erubis.rb
@@ -0,0 +1,36 @@
+require 'lib/test/test_helper'
+
+include Ramaze
+
+class MainController < Template::Erubis
+  trait :template_root => 'template/erubis/'
+
+  def index
+    'Erubis Index'
+  end
+
+  def sum num1, num2
+    @num1, @num2 = num1.to_i, num2.to_i
+  end
+
+  def inline *args
+    @args = args
+    "<%= @args.inspect %>"
+  end
+end
+
+ramaze do
+  context "Erubis" do
+    specify "index" do
+      get('/').should == 'Erubis Index'
+    end
+
+    specify "sum" do
+      get('/sum/1/2').should == '3'
+    end
+
+    specify "inline" do
+      get('/inline/foo/bar').should == %w[foo bar].inspect
+    end
+  end
+end
