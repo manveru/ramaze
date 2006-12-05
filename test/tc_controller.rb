79f02c63cd09026f09c1d5c883fa7abf1c8b1ded
manveru <m.fellinger@gmail.com>
2006-12-05 06:36:00 +0000

    add tests for private methods to the controller
    darcs-hash:20061205063636-24576-ef1043e3fbf7be7c28578f7a1237cebad912f7a6.gz

--- test/tc_controller.rb
+++ test/tc_controller.rb
@@ -8,6 +8,11 @@ class AmritaController < Template::Amrita2
   def index
     "The index"
   end
+
+  private
+
+  def test_private
+  end
 end
 
 class RamazeController < Template::Ramaze
@@ -17,6 +22,11 @@ class RamazeController < Template::Ramaze
 
   def sum first, second
     first.to_i + second.to_i
+  end
+
+  private
+
+  def test_private
   end
 end
 
@@ -29,11 +39,34 @@ ramaze do
     specify "summing two values" do
       get('/ramaze/sum/1/2').should == '3'
     end
+
+    specify "should not respond to private methods" do
+      %w[ session request response find_template handle_request trait test_private ].each do |action|
+        lambda{get("/ramaze/#{action}")}.should_raise OpenURI::HTTPError
+      end
+    end
   end
 
   context "Testing Amrita" do
     specify "simple request to index" do
       get('/amrita').should == "<div>The index</div>"
+    end
+
+    specify "should not respond to private methods" do
+      accessable = AmritaController.public_instance_methods(false).sort
+      not_accessable = AmritaController.private_instance_methods(false).sort
+
+      accessable.each do |action|
+        lambda{get("/amrita/#{action}")}.should_raise OpenURI::HTTPError
+      end
+
+      not_accessable.each do |action|
+        lambda{get("/amrita/#{action}")}.should_raise OpenURI::HTTPError
+      end
+
+      %w[ session request response find_template handle_request trait test_private ].each do |action|
+        lambda{get("/amrita/#{action}")}.should_raise OpenURI::HTTPError
+      end
     end
   end
 end
