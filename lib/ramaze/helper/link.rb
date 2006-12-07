e277b6b982a1b2e3f4a337ded879878af667f5e0
manveru <m.fellinger@gmail.com>
2006-12-07 11:33:00 +0000

    changing link_raw to R - you can also set titles now with link(:foo, :title => 'foo')
    darcs-hash:20061207113315-24576-ec17e5d830a4e48d49da415ef32cd15d56ee7b61.gz

--- lib/ramaze/helper/link.rb
+++ lib/ramaze/helper/link.rb
@@ -23,7 +23,7 @@ module Ramaze
   #
   # TODO:
   #   - handling of no passed parameters
-  #   - setting of a custom link-title, possibly images as well
+  #   - setting imagelinks
   #   - setting of id or class
   #   - taking advantae of Gestalt to build links
   #   - lots of other minor niceties, for the moment i'm only concerned to keep
@@ -37,6 +37,7 @@ module Ramaze
     #   link MinorController, :foo                #=> '<a href="/minor/foo">foo</a>'
     #   link MinorController, :foo, :bar          #=> '<a href="/minor/foo/bar">bar</a>'
     #   link MainController, :foo, :raw => true   #=> '/foo'
+    #   link MainController, :foo, :title => 'a'  #=> '<a href="/minor/foo/bar">a</a>'
 
     def link *to
       hash = to.last.is_a?(Hash) ? to.pop : {}
@@ -48,21 +49,21 @@ module Ramaze
       end
 
       link = to.join('/').squeeze('/')
-      title = link.split('/').last
 
       if hash[:raw]
         link
       else
+        title = link.split('/').last || hash[:title] || 'index'
         %{<a href="#{link}">#{title}</a>}
       end
     end
 
     # Usage:
-    #   link_raw MainController, :foo             #=> '/foo'
-    #   link_raw MinorController, :foo            #=> '/minor/foo'
-    #   link_raw MinorController, :foo, :bar      #=> '/minor/foo/bar'
+    #   R MainController, :foo        #=> '/foo'
+    #   R MinorController, :foo       #=> '/minor/foo'
+    #   R MinorController, :foo, :bar #=> '/minor/foo/bar'
 
-    def link_raw *to
+    def R *to
       if to.last.is_a?(Hash)
         to.last[:raw] = true
       else
