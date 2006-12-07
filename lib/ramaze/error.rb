4bbccd9776ea9729ff299ed4e43d3d46205b6acf
manveru <m.fellinger@gmail.com>
2006-12-07 13:33:00 +0000

    a new error-page, totally implemented in Template::Ramaze instead of Gestalt, left the Gestalt one in there commented, not sure what i should do with it... use it as an example? (might horrify people though ;)
    darcs-hash:20061207133353-24576-1fde840672d06036389a33baac9c8e4c95a49c24.gz

--- lib/ramaze/error.rb
+++ lib/ramaze/error.rb
@@ -55,6 +55,113 @@ module Ramaze
       # traceback, just install rubygems/coderay :)
       # oh, and yeah, no example - don't call this yourself
 
+      def error_page colors, title, *backtrace
+        ivs = {
+          :backtrace => backtrace,
+          :colors => colors,
+          :title => title,
+          :coderay => Object.constants.include?('CodeRay'),
+        }
+
+        template = 
+          <<-HEREDOC
+<html>
+  <head>
+    <title><%= @title %></title>
+    <link rel="stylesheet" href="css/coderay.css" />
+    <style type="text/css">
+      <!--
+      h1.main {
+        text-align: center;
+        }
+      table.main {
+        background: #000;
+        }
+      table.main tr.head {
+        background: #fee;
+        width:      100%;
+        }
+      table.main tr.source_container {
+        display:    none;
+        }
+      tr.source_container div {
+        width:      100%;
+        overflow:   auto;
+        }
+      tr.source_container div table {
+        background: #ddd;
+        width:      100%;
+        }
+      tr.source_container div table tr {
+        text-align:center;
+        }
+      tr.source_container div table tr.source {
+        text-align:left;
+        }
+      div.source {
+        background: #fff;
+        }
+      -->
+    </style>
+    <script type="text/javascript" src="/js/jquery.js"></script>
+  </head>
+  <body>
+    <span style="float:left;">
+      <img src="/ramaze.png" />
+    </span>
+    <h1><%= @title %></h1>
+    <table class="main">
+      <tr class="head">
+        <td>File</td><td>Line</td><td>Method</td>
+      </tr>
+      <?r @backtrace.each do |lines, hash, file, lineno, meth| ?>
+        <tr id="line_<%= hash %>" style="background:rgb(<%= @colors.shift %>,70,60);">
+          <td><%= file %></td><td><%= lineno %></td><td><%= meth %></td>
+        </tr>
+        <tr id="source_<%= hash %>" style="display:none;">
+          <td colspan="3">
+            <div class="source">
+              <table>
+                <tr>
+                  <td colspan="2">vim <%= file %> +<%= lineno %></td>
+                </tr>
+                <?r lines.each do |llineno, lcode, lcurrent| ?>
+                  <tr class="source"<%=  'style="background:#faa;"' if lcurrent %>>
+                    <td><%= llineno %></td>
+                    <td>
+                      <%= @coderay ? CodeRay.scan(lcode, :ruby).html : "<pre>\#{lcode}</pre>" %>
+                    </td>
+                  </tr>
+                <?r end ?>
+              </table>
+            </div>
+            <script type="text/javascript">
+              $("tr#line_<%= hash %>").click(function(){$("tr#source_<%= hash %>").toggle()});
+            </script>
+          </td>
+        </tr>
+      <?r end ?>
+    </table>
+    <?r { 'Session'   => Thread.current[:session], 'Request'   => Thread.current[:request], 'Response'  => Thread.current[:response], 'Global'    => Global, }.each do |title, content| hash = [title, content].object_id.abs ?>
+      <div class="additional">
+        <h3 id="show_<%= hash %>"><%= title %></h3>
+        <pre style="display:none" id="is_<%= hash %>"><%= content.pretty_inspect %></pre>
+        <script type="text/javascript">
+          $("h3#show_<%= hash %>").click(function(){$("pre#is_<%= hash %>").toggle()});
+        </script>
+      </div>
+    <?r end ?>
+  </body>
+</html>
+        HEREDOC
+
+        Template::Ramaze.new.send(:transform, template, ivs)
+      end
+    end
+  end
+end
+
+=begin
       def error_page(colors, title, *backtrace)
         show_hide_block = lambda do |title, content|
           hash = [title, content].object_id.abs
@@ -63,7 +170,6 @@ module Ramaze
               h3(:id => "show_#{hash}"){title}
               pre(:style => 'display:none', :id => "is_#{hash}"){content}
               script(:type => 'text/javascript') do
-                %{ $("h3#show_#{hash}").click(function(){$("pre#is_#{hash}").toggle()}); }
               end
             end
             }.to_s
@@ -162,9 +268,8 @@ module Ramaze
         }.to_s
       end
 
-      def show_hide_block(title, content)
+def show_hide_block(title, content)
         # get a unique id (more or less)
       end
     end
-  end
-end
+=end
