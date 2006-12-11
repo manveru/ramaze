edfc14c6e72d03b6ba0c68daff8521685ff2d799
manveru <m.fellinger@gmail.com>
2006-12-11 14:25:00 +0000

    require helper.rb in template.rb
    darcs-hash:20061211142503-24576-53086b8c73584cbd9ebcc978dd2706c451cf4f0d.gz

--- lib/ramaze/template.rb
+++ lib/ramaze/template.rb
@@ -1,6 +1,8 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
+require 'ramaze/helper'
+
 module Ramaze::Template
   %w[ Ramaze Amrita2 Erubis Markaby ].each do |const|
     autoload(const, "ramaze/template/#{const.downcase}")
