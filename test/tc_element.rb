eb5afdfc4e7c4a75dc4b9cd4c82f37ad7de6ca34
manveru <m.fellinger@gmail.com>
2006-12-13 10:05:00 +0000

    added a very basic testcases for how Element should work
    darcs-hash:20061213100507-24576-800b804c68e5db2cdcc88cd8a0ec275d314208f2.gz

---
+++ test/tc_element.rb
@@ -0,0 +1,34 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
+require 'lib/test/test_helper'
+
+include Ramaze
+
+class TCElementController < Template::Ramaze
+  def index
+    "The index"
+  end
+
+  def elementy
+    transform "<Page>#{index}</Page>"
+  end
+end
+
+class Page < Ramaze::Element
+  def render
+    %{ <wrap> #{content} </wrap> }
+  end
+end
+
+ramaze(:mapping => {'/' => TCElementController}) do
+  context "Mongrel" do
+    specify "simple request" do
+      get('/').should == "The index"
+    end
+
+    specify "with element" do
+      get('/elementy').should == "<wrap> The index </wrap>"
+    end
+  end
+end
