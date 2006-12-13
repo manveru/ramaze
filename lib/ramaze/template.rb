526cd92a00e377234ecc8624077b47d6264ec56e
manveru <m.fellinger@gmail.com>
2006-12-13 13:14:00 +0000

    this introduces Ramaze::Template::Template - it's a really stupid name, but i couldn't come up with anything better... this is the first step in slacking down the templates (they are really not DRY anymore)
    darcs-hash:20061213131428-24576-d808de48816f4ca51bf84d94b8efaaedc0cb7dcf.gz

--- lib/ramaze/template.rb
+++ lib/ramaze/template.rb
@@ -7,4 +7,52 @@ module Ramaze::Template
   %w[ Ramaze Amrita2 Erubis Markaby ].each do |const|
     autoload(const, "ramaze/template/#{const.downcase}")
   end
+
+  class Template
+    extend ::Ramaze::Helper
+
+    helper :link, :redirect
+
+    # This finds the template for the given action on the current controller
+    # there are some basic ways how you can provide an alternative path:
+    #
+    # Global.template_root = 'default/path'
+    #
+    # class FooController < Template::Ramaze
+    #   trait :template_root => 'another/path'
+    #   trait :index_template => :foo
+    #
+    #   def index
+    #   end
+    # end
+    #
+    # One feature also used in the above example is the custom template for
+    # one action, in this case :index - now the template of :foo will be
+    # used instead.
+
+    def self.find_template action
+      custom_template = trait["#{action}_template".intern] ||
+        self.class.trait["#{action}_template".intern]
+      action = custom_template if custom_template
+
+      path =
+        if template_root = trait[:template_root] || trait[:template_root]
+          p [template_root, action]
+          template_root / action
+        else
+          p [Global.template_root , Global.mapping.invert[self] , action]
+          Global.template_root / Global.mapping.invert[self] / action
+        end
+      path = File.expand_path(path)
+
+      extensions = ancestors.find{|a| a.trait[:template_extensions]}.trait[:template_extensions]
+
+      p :extensions => extensions
+
+      file = Dir["#{path}.{#{extensions.join(',')}}"].first
+      p :file => file
+      p :path => path
+      file
+    end
+  end
 end
