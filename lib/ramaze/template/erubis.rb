925a4de360498484a520ee79d38b50bbf28c5029
manveru <m.fellinger@gmail.com>
2006-12-13 13:16:00 +0000

    simplify template/erubis by inheriting from Template
    darcs-hash:20061213131606-24576-1ecf9f0ad26c418d7c2b29c16789b0414c55f830.gz

--- lib/ramaze/template/erubis.rb
+++ lib/ramaze/template/erubis.rb
@@ -4,35 +4,24 @@
 require 'erubis'
 
 module Ramaze::Template
-  class Erubis
+  class Erubis < Template
     extend Ramaze::Helper
 
     trait :actionless => false
-
-    private
-
-    helper :link, :redirect
-
-    def transform string, bound = binding
-      self.class.transform(string, bound)
-    end
+    trait :template_extensions => %w[rhtml rmze xhtml html]
 
     class << self
-      include Ramaze::Helper
-
       # initializes the handling of a request on the controller.
       # Creates a new instances of itself and sends the action and params.
       # Also tries to render the template.
       # In Theory you can use this standalone, this has not been tested though.
 
       def handle_request request, action, *params
-
         controller = self.new
         controller.instance_variable_set('@action', action)
         result = controller.send(action, *params) if controller.respond_to?(action)
 
         file = find_template(action)
-
 
         template =
           if file
@@ -45,31 +34,20 @@ module Ramaze::Template
 
         bound = result.is_a?(Binding) ? result : controller.send(:send, :binding)
 
-        transform(template, bound)
+        controller.send(:transform, template, bound)
       rescue Object => ex
         puts ex
         Logger.error ex
         ''
       end
+    end
 
-      def transform string, bound = binding
-        eruby = ::Erubis::Eruby.new(string)
-        eruby.result(bound)
-      end
+    private
 
-      def find_template action
-        path = 
-          if template_root = trait[:template_root]
-            template_root / action
-          else
-            Global.template_root / Global.mapping.invert[self] / action
-          end
-        path = File.expand_path(path)
 
-        exts = %w[rhtml rmze xhtml html ephp ec ejava escheme eprl ejs].join(',')
-
-        file = Dir["#{path}.{#{exts}}"].first
-      end
+    def transform string, bound = binding
+      eruby = ::Erubis::Eruby.new(string)
+      eruby.result(bound)
     end
   end
 end
