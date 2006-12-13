f7f03665f4fa9bcc74732748be7f14ed205c3991
manveru <m.fellinger@gmail.com>
2006-12-13 13:22:00 +0000

    remove the test for the amrita-controller, it's not neccesary anymore since we have an own testsuite for it...
    darcs-hash:20061213132254-24576-8233efb2ab7b3da5123ce212224f653c016f4a84.gz

--- test/tc_controller.rb
+++ test/tc_controller.rb
@@ -5,17 +5,6 @@ require 'lib/test/test_helper'
 
 include Ramaze
 
-class TCControllerAmritaController < Template::Amrita2
-  def index
-    "The index"
-  end
-
-  private
-
-  def test_private
-  end
-end
-
 class TCControllerRamazeController < Template::Ramaze
   def index
     "The index"
@@ -31,7 +20,7 @@ class TCControllerRamazeController < Template::Ramaze
   end
 end
 
-ramaze(:mapping => {'/amrita' => TCControllerAmritaController, '/ramaze' => TCControllerRamazeController}) do
+ramaze(:mapping => {'/ramaze' => TCControllerRamazeController}) do
   context "Testing Ramaze" do
     specify "simple request to index" do
       get('/ramaze').should == 'The index'
@@ -44,30 +33,6 @@ ramaze(:mapping => {'/amrita' => TCControllerAmritaController, '/ramaze' => TCCo
     specify "should not respond to private methods" do
       %w[ session request response find_template handle_request trait test_private ].each do |action|
         lambda{get("/ramaze/#{action}")}.should_raise OpenURI::HTTPError
-      end
-    end
-  end
-
-  context "Testing Amrita" do
-    specify "simple request to index" do
-      get('/amrita').should == "<div>The index</div>"
-    end
-
-    specify "should not respond to private methods" do
-      accessable = TCControllerAmritaController.public_instance_methods(false).sort
-      not_accessable = TCControllerAmritaController.private_instance_methods(false).sort
-
-      accessable.each do |action|
-        get("/amrita/#{action}").should_not == ''
-        lambda{get("/amrita/#{action}")}.should_not_raise OpenURI::HTTPError
-      end
-
-      not_accessable.each do |action|
-        lambda{get("/amrita/#{action}")}.should_raise OpenURI::HTTPError
-      end
-
-      %w[ session request response find_template handle_request trait test_private ].each do |action|
-        lambda{get("/amrita/#{action}")}.should_raise OpenURI::HTTPError
       end
     end
   end
