9945be4a8c92f7a253b2f532d5a7abe63e53a281
manveru <m.fellinger@gmail.com>
2006-12-15 13:11:00 +0000

    reimplementation of Global - should be a lot saner and simpler now ;)
    darcs-hash:20061215131100-24576-096d0c89d4a831806e737436e914be6dd3ad397c.gz

--- lib/ramaze/global.rb
+++ lib/ramaze/global.rb
@@ -1,116 +1,77 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
+require 'ostruct'
+
 module Ramaze
-  class Global
-    class << self
-      # The default variables for Global:
-      #   uses the class from Adapter:: (is required automatically)
-      #       Global.adapter #=> :webrick
-      #   restrict access to a specific host
-      #       Global.host #=> '0.0.0.0'
-      #   adapter runs on that port
-      #       Global.port #=> 7000
-      #   the running/debugging-mode (:debug|:stage|:live|:silent) - atm only differ in verbosity
-      #       Global.mode #=> :debug
-      #   detaches Ramaze to run in the background (used for testcases)
-      #       Global.run_loose #=> false
-      #   caches requests to the controller based on request-values (action/params)
-      #       Global.cache #=> false
-      #   run tidy over the generated html if Content-Type is text/html
-      #       Global.tidy #=> false
-      #   display an error-page with backtrace on errors (empty page otherwise)
-      #       Global.error_page    #=> true
-      #   trap this signal for clean shutdown (calls Ramaze.shutdown)
-      #       Global.shutdown_trap #=> 'SIGINT'
+  class GlobalStruct < OpenStruct
+    # The default variables for Global:
+    #   uses the class from Adapter:: (is required automatically)
+    #       Global.adapter #=> :webrick
+    #   restrict access to a specific host
+    #       Global.host #=> '0.0.0.0'
+    #   adapter runs on that port
+    #       Global.port #=> 7000
+    #   the running/debugging-mode (:debug|:stage|:live|:silent) - atm only differ in verbosity
+    #       Global.mode #=> :debug
+    #   detaches Ramaze to run in the background (used for testcases)
+    #       Global.run_loose #=> false
+    #   caches requests to the controller based on request-values (action/params)
+    #       Global.cache #=> false
+    #   run tidy over the generated html if Content-Type is text/html
+    #       Global.tidy #=> false
+    #   display an error-page with backtrace on errors (empty page otherwise)
+    #       Global.error_page    #=> true
+    #   trap this signal for clean shutdown (calls Ramaze.shutdown)
+    #       Global.shutdown_trap #=> 'SIGINT'
 
-      DEFAULT_OPTIONS = {
-        :host           => '0.0.0.0',
-        :port           => 7000,
-        :mode           => :debug,
-        :tidy           => false,
-        :cache          => false,
-        :adapter        => :webrick,
-        :run_loose      => false,
-        :error_page     => true,
-        :template_root  => 'template',
+    DEFAULT = {
+      :host           => '0.0.0.0',
+      :port           => 7000,
+      :mode           => :debug,
+      :tidy           => false,
+      :cache          => false,
+      :adapter        => :webrick,
+      :run_loose      => false,
+      :error_page     => true,
+      :template_root  => 'template',
 
-        :autoreload => {
-          :benchmark  => 5,
-          :debug      => 5,
-          :stage      => 10,
-          :live       => 20,
-          :silent     => 40,
+      :autoreload => {
+        :benchmark  => 5,
+        :debug      => 5,
+        :stage      => 10,
+        :live       => 20,
+        :silent     => 40,
         },
-        :logger => {
-          :timestamp    => "%Y-%m-%d %H:%M:%S",
-          :prefix_info  => 'INFO',
-          :prefix_error => 'ERRO',
-          :prefix_debug => 'DEBG',
-        }
+      :logger => {
+        :timestamp    => "%Y-%m-%d %H:%M:%S",
+        :prefix_info  => 'INFO',
+        :prefix_error => 'ERRO',
+        :prefix_debug => 'DEBG',
       }
+    }
 
-      @@table = nil
+    def setup hash = {}
+      table.merge!(hash)
+    end
 
-      def create(hash = DEFAULT_OPTIONS)
-        @@table = Thread.main[:global] = hash
-      end
+    def update hash = {}
+      table.merge!(hash.merge(table))
+    end
 
-      def table
-        @@table
-      end
+    def []=(key, value)
+      table[key] = value
+    end
 
-      def setup hash = {}
-        create unless @@table
-        @@table.merge!(hash)
-      end
+    def [](key)
+      table[key]
+    end
 
-      def update(hash = {})
-        create unless @@table
-        @@table = hash.merge(@@table)
-      end
+    def values_at(*keys)
+      table.values_at(*keys)
+    end
+  end
 
-      def [](key)
-        create unless @@table
-        @@table[key.to_sym]
-      end
-
-      def []=(key, value)
-        create unless @@table
-        @@table[key.to_sym] = value
-      end
-
-      def method_missing(meth, *args, &block)
-        create unless @@table
-        if meth.to_s[-1..-1] == '='
-          key = meth.to_s[0..-2].to_sym
-          @@table.send("[]=", key, *args)
-          class_eval %{
-            def #{key}
-              @@table[#{key}]
-            end
-          }
-          self.send(key)
-        elsif args.empty?
-          @@table[meth] ||= nil
-        else
-          @@table.send(meth, *args, &block)
-        end
-      rescue => ex
-        nil
-      end
-
-      def inspect
-        create unless @@table
-        @@table.inspect
-      end
-
-      def pretty_inspect
-        create unless @@table
-        @@table.pretty_inspect
-      end
-
-    end # class << self
-
-  end # Global
-end # Ramaze
+  Thread.current[:global] = GlobalStruct.new(GlobalStruct::DEFAULT)
+  Global = Thread.current[:global]
+end
