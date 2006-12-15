8ba8d8fe5d33f2da5100df72245bb104b8efa53b
manveru <m.fellinger@gmail.com>
2006-12-15 13:12:00 +0000

    rewrite of the tc_global for the new structure
    darcs-hash:20061215131210-24576-cdd86e7baeed638049947e60b964ed652ee9cbef.gz

--- test/tc_global.rb
+++ test/tc_global.rb
@@ -6,29 +6,39 @@ require 'lib/test/test_helper'
 include Ramaze
 
 context "Global" do
-  specify "create" do
-    Global.create :foo => :bar, :x => :y
-    Global.foo.should.equal :bar
-    Global.x.should.equal :y
-  end
-
-  specify "update" do
-    Global.update :foo => :fuz, :baz => :foobar
-    Global[:foo].should == :bar
-    Global[:baz].should == :foobar
+  specify "just simple assignment and retrive" do
+    Global.some = :xxx
+    Global.some.should == :xxx
   end
 
   specify "setup" do
-    Global.setup :foo => :baz, :foobar => :bar
-    Global.foo.should == :baz
-    Global.foobar.should == :bar
-    Global.update :foo => :bar
-    Global.foo.should == :baz
-    Global.foobar.should == :bar
+    Global.setup :a => :b
+    Global.a.should == :b
+    Global.some.should == :xxx
+    Global.setup :a => :c
+    Global.a.should == :c
   end
 
-  specify "just simple assignment and retrive" do
-    Global.some = :xxx
-    Global.some.should.equal :xxx
+  specify "more neat stuff" do
+    Global.update :a => :d, :foo => :bar
+    Global.a.should == :c
+    Global.foo.should == :bar
+  end
+
+  specify "values_at" do
+    Global.values_at(:a, :foo).should == [:c, :bar]
+  end
+
+  specify "getting thready" do
+    (1..10).each do |i|
+      Thread.new do
+        sleep i / 10.0
+        Global[:i] = i
+        Thread.main[:i] = i
+      end
+    end
+    sleep 0.5
+    Global[:i].should == Thread.main[:i]
+    Global[:i].should == 5
   end
 end
