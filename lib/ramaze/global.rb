136b2c18d6773453d73f6d9e533d7018114dfea3
manveru <m.fellinger@gmail.com>
2006-12-15 05:20:00 +0000

    patching Global further, we move the defaults in here since they only clutter the ramaze.rb and make handling difficult
    darcs-hash:20061215052021-24576-741bb1b56d0fd279df0e04a6dd52848dc2fcecab.gz

--- lib/ramaze/global.rb
+++ lib/ramaze/global.rb
@@ -4,6 +4,52 @@
 module Ramaze
   class Global
     class << self
+      # The default variables for Global:
+      #   uses the class from Adapter:: (is required automatically)
+      #       Global.adapter #=> :webrick
+      #   restrict access to a specific host
+      #       Global.host #=> '0.0.0.0'
+      #   adapter runs on that port
+      #       Global.port #=> 7000
+      #   the running/debugging-mode (:debug|:stage|:live|:silent) - atm only differ in verbosity
+      #       Global.mode #=> :debug
+      #   detaches Ramaze to run in the background (used for testcases)
+      #       Global.run_loose #=> false
+      #   caches requests to the controller based on request-values (action/params)
+      #       Global.cache #=> false
+      #   run tidy over the generated html if Content-Type is text/html
+      #       Global.tidy #=> false
+      #   display an error-page with backtrace on errors (empty page otherwise)
+      #       Global.error_page    #=> true
+      #   trap this signal for clean shutdown (calls Ramaze.shutdown)
+      #       Global.shutdown_trap #=> 'SIGINT'
+
+      DEFAULT_OPTIONS = {
+        :host           => '0.0.0.0',
+        :port           => 7000,
+        :mode           => :debug,
+        :tidy           => false,
+        :cache          => false,
+        :adapter        => :webrick,
+        :run_loose      => false,
+        :error_page     => true,
+        :template_root  => 'template',
+
+        :autoreload => {
+          :benchmark  => 5,
+          :debug      => 5,
+          :stage      => 10,
+          :live       => 20,
+          :silent     => 40,
+        },
+        :logger => {
+          :timestamp    => "%Y-%m-%d %H:%M:%S",
+          :prefix_info  => 'INFO',
+          :prefix_error => 'ERRO',
+          :prefix_debug => 'DEBG',
+        }
+      }
+
       @@table = {}
 
       def create(h = {})
@@ -12,6 +58,11 @@ module Ramaze
 
       def table
         @@table
+      end
+
+      def setup hash = DEFAULT_OPTIONS
+        create
+        update hash
       end
 
       def update(h = {})
@@ -31,13 +82,19 @@ module Ramaze
         if meth.to_s[-1..-1] == '='
           key = meth.to_s[0..-2].to_sym
           @@table.send("[]=", key, *args)
+          class_eval %{
+            def #{key}
+              @@table[#{key}]
+            end
+          }
+          self.send(key)
         elsif args.empty?
           @@table[meth] ||= nil
         else
           @@table.send(meth, *args, &block)
         end
-      rescue
-        super
+      rescue => ex
+        nil
       end
 
       def inspect
@@ -49,5 +106,6 @@ module Ramaze
       end
 
     end # class << self
+
   end # Global
 end # Ramaze
