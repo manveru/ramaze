c3e3c1ab82418990af23449eb323724d76a6b877
manveru <m.fellinger@gmail.com>
2006-12-24 18:51:00 +0000

    fix the error-page to work with the new template-implementation... also remove the old Gestalt-code, it's in darcs anyway
    darcs-hash:20061224185118-24576-6a1a2f791307059f1728575d0bf51347e974a366.gz

--- lib/ramaze/error.rb
+++ lib/ramaze/error.rb
@@ -57,12 +57,10 @@ module Ramaze
       # oh, and yeah, no example - don't call this yourself
 
       def error_page colors, title, *backtrace
-        ivs = {
-          :backtrace => backtrace,
-          :colors => colors,
-          :title => title,
-          :coderay => Object.constants.include?('CodeRay'),
-        }
+        @backtrace = backtrace
+        @colors = colors
+        @title = title
+        @coderay = Object.constants.include?('CodeRay')
 
         template = 
           <<-HEREDOC
@@ -164,121 +162,8 @@ module Ramaze
 </html>
         HEREDOC
 
-        Ramaze::Template::Ramaze.new.send(:transform, template, ivs)
+        Ramaze::Template::Ramaze.new.send(:transform, template, binding)
       end
     end
   end
 end
-
-=begin
-      def error_page(colors, title, *backtrace)
-        show_hide_block = lambda do |title, content|
-          hash = [title, content].object_id.abs
-          Gestalt.new{
-            div(:class => :additional) do
-              h3(:id => "show_#{hash}"){title}
-              pre(:style => 'display:none', :id => "is_#{hash}"){content}
-              script(:type => 'text/javascript') do
-              end
-            end
-            }.to_s
-        end
-
-        coderay = Object.constants.include?('CodeRay')
-
-        Gestalt.new{
-          html do
-            head do
-              title{ title }
-              link :rel => "stylesheet", :href => "css/coderay.css"
-              style(:type => 'text/css') do
-                  %[
-                  <!--
-                  h1.main {
-                    text-align: center;
-                    }
-                  table.main {
-                    width:      100%;
-                    background: #000;
-                    }
-                  table.main tr.head {
-                    background: #fee;
-                    width:      100%;
-                    }
-                  table.main tr.source_container {
-                    display:    none;
-                    }
-                  tr.source_container div {
-                    width:      100%;
-                    overflow:   auto;
-                    }
-                  tr.source_container div table {
-                    background: #ddd;
-                    width:      100%;
-                    }
-                  tr.source_container div table tr {
-                    text-align:center;
-                    }
-                  tr.source_container div table tr.source {
-                    text-align:left;
-                    }
-                  div.source {
-                    background: #fff;
-                    }
-                  -->
-                   ]
-              end
-              # stupid firefox-hack... had to wait till the day something like that is
-              # neccesary :P
-              script(:type => 'text/javascript', :src => '/js/jquery.js'){}
-            end
-            body do
-              span(:style => 'float:left') do
-                img :src => '/ramaze.png'
-              end
-              h1(:class => :main){ title }
-              table(:class => :main) do
-                tr(:class => :head){ %w[File Line Method].each{|s| td{ s } } }
-
-                backtrace.each do |lines, hash, file, lineno, meth|
-                  tr(:id => "line_#{hash}", :style => "background:rgb(#{colors.shift},70,60);") do
-                    [file, lineno, meth].each{|s| td{ s } }
-                  end
-                  tr(:id => "source_#{hash}", :style => 'display:none') do
-                    td(:colspan => 3) do
-                      div(:class => :source) do
-                        table do
-                          tr{ td(:colspan => 2){ "vim #{file} +#{lineno}" } }
-
-                          lines.each do |llineno, lcode, lcurrent|
-                            style = lcurrent ? {:style => 'background:#faa;'} : {}
-                            tr(style.merge(:class => :source)) do
-                              td{ llineno.to_s }
-                              td do
-                                coderay ? CodeRay.scan(lcode, :ruby).html : pre{ lcode }
-                              end # td
-                            end # tr
-                          end # lines.each
-                        end # table
-                      end # div
-                      script(:type => 'text/javascript') do
-                        %{ $("tr#line_#{hash}").click(function(){$("tr#source_#{hash}").toggle()}); }
-                      end # script
-                    end # td
-                  end # tr
-                end # backtrace.each
-              end # table
-              show_hide_block['Session',  Thread.current[:session].pretty_inspect  ] +
-              show_hide_block['Request',  Thread.current[:request].pretty_inspect  ] +
-              show_hide_block['Response', Thread.current[:response].pretty_inspect ] +
-              show_hide_block['Global',   Global.pretty_inspect                    ]
-            end # body
-          end # html
-        }.to_s
-      end
-
-def show_hide_block(title, content)
-        # get a unique id (more or less)
-      end
-    end
-=end
