5fe2458b7494203f03b5b1b21f00eaf761da787f
manveru <m.fellinger@gmail.com>
2006-12-29 04:25:00 +0000

    change from digest/sha1 to digest/sha2 - would someone explain me what this does?
    darcs-hash:20061229042519-24576-8efa60e45158d3be1d73e7d1bab871abadaef3df.gz

--- lib/ramaze/trinity/session.rb
+++ lib/ramaze/trinity/session.rb
@@ -1,7 +1,7 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
-require 'digest/sha1'
+require 'digest/sha2'
 
 module Ramaze
   class Session
