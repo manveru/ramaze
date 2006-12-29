c1cd946482bd9e732277c756e0063028810b1371
manveru <m.fellinger@gmail.com>
2006-12-29 04:42:00 +0000

    fix tc_tidy
    darcs-hash:20061229044223-24576-1a5a38aaf3cca193d87a2b9555296d8393439a3b.gz

--- test/tc_tidy.rb
+++ test/tc_tidy.rb
@@ -1,8 +1,7 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
-require 'ramaze'
-require 'test/test_helper'
+require 'lib/test/test_helper'
 
 require 'ramaze/tool/tidy'
 
