a69e6cbe97b9f373b816a528bc95d8be9294af68
manveru <m.fellinger@gmail.com>
2006-12-31 14:32:00 +0000

    big changes again in the form-helper... added loads of options, most are used in the testcases (that still have to be expanded, but they give a good idea of what is possible now)
    darcs-hash:20061231143251-24576-a5926199a1edb9a494d1b0738bd8c22a2267a79f.gz

--- lib/ramaze/helper/form.rb
+++ lib/ramaze/helper/form.rb
@@ -6,7 +6,10 @@ module Ramaze
       Fixnum  => :number,
     }
 
-    def form obj
+    def form obj, options = {}
+      default = {:except => /oid/, :submit => true}
+      options = default.merge(options)
+
       if obj.respond_to? :serializable_attributes
         instance = obj.new
       else
@@ -18,79 +21,93 @@ module Ramaze
       out = []
 
       attributes.each do |attribute|
-        klass   = obj.ann[attribute].class
-        control = obj.ann[attribute].control
-        value   = instance.send(attribute) rescue nil
-        control = ClassMap[klass] unless control.is_a?(Symbol)
+        keep = decide_attribute(attribute, options)
+        next unless keep
 
-        p :klass => klass, :control => control, :value => value
-        out << Control.send(control, attribute, value)
+        o = OpenStruct.new :klass => obj.ann[attribute].class,
+                           :value => (instance.send(attribute) rescue nil),
+                           :name  => attribute,
+                           :title => (options.has_key?(attribute) ? options[attribute] : attribute)
+
+        control = obj.ann[attribute].control
+        control = ClassMap[o.klass] unless control.is_a?(Symbol)
+
+        out << Control.send(control, o)
+      end
+      if options[:submit] == true
+        out << %{<input type="submit" />}
+      elsif options[:submit]
+        out << %{<input type="submit" value="#{options[:submit]}" />}
       end
       out.join("<br />\n")
+    end
+
+    def decide_attribute(attribute, options)
+      keep = true
+      options.each do |key, values|
+        return keep unless keep
+        values = [values].flatten
+        case key
+        when :except, :reject, :exclude
+          values.each do |value|
+            if value.kind_of?(Regexp)
+              keep = !attribute.to_s.match(value)
+            else
+              keep = attribute != value
+            end
+          end
+        end
+      end
+      !!keep
     end
 
     module Control
       class << self
 
-        def number name, value
-          value ||= 0
-          text(name, value)
+        def number o
+          o.value ||= 0
+          text(o)
         end
 
-        def text name, value
-          value ||= ""
-          %{<input type="text" name="#{name}" value="#{value}" />}
+        def text o
+          o.value ||= ""
+          tag = ''
+          tag << "#{o.title}: " if o.title
+          tag << %{<input type="text" name="#{o.name}" value="#{o.value}" />}
         end
 
-        def textarea name, value
-          value ||= ""
-          %{<textarea name="#{name}">#{value}</textarea>}
+        def textarea o
+          o.value ||= ""
+          %{<textarea name="#{o.name}">#{o.value}</textarea>}
         end
 
-        def date name, value
-          value ||= Date.today
+        def date o
+          o.value ||= Date.today
           selects = []
-          selects << date_day(name, value.day)
-          selects << date_month(name, value.month)
-          selects << date_year(name, value.year)
+          selects << date_day(o.temp(:value => o.value.day))
+          selects << date_month(o.temp(:value => o.value.month))
+          selects << date_year(o.temp(:value => o.value.year))
           selects.join("\n")
         end
 
-        def time name, value
-          value ||= Time.now
+        def time o
+          o.value ||= Time.now
           selects = []
-          selects << date_day(name, value.day)
-          selects << date_month(name, value.month)
-          selects << date_year(name, value.year)
-          selects << time_hour(name, value.hour)
-          selects << time_minute(name, value.min)
-          selects << time_second(name, value.sec)
+          selects << date_day(    o.temp(:value => o.value.day))
+          selects << date_month(  o.temp(:value => o.value.month))
+          selects << date_year(   o.temp(:value => o.value.year))
+          selects << time_hour(   o.temp(:value => o.value.hour))
+          selects << time_minute( o.temp(:value => o.value.min))
+          selects << time_second( o.temp(:value => o.value.sec))
           selects.join("\n")
         end
 
-        def date_day name, value
-          select name, (1..31), value
-        end
-
-        def date_month name, value
-          select name, (1..21), value
-        end
-
-        def date_year name, value
-          select name, (1950..2050), value
-        end
-
-        def time_hour name, value
-          select name, (0...24), value
-        end
-
-        def time_minute name, value
-          select name, (0...60), value
-        end
-
-        def time_second name, value
-          select name, (0...60), value
-        end
+        def time_second(o) select(o.name, (0...60),     o.value) end
+        def time_minute(o) select(o.name, (0...60),     o.value) end
+        def time_hour(o)   select(o.name, (0...24),     o.value) end
+        def date_day(o)    select(o.name, (1..31),      o.value) end
+        def date_month(o)  select(o.name, (1..21),      o.value) end
+        def date_year(o)   select(o.name, (1950..2050), o.value) end
 
         def select name, range, default
           out = %{<select name="#{name}">\n}
---
+++ test/tc_helper_form.rb
@@ -0,0 +1,76 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
+require 'lib/test/test_helper'
+
+require 'og'
+
+class Entry
+  attr_accessor :title, String
+end
+
+Og.start :destroy => true
+
+include Ramaze
+
+class TCFormHelperController < Template::Ramaze
+  helper :form
+
+  def index
+    'FormHelper Index'
+  end
+
+  def form_with_submit
+    form Entry
+  end
+
+  def form_without_submit
+    form Entry, :submit => false
+  end
+
+  def form_with_title
+    form Entry, :title => 'Title'
+  end
+
+  def form_without_title
+    form Entry, :title => false
+  end
+
+  def form_with_oid
+    form Entry, :except => nil
+  end
+end
+
+
+ramaze(:mapping => {'/' => TCFormHelperController}) do
+  context "FormHelper" do
+    specify "testrun" do
+      get('/').should == 'FormHelper Index'
+    end
+
+    specify "with submit" do
+      get('/form_with_submit').should ==
+        %{title: <input type="text" name="title" value="" /><br />\n<input type="submit" />}
+    end
+
+    specify "without submit" do
+      get('/form_without_submit').should ==
+        %{title: <input type="text" name="title" value="" />}
+    end
+
+    specify "with title" do
+      get('/form_with_title').should ==
+        %{Title: <input type="text" name="title" value="" /><br />\n<input type="submit" />}
+    end
+
+    specify "without title" do
+      get('/form_without_title').should ==
+        %{<input type="text" name="title" value="" /><br />\n<input type="submit" />}
+    end
+
+    specify "with oid" do
+      get('/form_with_oid').should ==
+        %{title: <input type="text" name="title" value="" /><br />\noid: <input type="text" name="oid" value="0" /><br />\n<input type="submit" />}
+    end
+  end
+end
