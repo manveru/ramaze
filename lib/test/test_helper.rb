b1ee3164ae3acc055b9d3ae4e1e2bdb561274832
manveru <m.fellinger@gmail.com>
2006-12-31 22:05:00 +0000

    huge change to the test_helper... no more ramaze{} but ramaze() - this will shield us from future pain and is also a lot nicer for integration into RSpec
    darcs-hash:20061231220537-24576-1c2ffdc3d64eb0b74e29fd643a220b7b58cfa0f2.gz

--- lib/test/test_helper.rb
+++ lib/test/test_helper.rb
@@ -10,21 +10,78 @@ require 'timeout'
 require 'open-uri'
 require 'net/http'
 
-class Context
-  def initialize(url = '/')
-    @cookie = open(url).meta['set-cookie']
+# provides get/post methods for use inside your
+# Spec::Runner
+
+module StatelessContext
+  def get url = ''
+    url = "http://localhost:#{Ramaze::Global.port}" + "/#{url}".gsub(/^\/+/, '/')
+    Timeout.timeout(1) do
+      result = open(url).read.strip
+      #p url => result
+      result
+    end
   end
+
+  def post url = '', params = {}
+    url = "http://localhost:#{Ramaze::Global.port}" + "/#{url}".gsub(/^\/+/, '/')
+    uri = URI.parse(url)
+    Timeout.timeout(1) do
+      res = Net::HTTP.post_form(uri, params)
+      result = res.body.to_s.strip
+      #p res => result
+      result
+    end
+  end
+end
+
+include StatelessContext
+
+# provides a convinient Context for your requests
+# so you can simulate sessions and consecutive requests
+
+class Context
+
+  # initialize the context with an url to obtain your cookie
+
+  def initialize(url = '/')
+    @cookie_url = url
+    reset
+  end
+
+  # reset your session
+
+  def reset(url = @cookie_url)
+    @cookie = obtain_cookie(url)
+  end
+
+  # just get a cookie, doesn't reset your session
+
+  def obtain_cookie(url = @cookie_url)
+    open(url).meta['set-cookie']
+  end
+
+  # our custom little open, take into account parameters and
+  # the port the server is running on
+  # the parameters are used for the session but could use
+  # any headers you wanna use for the request
 
   def open url, hash = {}
     Kernel.open("http://localhost:#{Global.port}#{url}", hash)
   end
 
-  def get url = ''
-    open(url, 'Set-Cookie' => @cookie).read
+  # use Context#open with our cookie
+  # you can pass any header you want with an hash
+
+  def get url = '', headers = {}
+    open(url, {'Set-Cookie' => @cookie}.merge(headers)).read
   end
 
+  # Net::HTTP.post_form with the cookie
+  # params are for the POST-parameters
+
   def post url = '', params = {}
-    url = "http://localhost:#{Ramaze::Global.port}" + "/#{url}".gsub(/^\/+/, '/')
+    url = "http://localhost:#{Ramaze::Global.port}" + "/#{url}".squeeze('/')
     uri = URI.parse(url)
     params['Set-Cookie'] = @cookie
     res = Net::HTTP.post_form(uri, params)
@@ -33,13 +90,27 @@ class Context
     result
   end
 
+  # like post, but seval the returned string
+  # very comfortable if you just do {'foo' => 'bar'}.inspect in your template
+  # and in your specs you can
+  # epost('/foo').should == {'foo' => 'bar'}
+
   def epost opt = '', params = {}
     seval(post(opt, params))
   end
 
+  # like get, but seval the returned string
+  # very comfortable if you just do {'foo' => 'bar'}.inspect in your template
+  # and in your specs you can
+  # eget('/foo').should == {'foo' => 'bar'}
+
   def eget opt = ''
     seval(get(opt))
   end
+
+  # a very simple wrapper for eval that returns the
+  # error-message instead of the result of the eval
+  # in case there are errors.
 
   def seval(string)
     eval(string)
@@ -48,43 +119,23 @@ class Context
   end
 end
 
-def get url = ''
-  url = "http://localhost:#{Ramaze::Global.port}" + "/#{url}".gsub(/^\/+/, '/')
-  Timeout.timeout(1) do
-    result = open(url).read.strip
-    #p url => result
-    result
+module Spec::Runner::ContextEval::ModuleMethods
+  def ramaze_start hash = {}
+    options = {
+      :mode       => :debug,
+      :adapter    => :webrick,
+      :run_loose  => true,
+      :error_page => false,
+    }
+
+    Ramaze.start(options.merge(hash))
   end
-end
 
-def post url = '', params = {}
-  url = "http://localhost:#{Ramaze::Global.port}" + "/#{url}".gsub(/^\/+/, '/')
-  uri = URI.parse(url)
-  Timeout.timeout(1) do
-    res = Net::HTTP.post_form(uri, params)
-    result = res.body.to_s.strip
-    #p res => result
-    result
+  def ramaze_teardown
+    #Ramaze.teardown
   end
-end
 
-def ramaze_start hash = {}
-  options = {
-    :mode       => :debug,
-    :adapter    => :webrick,
-    :run_loose  => true,
-    :error_page => false,
-  }
-
-  Ramaze.start(options.merge(hash))
-end
-
-def ramaze_teardown
-  #Ramaze.shutdown
-end
-
-def ramaze(hash = {})
-  ramaze_start(hash)
-    yield if block_given?
-  ramaze_teardown
+  def ramaze(hash = {})
+    ramaze_start(hash)
+  end
 end
