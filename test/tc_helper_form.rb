0883dce414545a6ad2b25ee37b5e2483b1c47cdd
manveru <m.fellinger@gmail.com>
2007-01-02 01:03:00 +0000

    don't break this test if we got no Og... suboptimal :|
    darcs-hash:20070102010321-24576-91db6b7003d233503039b1345046846625ca2f34.gz

--- test/tc_helper_form.rb
+++ test/tc_helper_form.rb
@@ -3,74 +3,78 @@
 
 require 'lib/test/test_helper'
 
-require 'og'
+begin
+  require 'og'
 
-class Entry
-  attr_accessor :title, String
-end
+  class Entry
+    attr_accessor :title, String
+  end
 
-Og.start :destroy => true
+  Og.start :destroy => true
 
-include Ramaze
+  include Ramaze
 
-class TCFormHelperController < Template::Ramaze
-  helper :form
+  class TCFormHelperController < Template::Ramaze
+    helper :form
 
-  def index
+    def index
     'FormHelper Index'
+    end
+
+    def form_with_submit
+      form Entry
+    end
+
+    def form_without_submit
+      form Entry, :submit => false
+    end
+
+    def form_with_title
+      form Entry, :title => 'Title'
+    end
+
+    def form_without_title
+      form Entry, :title => false
+    end
+
+    def form_with_oid
+      form Entry, :except => nil
+    end
   end
 
-  def form_with_submit
-    form Entry
-  end
 
-  def form_without_submit
-    form Entry, :submit => false
-  end
+  context "FormHelper" do
+    ramaze(:mapping => {'/' => TCFormHelperController})
 
-  def form_with_title
-    form Entry, :title => 'Title'
-  end
+    specify "testrun" do
+      get('/').should == 'FormHelper Index'
+    end
 
-  def form_without_title
-    form Entry, :title => false
-  end
-
-  def form_with_oid
-    form Entry, :except => nil
-  end
-end
-
-
-context "FormHelper" do
-  ramaze(:mapping => {'/' => TCFormHelperController})
-
-  specify "testrun" do
-    get('/').should == 'FormHelper Index'
-  end
-
-  specify "with submit" do
-    get('/form_with_submit').should ==
+    specify "with submit" do
+      get('/form_with_submit').should ==
       %{title: <input type="text" name="title" value="" /><br />\n<input type="submit" />}
-  end
+    end
 
-  specify "without submit" do
-    get('/form_without_submit').should ==
+    specify "without submit" do
+      get('/form_without_submit').should ==
       %{title: <input type="text" name="title" value="" />}
-  end
+    end
 
-  specify "with title" do
-    get('/form_with_title').should ==
+    specify "with title" do
+      get('/form_with_title').should ==
       %{Title: <input type="text" name="title" value="" /><br />\n<input type="submit" />}
-  end
+    end
 
-  specify "without title" do
-    get('/form_without_title').should ==
+    specify "without title" do
+      get('/form_without_title').should ==
       %{<input type="text" name="title" value="" /><br />\n<input type="submit" />}
+    end
+
+    specify "with oid" do
+      get('/form_with_oid').should ==
+      %{title: <input type="text" name="title" value="" /><br />\noid: <input type="text" name="oid" value="0" /><br />\n<input type="submit" />}
+    end
   end
 
-  specify "with oid" do
-    get('/form_with_oid').should ==
-      %{title: <input type="text" name="title" value="" /><br />\noid: <input type="text" name="oid" value="0" /><br />\n<input type="submit" />}
-  end
+rescue LoadError
 end
