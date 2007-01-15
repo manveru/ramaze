d9302a78a408f1b987bdd78d73d91d1c4f7300e1
manveru <m.fellinger@gmail.com>
2007-01-15 12:17:00 +0000

    all_tests gives _much_ prettier and more informative output... still gotta use systemu for catching that nasty output from Og though...
    darcs-hash:20070115121742-24576-208a754006489ec99c2e4a31048723453e0e29bd.gz

--- lib/test/all_tests.rb
+++ lib/test/all_tests.rb
@@ -2,6 +2,17 @@
 # All files in this distribution are subject to the terms of the Ruby license.
 
 require 'pp'
+begin
+  require 'term/ansicolor'
+  class String
+    include Term::ANSIColor
+  end
+rescue LoadError
+  class String
+    def red() self end
+    def green() self end
+  end
+end
 
 $stdout.sync = true
 
@@ -15,10 +26,20 @@ Dir['test/**/tc_*.rb'].each do |test_case|
   out.split("\n").each do |line|
     if line =~ /(\d+) specifications?, (\d+) failures?/
       s, f = $1.to_i, $2.to_i
-      puts "[#{s.to_s.rjust(3)} specs]"
-      problematic[test_case] = out unless f == 0
+
       specs    += s
       failures += f
+
+      out = "[#{s.to_s.rjust(3)} specs | "
+
+      if f.nonzero?
+        problematic[test_case] = out
+        out << "#{f.to_s.rjust(3)} failed ]"
+        puts out.red
+      else
+        out << "all passed ]"
+        puts out.green
+      end
     end
   end
 end
@@ -32,5 +53,8 @@ problematic.each do |key, value|
   puts "-" * 80
 end
 
-puts "Total: #{specs} specifications, #{failures} failures"
-puts "failed specs: #{problematic.keys.join(', ') || 'none'}"
+puts "Total: #{specs} contexts, #{failures} failures"
+
+if problems = problematic.keys.join(', ')
+  puts "These failed: #{problems}"
+end
