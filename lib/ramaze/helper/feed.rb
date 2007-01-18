8f77191d64ccb0140307431552bf260a4c7366fb
manveru <m.fellinger@gmail.com>
2007-01-18 04:33:00 +0000

    rewrite FeedHelper, still not functional, but a lot nicer to build XML from objects
    darcs-hash:20070118043357-24576-6d19ed6d5f6e39e3c29022542dd7dcb1bed25758.gz

--- lib/ramaze/helper/feed.rb
+++ lib/ramaze/helper/feed.rb
@@ -1,16 +1,84 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
+
 module Ramaze
   module FeedHelper
-    def rss_feed(&block)
-      Feed::RSS(2.0)::Channel.new(&block)
-    end
-
-    def atom_feed(&block)
-      Feed::Atom.new(&block)
+    def feed
     end
   end
 end
+
+module ReFeed
+  def self.included(klass)
+    klass.class_eval do
+      const_set('XML_ATTRIBUTES', {})
+
+      class << self
+        def xml_accessor(*args)
+          args = xml(*args)
+          attr_accessor(*args)
+        end
+
+        def xml(*arguments)
+          args = []
+          hash = nil
+          klass = Object.const_get(self.to_s)
+
+          arguments.each do |arg|
+            if arg.respond_to?(:to_sym)
+              args << arg.to_sym
+            elsif arg.respond_to?(:to_hash)
+              hash = arg
+            end
+          end
+
+          args.each do |arg|
+            klass::XML_ATTRIBUTES[arg] = hash
+          end
+        end
+      end
+    end
+  end
+
+  def xml_attributes
+    self.class::XML_ATTRIBUTES
+  end
+
+  def to_xml
+    name = self.class.name
+    xml = xml_attributes.map do |key, opts|
+      value = send(key)
+      next unless value
+      if value.respond_to?(:to_xml)
+        value.to_xml
+      elsif value.respond_to?(:all?) and value.all?{|v| v.respond_to?(:to_xml) }
+        value.map{|v| v.to_xml }
+      else
+        "<#{key}>#{value}</#{key}>"
+      end
+    end
+    "<#{name}>#{xml}</#{name}>"
+  end
+end
+
+=begin
+module Ramaze
+  module FeedHelper
+    def feed_rss(&block)
+    end
+
+    def feed_atom(&block)
+    end
+
+    def feed(protocol = :atom, &block)
+      this = send("feed_#{protocol}", &block).to_s
+      p :feed => this
+      this
+    end
+  end
+end
+
+=begin
 
 module Feed
   class Bag
@@ -46,13 +114,13 @@ module Feed
       @tags.map{|t| yield(t)}
     end
 
-    def to_s(indent = '  ')
+    def transform(indent = '  ')
       name = self.class.name.split('::').last.downcase
       [
         "#{indent}<#{name}>",
         tags(indent),
         out << "#{indent}</#{name}>",
-      ]
+      ].join
     end
 
     def tags(indent = '  ')
@@ -61,7 +129,7 @@ module Feed
           %{#{indent}  <#{key} href="#{value}" />}
         else
           if value.is_a?(Bag)
-            value.to_s(indent + '  ')
+            value.transform(indent + '  ')
           else
             %{#{indent}  <#{key}>#{value}</#{key}>}
           end
@@ -92,20 +160,21 @@ atom =
   end
 end
 
-puts atom.to_atom
+puts atom
 =end
 
+=begin
 module Feed::Atom
   STRFTIME = "%Y-%m-%d"
 
   class AtomBag < ::Feed::Bag
-    def to_atom
+    def to_s
       [
         '<?xml version="1.0" encoding="utf-8"?>',
         '<feed xmlns="http://www.w3.org/2005/Atom">',
-        to_s('  '),
+        transform,
         '</feed>',
-      ]
+      ].join("\n")
     end
   end
 
@@ -149,17 +218,18 @@ end
 puts rss.to_rss
 =end
 
+=begin
 module Feed::RSS_2_0
   STRFTIME = "%Y-%m-%d"
 
   class RSSBag < ::Feed::Bag
-    def to_rss
+    def to_s
       [
         '<?xml version="1.0"?>',
         '<rss version="2.0">',
-        to_s('  '),
+        transform,
         '</rss>',
-      ]
+      ].join("\n")
     end
   end
 
@@ -167,3 +237,4 @@ module Feed::RSS_2_0
   class Item < RSSBag; end
   class Channel < RSSBag; end
 end
+=end
