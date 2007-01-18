e866219cebeaf4931e6d4667deace435ae44b405
manveru <m.fellinger@gmail.com>
2007-01-18 15:11:00 +0000

    ignore memcached in the tests if it's not installed
    darcs-hash:20070118151104-24576-82129a08ec44dfab90d7d769a9980454cb157c14.gz

--- test/tc_cache.rb
+++ test/tc_cache.rb
@@ -28,29 +28,6 @@ context "MemoryCache" do
   end
 end
 
-context "MemcachedCache" do
-  cache = MemcachedCache.new
-
-  specify "delete" do
-    cache.delete(:one)
-    cache.delete(:two)
-  end
-
-  specify "set keys" do
-    (cache[:one] = 'eins').should == "eins"
-    (cache[:two] = 'zwei').should == "zwei"
-  end
-
-  specify "read keys" do
-    cache[:one].should == 'eins'
-    cache[:two].should == 'zwei'
-  end
-
-  specify "values_at" do
-    cache.values_at(:one, :two).should == %w[eins zwei]
-  end
-end
-
 context "YAMLStoreCache" do
   cache = YAMLStoreCache.new
 
@@ -72,4 +49,31 @@ context "YAMLStoreCache" do
   specify "values_at" do
     cache.values_at(:one, :two).should == %w[eins zwei]
   end
+end
+
+begin
+  context "MemcachedCache" do
+    cache = MemcachedCache.new
+
+    specify "delete" do
+      cache.delete(:one)
+      cache.delete(:two)
+    end
+
+    specify "set keys" do
+      (cache[:one] = 'eins').should == "eins"
+      (cache[:two] = 'zwei').should == "zwei"
+    end
+
+    specify "read keys" do
+      cache[:one].should == 'eins'
+      cache[:two].should == 'zwei'
+    end
+
+    specify "values_at" do
+      cache.values_at(:one, :two).should == %w[eins zwei]
+    end
+  end
+rescue LoadError
+  puts "cannot run test for memcached"
 end
