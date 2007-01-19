6a341135bcbe6466cb6d6bae8834fb79da0fe973
manveru <m.fellinger@gmail.com>
2007-01-19 12:38:00 +0000

    add/remove documentation from snippets/kernel/self_method
    darcs-hash:20070119123823-24576-05bf11c0f907927c901158ad9a0429bc9328b029.gz

--- lib/ramaze/snippets/kernel/self_method.rb
+++ lib/ramaze/snippets/kernel/self_method.rb
@@ -1,64 +1,12 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
-=begin
-
-# Title
-
-Adding Kernel#self_method or Kernel#this_method
-
-
-#  Abstract
-
- We all are used to self by now, but how to fetch the current method?
- My proposal is to add a method that returns either the name or just the object of the current method itself.
-
-
-# Proposal
-
- This is a common problem, that often shows up while debugging method-calls and tracing them.
- The usual approach is to add a hack like that below, by (ab)using Kernel#caller.
- However, adding this method doesn't help the clarity of any program and is only a hack for something that self is doing already - providing a handle to itself.
-
-# Analysis
-
-# Example of usage
-
-class A
-  def a; method         end
-  def b; method.name    end
-  def c; method(:b)     end
-  def d; B.send(method.name) end
-
-  class << self
-    def a; method         end
-    def b; method.name    end
-    def c; method(:b)     end
-    def d; B.send(method.name) end
-  end
-end
-
-class B
-  def self.d; method end
-end
-
-a_class = A
-a_object = A.new
-
-[:a,:b,:c,:d].each do |m|
-  puts "A.send(:#{m})"
-  p a_class.send(m)
-  puts "A.new.send(:#{m})"
-  p a_object.send(m)
-end
-
-p method(:puts).name
-p method(:p)
-
-=end
-
 module Kernel
   alias_method :method_get, :method unless defined? method_get
+
+  # get the object of the method you are currently in or any
+  # other out of the backtrace, as long as it is in the same
+  # instance and retrievable via method_get (which is the old #method).
 
   def method(n = 0)
     return method_get(n) unless n.is_a? Integer
@@ -67,6 +15,9 @@ module Kernel
 end
 
 class Method
+
+  # name of the Method
+
   def name
     #<Method: A.d>
     inspect.gsub(/#<Method: .*?[\.#](.*?)>/, '\1')
