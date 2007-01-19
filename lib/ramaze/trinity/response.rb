d3806fcb0d14876905eb4e7b6524d779fa08b5ff
manveru <m.fellinger@gmail.com>
2007-01-19 12:06:00 +0000

    document all of trinity/response
    darcs-hash:20070119120656-24576-fd95a797c43da245775f4f468ffde5acc2f1eaf4.gz

--- lib/ramaze/trinity/response.rb
+++ lib/ramaze/trinity/response.rb
@@ -4,20 +4,25 @@
 module Ramaze
   # The Response given back to the adapter, this is the center of every request
   # made.
+  #
   # In case you do a custom response, just make sure you implement all three
-  # properties: out, code, head
-  # where head has to respond to []
+  # properties: #out, #code, #head
+  # where head has to #respond_to? #[]
+  #
   # code is the status-code ( http://en.wikipedia.org/wiki/List_of_HTTP_status_codes )
   # and out should be something very String-like
 
   class ResponseStruct < Struct
+
+    # just #inspect for this class in the format of
+    #   <Response#324543 @code => 200, @head => {'Content-Type'=>'text/html', @out.size => 234>
+
     def inspect
       "<Response##{object_id} @code => #{code}, @head => #{head.inspect}, @out.size => #{out.size}>"
     end
+    alias pretty_inspect inspect
 
-    def pretty_inspect
-      "<Response##{object_id} @code => #{code}, @head => #{head.inspect}, @out.size => #{out.size}>"
-    end
+    # same as #head['Content-Type']
 
     def content_type
       @head['Content-Type']
