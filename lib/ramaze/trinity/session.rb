dc9f7a5187adf102de88f5b5c954730431d62fd2
manveru <m.fellinger@gmail.com>
2007-01-19 12:07:00 +0000

    document all of trinity/session
    darcs-hash:20070119120714-24576-df5b3464ad970407f487353a44938f3aa71ab7b3.gz

--- lib/ramaze/trinity/session.rb
+++ lib/ramaze/trinity/session.rb
@@ -4,27 +4,51 @@
 require 'digest/sha2'
 
 module Ramaze
+
+  # Session is the object that stores all the session-information of the user.
+  # It is heavily based on cookies storing the key to the information stored
+  # on the server.
+  #
+  # It uses caching as set in Global.cache and tries to set a cookie in case
+  # there is none set yet.
+
   class Session
     SESSION_KEY = '_ramaze_session_id'
     attr_accessor :session
 
     class << self
+
+      # the current session, you can call this from everywhere with Ramaze::Session.current
+
       def current
         Thread.current[:session]
       end
     end
 
+    # pass the request-object and it will extract the session-id (which is
+    # stored in the cookie with the key of SESSION_KEY
+
     def initialize request
       @session_id = parse(request)[SESSION_KEY]
     end
+
+    # current session_id, will generate a new one based on #hash if no session
+    # is currently active
 
     def session_id
       @session_id ||= hash
     end
 
+    # the current contents of session
+
     def current
       sessions[session_id] ||= {}
     end
+
+    # all the sessions currently stored, in case there are none yet it will
+    # set the constant Ramaze::SessionCache and from then on start populating
+    # it with the sessions. SessionCache is an instance of Global.cache as
+    # well.
 
     def sessions
       silently do
@@ -34,15 +58,23 @@ module Ramaze
       SessionCache
     end
 
+    # this runs before #parse and will extract the information stored in the cookie
+    # of the client, in case there is none it will try to set one.
+    # Unfortunatly we have to go seperate paths here for mongrel and webrick,
+    # since they do not share the same API.
+
     def pre_parse request
       if Global.adapter == :webrick
-        # input looks like this: "Set-Cookie: _ramaze__session_id=fa8cc88dafcb0973b48d4d65ef57e7d3\r\n"
+        # input looks like this:
+        #   "Set-Cookie: _ramaze__session_id=fa8cc88dafcb0973b48d4d65ef57e7d3\r\n"
         cookie = request.raw_header.grep(/Set-Cookie/).first rescue ''
         cookie.to_s.gsub(/Set-Cookie: (.*?)\r\n/, '\1')
       else
         cookie = (request.http_cookie rescue request.http_set_cookie rescue '') || ''
       end
     end
+
+    # parse the cookie and extract all the variables stored in there.
 
     def parse request
       cookie = pre_parse(request)
@@ -55,9 +87,15 @@ module Ramaze
       {SESSION_KEY => hash}
     end
 
+    # tries to catch all methods and proxy them to the #current
+    # this makes it easier to do i.e. hash-manipulation directly
+    # on #session in the controller.
+
     def method_missing meth, *args, &block
       current.send(meth, *args, &block)
     end
+
+    # show the contents of the session without key/value of the cookie
 
     def inspect
       tmp = current.clone
@@ -65,9 +103,13 @@ module Ramaze
       tmp.inspect
     end
 
+    # show only the key/value of the cookie, useful for debugging.
+
     def export
       "#{SESSION_KEY}=#{session_id}"
     end
+
+    # generate an unique #hash for the current session
 
     def hash
       h = [
