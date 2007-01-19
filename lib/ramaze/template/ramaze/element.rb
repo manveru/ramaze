df2cb7610f42278a6c81dba12863dc0bc2ef5013
manveru <m.fellinger@gmail.com>
2007-01-19 12:38:00 +0000

    complete documentation of template/ramaze/element
    darcs-hash:20070119123855-24576-012ffb2a637e66553a590dc83775d50246690cb6.gz

--- lib/ramaze/template/ramaze/element.rb
+++ lib/ramaze/template/ramaze/element.rb
@@ -2,6 +2,59 @@
 # All files in this distribution are subject to the terms of the Ruby license.
 
 module Ramaze
+
+  # an Element is almost like an Controller, however, instead
+  # of connecting actions to templates it is only used in
+  # Ramaze::Template::Ramaze and can be used inside the
+  # templates of the Controller as a simple wrapper.
+  #
+  # Example:
+  #
+  # Your Element called Page:
+  #
+  #   class Page < Element
+  #     def render
+  #       %{
+  #        <html>
+  #          <h1>
+  #            #{@hash['title']}
+  #          </h1>
+  #          #{content}
+  #        </html>
+  #        }
+  #     end
+  #   end
+  #
+  # and one called SideBar
+  #
+  #   class SideBar < Element
+  #     def render
+  #       %{
+  #         <a href="http://something.com">something</a>
+  #        }
+  #      end
+  #    end
+  #
+  # and your template (any template for any action):
+  #
+  #   <Page title="Test">
+  #     <SideBar />
+  #     <p>
+  #       Hello, World!
+  #     </p>
+  #   <Page>
+  #
+  # would result in:
+  #
+  #   <html>
+  #     <h1>
+  #       Test
+  #     </h1>
+  #     <p>
+  #       Hello, World!
+  #     </p>
+  #   </html>
+
   class Element
     extend Ramaze::Helper
 
@@ -9,15 +62,32 @@ module Ramaze
 
     attr_accessor :content
 
+    # this will be called by #transform, passes along the
+    # stuff inside the tags for the element
+
     def initialize(content)
       @content = content
     end
+
+    # The method that will be called upon to render the things
+    # inside the element, you can access #content from here, which
+    # contains the contents between the tags.
+    #
+    # It should answer with a String.
 
     def render *args
       @content
     end
 
     class << self
+      # transforms all <Element> tags within the string, takes also
+      # a binding to be compatible to the transform-pipeline, won't have
+      # any use for it though.
+      #
+      # It also sets a instance-variable for you called @hash, which
+      # contains the parameters you gave the <Element> tag.
+      # See above for an example of writing and using them.
+
       def transform string = '', binding = nil
         string = string.to_s
         matches = string.scan(/<([A-Z][a-zA-Z0-9]*)(.*?)?>/)
@@ -36,7 +106,11 @@ module Ramaze
         string
       end
 
-      # very buggy and not reliable, but for my usual purposes it's good enough :)
+      # basically processes stuff like
+      #   'foo="bar" foobar="baz"'
+      # do NOT pass actual objects that cannot be simply read as a string
+      # here, the information will be lost.
+
       def demunge_passed_variables(string)
         string.scan(/\s?(.*?)="(.*?)"/).inject({}) do |hash, (key, value)|
           hash.merge key => value
