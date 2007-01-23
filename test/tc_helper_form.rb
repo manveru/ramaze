9a96cfa4ef2433aee978fcf60bdff423ed7a574b
manveru <m.fellinger@gmail.com>
2007-01-23 11:34:00 +0000

    rearrange tc_heper_form to resemble tc_morpher in regard to the require-failsafe
    darcs-hash:20070123113433-24576-4e7a8efef0ad3a0503f96132dde700d024b1ba2c.gz

--- test/tc_helper_form.rb
+++ test/tc_helper_form.rb
@@ -7,152 +7,153 @@ begin
   require 'og'
   require 'glue/timestamped'
 
-  class Entry
-    attr_accessor :title, String
+class Entry
+  attr_accessor :title, String
+end
+
+class EntryTimestamped
+  attr_accessor :title, String
+  is Timestamped
+end
+
+class EntryDated
+  attr_accessor :date, Date
+end
+
+# catches all the stuff Og sends to Logger,
+# i wished on could mute it otherwise
+
+gulp_class = Class.new do
+  def method_missing(*all)
+    p(all)
+  end
+end
+Logger.send(:class_variable_set, "@@global_logger", gulp_class.new)
+
+Og.start :destroy => true
+
+include Ramaze
+
+class TCFormHelperEntryController < Template::Ramaze
+  helper :form
+
+  def index
+    'FormHelper Entry'
   end
 
-  class EntryTimestamped
-    attr_accessor :title, String
-    is Timestamped
+  def form_with_submit
+    form Entry
   end
 
-  class EntryDated
-    attr_accessor :date, Date
+  def form_without_submit
+    form Entry, :submit => false
   end
 
-  # catches all the stuff Og sends to Logger,
-  # i wished on could mute it otherwise
-
-  gulp_class = Class.new do
-    def method_missing(*all)
-      p(all)
-    end
-  end
-  Logger.send(:class_variable_set, "@@global_logger", gulp_class.new)
-
-  Og.start :destroy => true
-
-  include Ramaze
-
-  class TCFormHelperEntryController < Template::Ramaze
-    helper :form
-
-    def index
-      'FormHelper Entry'
-    end
-
-    def form_with_submit
-      form Entry
-    end
-
-    def form_without_submit
-      form Entry, :submit => false
-    end
-
-    def form_with_title
-      form Entry, :title => 'Title'
-    end
-
-    def form_without_title
-      form Entry, :title => false
-    end
-
-    def form_with_oid
-      form Entry, :deny => nil
-    end
+  def form_with_title
+    form Entry, :title => 'Title'
   end
 
-  class TCFormHelperEntryTimestampedController < Template::Ramaze
-    helper :form
-
-    def index
-      "FormHelper EntryTimestamped"
-    end
-
-    def form_with_submit
-      form EntryTimestamped
-    end
+  def form_without_title
+    form Entry, :title => false
   end
 
-  class TCFormHelperEntryDated < Template::Ramaze
-    helper :form
+  def form_with_oid
+    form Entry, :deny => nil
+  end
+end
 
-    def index
-      "FormHelper Dated"
-    end
+class TCFormHelperEntryTimestampedController < Template::Ramaze
+  helper :form
 
-    def form_with_submit
-      form EntryDated
-    end
+  def index
+    "FormHelper EntryTimestamped"
   end
 
-  context "FormHelper" do
-    ramaze
+  def form_with_submit
+    form EntryTimestamped
+  end
+end
 
-    context "Entry" do
-      Global.mapping['/entry'] = TCFormHelperEntryController
+class TCFormHelperEntryDated < Template::Ramaze
+  helper :form
+
+  def index
+    "FormHelper Dated"
+  end
+
+  def form_with_submit
+    form EntryDated
+  end
+end
+
+context "FormHelper" do
+  ramaze
+
+  context "Entry" do
+    Global.mapping['/entry'] = TCFormHelperEntryController
+
+    specify "testrun" do
+      get('/entry/').should == 'FormHelper Entry'
+    end
+
+    specify "with submit" do
+      get('/entry/form_with_submit').should ==
+        %{title: <input type="text" name="title" value="" /><br />\n<input type="submit" />}
+    end
+
+    specify "without submit" do
+      get('/entry/form_without_submit').should ==
+        %{title: <input type="text" name="title" value="" />}
+    end
+
+    specify "with title" do
+      get('/entry/form_with_title').should ==
+        %{Title: <input type="text" name="title" value="" /><br />\n<input type="submit" />}
+    end
+
+    specify "without title" do
+      get('/entry/form_without_title').should ==
+        %{<input type="text" name="title" value="" /><br />\n<input type="submit" />}
+    end
+
+    specify "with oid" do
+      get('/entry/form_with_oid').should ==
+        %{title: <input type="text" name="title" value="" /><br />\noid: <input type="text" name="oid" value="0" /><br />\n<input type="submit" />}
+    end
+
+    context "EntryTimestamped" do
+      Global.mapping['/entry_timestamped'] = TCFormHelperEntryTimestampedController
 
       specify "testrun" do
-        get('/entry/').should == 'FormHelper Entry'
+        get('/entry_timestamped/').should == "FormHelper EntryTimestamped"
       end
 
       specify "with submit" do
-        get('/entry/form_with_submit').should ==
-          %{title: <input type="text" name="title" value="" /><br />\n<input type="submit" />}
+        get('/entry_timestamped/form_with_submit').should ==
+          "title: <input type=\"text\" name=\"title\" value=\"\" /><br />\n<input type=\"submit\" />"
+      end
+    end
+
+    context "EntryDated" do
+      Global.mapping['/entry_dated'] = TCFormHelperEntryDated
+
+      specify "testrun" do
+        get('/entry_dated').should ==
+          "FormHelper Dated"
       end
 
-      specify "without submit" do
-        get('/entry/form_without_submit').should ==
-          %{title: <input type="text" name="title" value="" />}
-      end
-
-      specify "with title" do
-        get('/entry/form_with_title').should ==
-          %{Title: <input type="text" name="title" value="" /><br />\n<input type="submit" />}
-      end
-
-      specify "without title" do
-        get('/entry/form_without_title').should ==
-          %{<input type="text" name="title" value="" /><br />\n<input type="submit" />}
-      end
-
-      specify "with oid" do
-        get('/entry/form_with_oid').should ==
-          %{title: <input type="text" name="title" value="" /><br />\noid: <input type="text" name="oid" value="0" /><br />\n<input type="submit" />}
-      end
-
-      context "EntryTimestamped" do
-        Global.mapping['/entry_timestamped'] = TCFormHelperEntryTimestampedController
-
-        specify "testrun" do
-          get('/entry_timestamped/').should == "FormHelper EntryTimestamped"
-        end
-
-        specify "with submit" do
-          get('/entry_timestamped/form_with_submit').should ==
-            "title: <input type=\"text\" name=\"title\" value=\"\" /><br />\n<input type=\"submit\" />"
-        end
-      end
-
-      context "EntryDated" do
-        Global.mapping['/entry_dated'] = TCFormHelperEntryDated
-
-        specify "testrun" do
-          get('/entry_dated').should ==
-            "FormHelper Dated"
-        end
-
-        specify "with submit" do
-          result = get('/entry_dated/form_with_submit')
-          result.should =~ /date\[day\]/
-          result.should =~ /date\[month\]/
-          result.should =~ /date\[year\]/
-          result.should =~ /<input type="submit" \/>/
-        end
+      specify "with submit" do
+        result = get('/entry_dated/form_with_submit')
+        result.should =~ /date\[day\]/
+        result.should =~ /date\[month\]/
+        result.should =~ /date\[year\]/
+        result.should =~ /<input type="submit" \/>/
       end
     end
   end
+end
 
 rescue LoadError
-  puts "skip #{__FILE__}, no Og installed"
+  puts ex
+  puts "Won't run #{__FILE__} unless you install og"
 end
