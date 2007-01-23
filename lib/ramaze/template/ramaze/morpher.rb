23ad621dc8f38e7fad24433a53766651b1fadc72
manveru <m.fellinger@gmail.com>
2007-01-23 12:09:00 +0000

    adding documentation for Morpher#transform and making the rescue a lot better.
    darcs-hash:20070123120916-24576-699c640f0a78c8549ed16d7d99fc90be252f9e9a.gz

--- lib/ramaze/template/ramaze/morpher.rb
+++ lib/ramaze/template/ramaze/morpher.rb
@@ -1,43 +1,106 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
-begin
-  require 'hpricot'
 
-  module Ramaze
-    class Morpher
-      trait :transformer => [:if, :unless, :for]
+module Ramaze
+  class Morpher
+    trait :morphs => ['if', 'unless', 'for']
 
-      def self.transform template, bound = nil
-        transformer = trait[:transformer].map{|t| t.to_s}.select do |t|
-          template.include?("#{t}=")
-        end
+    # This applies a morphing-replace for the template.
+    #
+    # To use the functionality of Morpher you will need to have hpricot
+    # installed, you will get one error in case you don't and the method
+    # will be replaced by a stub that simply returns the template.
+    #
+    # The method first checks if you use any morphers and just skips
+    # the step if you don't, this should give quite some speedup for
+    # smaller templates that don't use this functionality at all.
+    # the check works by searching the morphs with appended '='
+    # in the template. There may be a few cases where this won't work
+    # since we cannot make any assumptions on the format.
+    #
+    # If you want to turn this functionality off, either remove Morpher
+    # from:
+    #   Ramaze::Template::Ramaze.trait[:transform_pipeline]
+    # or do:
+    #   Ramaze::Morpher.trait[:morphs] = []
+    #
+    # The latter is a tad slower, but i mention the possibility in case you
+    # find good use for it.
+    #
+    # You can add your own morphers in Ramaze::Morpher.trait[:morphs]
+    #
+    # Please note that a morpher has to be converted from
+    #   <tag morph="expression">content</tag>
+    # to the format
+    #   <?r morph expression ?><tag>content</tag><?r end ?>
+    #
+    # The expression will be terminated by ; in the templating, there will be
+    # no 'do' or any parenthesis added.
+    #
+    # Since the functionality is best explained by examples, here they come.
+    #
+    # Example:
+    #
+    # if:
+    #   <div if="@name">#@name</div>
+    # morphs to:
+    #   <?r if @name ?>
+    #     <div>#@name</div>
+    #   <?r end ?>
+    #
+    # unless:
+    #   <div unless="@name">No Name</div>
+    # morphs to:
+    #   <?r unless @name ?>
+    #     <div>No Name</div>
+    #   <?r end ?>
+    #
+    # for:
+    #   <div for="name in @names">#{name}</div>
+    # morphs to:
+    #   <?r for name in @names ?>
+    #     <div>#{name}</div>
+    #   <?r end ?>
+    #
+    # TODO:
+    #   - Add pure Ruby implementation as a fallback.
 
-        return template if transformer.empty?
+    def self.transform template, bound = nil
+      morphs = trait[:morphs].map{|t| t.to_s}.select do |t|
+        template.include?("#{t}=")
+      end
 
-        hp = Hpricot(template)
-        hp.each_child do |child|
-          if child.elem?
-            transformer.each do |attribute|
-              if cond = child[attribute]
-                old = child.to_html
-                child.remove_attribute(attribute)
-                template.gsub!(old, "<?r #{attribute} #{cond} ?>#{child.to_html}<?r end ?>")
-              end
+      return template if morphs.empty?
+
+      require 'hpricot'
+
+      hp = Hpricot(template)
+      hp.each_child do |child|
+        if child.elem?
+          morphs.each do |attribute|
+            if cond = child[attribute]
+              old = child.to_html
+              child.remove_attribute(attribute)
+              template.gsub!(old, "<?r #{attribute} #{cond} ?>#{child.to_html}<?r end ?>")
             end
           end
         end
-        template
       end
-    end
-  end
-rescue LoadError => ex
-  puts ex
-  puts "Please `gem install hpricot` to get Morpher"
-  module Ramaze
-    class Morpher
-      def self.transform(template, bound = nil)
-        template
+
+      template
+
+    rescue LoadError => ex
+      error "Please install hpricot (for example via `gem install hpricot`) to get morphing"
+
+      # replace this method with a stub that only returns the template.
+
+      self.class_eval do
+        def self.transform(template, bound = nil)
+          template
+        end
       end
+
+      template
     end
   end
 end
