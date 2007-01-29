2c9b152fd7b7368ca51a78dd19bcff60ef2794c6
manveru <m.fellinger@gmail.com>
2007-01-29 14:57:00 +0000

    vast upgrade of AuthHelper
    darcs-hash:20070129145759-24576-0c27649db00dc3fae45c5f7a280716f00eb28f47.gz

--- lib/ramaze/helper/auth.rb
+++ lib/ramaze/helper/auth.rb
@@ -1,5 +1,8 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
+
+require 'digest/sha1'
+
 module Ramaze
 
   # A really, really, totally stupid way to do authentication. It has no
@@ -11,24 +14,31 @@ module Ramaze
 
   module AuthHelper
 
-    # the default password
+    # The default Element to use (if any)
 
-    PASSWORD = 'passwort'
+    AUTH_ELEMENT = 'Page'
 
     # action for login, takes a password
     # ( ?password=passwort or /login/passwort or via a form )
     # if no password given, shows a simple form to input it.
 
-    def login password = nil
-      if pass = password || request.params['password']
-        session[:logged_in] = (pass == PASSWORD)
+    def login
+      if check_auth(request['username'], request['password'])
+        p :session => session
+        session[:logged_in] = true
+        p :session => session
         redirect R(self)
       else
         %{
-          <form method="POST" action="#{R(BlogController, :login)}"
-            <input type="password" name="password" />
-            <input type="submit" />
-          </form>
+          <#{AUTH_ELEMENT}>
+            <form method="POST" action="#{R(self, :login)}"
+              <ul style="list-style:none;">
+                <li>Username: <input type="text" name="username" /></li>
+                <li>Password: <input type="password" name="password" /></li>
+                <li><input type="submit" /></li>
+              </ul>
+            </form>
+          </#{AUTH_ELEMENT}>
         }
       end
     end
@@ -37,6 +47,7 @@ module Ramaze
     # current controller.
 
     def logout
+      p :logout => session
       session.clear
       redirect R(self)
     end
@@ -52,7 +63,20 @@ module Ramaze
     # checks if the user is already logged in.
 
     def check_login
+      p :check_login => session[:logged_in]
       session[:logged_in]
+    end
+
+    def check_auth user, pass
+      authtable = {
+        'manveru' => '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8'
+      }
+      password = Digest::SHA1.hexdigest(pass.to_s)
+      p :user => user, :pass => pass, :hash => password
+      p authtable
+      p authtable[user.to_s]
+      p authtable[user.to_s] == password
+      authtable[user.to_s] == password
     end
   end
 end
