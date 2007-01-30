149ccf4e74153e9c04ac2cb951d0815dd06ef594
manveru <m.fellinger@gmail.com>
2007-01-30 16:09:00 +0000

    add a tc_error
    darcs-hash:20070130160922-24576-b261054d7a025d5397c28697255a9d5b3589105a.gz

---
+++ test/tc_error.rb
@@ -0,0 +1,23 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
+require 'lib/test/test_helper'
+
+include Ramaze
+
+class TCErrorController < Template::Ramaze
+  def index
+    self.class.name
+  end
+end
+
+context "Error" do
+  context "in dispatcher" do
+    ramaze :mapping => {'/' => TCErrorController}, :error_page => true
+
+    specify "wrong parameter" do
+      get('/').should == 'TCErrorController'
+      get('/1').should =~ /NoAction/
+    end
+  end
+end
