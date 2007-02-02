fd1d16b9329e8f9cff528cca99177a82c202a978
m.fellinger <m.fellinger@gmail.com>
2007-02-02 12:12:00 +0000

    document String#snake_case
    darcs-hash:20070202121210-40155-18d6b161ebfae091ca21e155747356821afaaead.gz

--- lib/ramaze/snippets/string/snake_case.rb
+++ lib/ramaze/snippets/string/snake_case.rb
@@ -1,6 +1,11 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 class String
+
+  # convert to snake_case from CamelCase
+  #
+  #   'FooBar'.snake_case # => 'foo_bar'
+
   def snake_case
     gsub(/\B[A-Z]/, '_\&').downcase.gsub(' ', '_')
   end
