7be5c5ebd26f5fd70d5a04540fbaac601a2268fa
m.fellinger <m.fellinger@gmail.com>
2007-02-02 11:24:00 +0000

    adding docs for OpenStruct#temp
    darcs-hash:20070202112454-40155-4f6f1fcf25ad045284d7b3ac988c6cc588dc607a.gz

--- lib/ramaze/snippets/openstruct/temp.rb
+++ lib/ramaze/snippets/openstruct/temp.rb
@@ -1,6 +1,9 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 class OpenStruct
+
+  # create a new OpenStruct and fill it with a merge of the old @table and the passed hash
+
   def temp hash
     self.class.new(@table.merge(hash))
   end
