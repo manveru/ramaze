5e76fa129550a420ae92fa0e760e260844f6e75f
m.fellinger <m.fellinger@gmail.com>
2007-02-02 12:11:00 +0000

    docu for String#camel_case
    darcs-hash:20070202121103-40155-93c6ac701e0673f967b22a9ce4f48dc3d17997fc.gz

--- lib/ramaze/snippets/string/camel_case.rb
+++ lib/ramaze/snippets/string/camel_case.rb
@@ -1,7 +1,12 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
+
 class String
   def camel_case
+
+    # simple transformation to CamelCase from snake_case
+    #   'foo_bar'.camel_case # => 'FooBar'
+
     split('_').map{|e| e.capitalize}.join
   end
 end
