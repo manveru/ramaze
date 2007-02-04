30e736e088060c27607fc976df1f9ce29afff76f
manveru <m.fellinger@gmail.com>
2007-02-04 13:50:00 +0000

    finally make ramaze --create project work
    darcs-hash:20070204135029-24576-8bf93c67ee2d8158ce780597c8783ae544a279a3.gz

--- lib/ramaze/tool/create.rb
+++ lib/ramaze/tool/create.rb
@@ -9,8 +9,39 @@ module Ramaze
     class Create
       class << self
         def create project
-          mkdir project
-          FileUtils.cp_r((::Ramaze::BASEDIR / 'proto'), project)
+          @basedir = ::Ramaze::BASEDIR / 'proto'
+          @destdir = Dir.pwd / project
+
+          puts "creating project: #{project}"
+
+          FileUtils.mkdir_p(project)
+
+          puts "copy proto to new project (#@destdir)..."
+
+          directories, files =
+            Dir[@basedir / '**' / '*'].partition{|f| File.directory?(f) }
+
+          create_dirs(*directories)
+          copy_files(*files)
+
+        end
+
+        def create_dirs(*dirs)
+          dirs.each do |dir|
+            dest = dir.gsub(@basedir, @destdir)
+
+            puts "create directory: '#{dest}'"
+            FileUtils.mkdir_p(dest)
+          end
+        end
+
+        def copy_files(*files)
+          files.each do |file|
+            dest = file.gsub(@basedir, @destdir)
+
+            puts "copy file: '#{dest}'"
+            FileUtils.cp(file, dest)
+          end
         end
       end
     end
