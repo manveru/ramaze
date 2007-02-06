c87e1a935e9d3b3c96aabf9a49afd6996140fb43
manveru <m.fellinger@gmail.com>
2007-02-06 04:50:00 +0000

    remove the require coderay in error.rb main-body, this is done dynamically and with a proper rescue in the error-method instead, therefor removing the shown dependecy in rake dependencies
    darcs-hash:20070206045029-24576-d7957104a31e0a0ca8e7e124f7b085e65c7c4f3d.gz

--- lib/ramaze/error.rb
+++ lib/ramaze/error.rb
@@ -1,11 +1,6 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
-begin
-  require 'coderay'
-rescue LoadError => ex
-end
-
 module Ramaze
   module Error
     class NoAction < StandardError; end
