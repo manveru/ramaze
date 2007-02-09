4e1fafbf4751327281eb5d03e120044000ba6af5
m.fellinger <m.fellinger@gmail.com>
2007-02-09 20:43:00 +0000

    using ramaze/store/default for proto
    darcs-hash:20070209204325-40155-def25b368d432a38257ff7cfb4471b69120f6c7b.gz

--- lib/proto/src/controller/main.rb
+++ lib/proto/src/controller/main.rb
@@ -1,5 +1,6 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
+
 class MainController < Template::Ramaze
   def index
     "Hello, World"
--- lib/proto/src/model.rb
+++ lib/proto/src/model.rb
@@ -1,5 +1,6 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
-require 'yaml/store'
 
-Db = YAML::Store.new('yaml.db')
+require 'ramaze/store/default'
+
+Db = Ramaze::Store::Default.new('yaml.db')
