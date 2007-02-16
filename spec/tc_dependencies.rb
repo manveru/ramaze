0f4c3ed74f9d85edfc7ed3a65877d8aca02ce953
manveru <m.fellinger@gmail.com>
2007-02-16 04:01:00 +0000

    add spec/tc_dependencies to make sure we don't leak any gems into the vanilla ramaze
    darcs-hash:20070216040117-24576-ea93f2432ccefdd2f57b9959fa8b9a79e0e7838b.gz

---
+++ spec/tc_dependencies.rb
@@ -0,0 +1,11 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
+require 'spec/spec_helper'
+
+context "dependencies" do
+  specify "no gems" do
+    gems = $:.grep(/gems/).reject{|g| g =~ /rspec/}
+    gems.should == []
+  end
+end
