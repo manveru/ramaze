49b26aa2786ea056cfe1fb868b33d17fb60f1a5c
m.fellinger <m.fellinger@gmail.com>
2007-03-14 06:44:00 +0000

    remove the actionview_stub from HAML, it's not needed with HAML 1.5 (make sure you upgrade though)
    darcs-hash:20070314064442-40155-62913be79152ad9cfb60e9fb4e91ccfb7b02cbb0.gz

--- lib/ramaze/template/haml.rb
+++ lib/ramaze/template/haml.rb
@@ -1,7 +1,6 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
-require 'ramaze/template/haml/actionview_stub'
 require 'haml/engine'
 
 module Ramaze::Template
