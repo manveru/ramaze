7325f951684b2e9054ec39448a2ca7704048c5b8
m.fellinger <m.fellinger@gmail.com>
2007-03-16 10:39:00 +0000

    document FlashHelper
    darcs-hash:20070316103920-40155-e334e55a228d23de21063942312cb4e6cc35398b.gz

--- lib/ramaze/helper/flash.rb
+++ lib/ramaze/helper/flash.rb
@@ -2,8 +2,30 @@
 # All files in this distribution are subject to the terms of the Ruby license.
 
 module Ramaze
+
+  # The purpose of this class is to provide an easy way of setting/retrieving
+  # from the current flash.
+  #
+  # Flash is a way to keep a temporary pairs of keys and values for the duration
+  # of two requests, the current and following.
+  #
+  # Very vague Example:
+  #
+  # On the first request, for example on registering:
+  #
+  #   flash[:error] = "You should reconsider your username, it's taken already"
+  #   redirect R(self, :register)
+  #
+  # This is the request from the redirect:
+  #
+  #   do_stuff if flash[:error]
+  #
+  # On the request after this, flash[:error] is gone.
+
   module FlashHelper
     private
+
+    # answers with Session.current.flash
 
     def flash
       Session.current.flash
