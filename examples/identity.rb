f6303979a9907c06dd256254a665c1e432a426f2
m.fellinger <m.fellinger@gmail.com>
2007-03-22 04:09:00 +0000

    add examples/identity
    darcs-hash:20070322040912-40155-6074055aeb09f18d8b35abe250b4bd21eb3e5121.gz

---
+++ examples/identity.rb
@@ -0,0 +1,21 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
+require 'ramaze'
+
+class MainController < Ramaze::Controller
+  helper :identity
+
+  def index
+    if session[:openid_identity]
+      %{
+        <h1>#{flash[:success]}</h1>
+        <p>You are logged in as #{session[:openid_identity]}</p>
+      }
+    else
+      openid_login_form
+    end
+  end
+end
+
+Ramaze.start
