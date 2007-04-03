c7899ea61d96c5e23c8cc047865810364b90ed74
m.fellinger <m.fellinger@gmail.com>
2007-04-03 06:23:00 +0000

    add further colors/styles to snippets/string/color
    darcs-hash:20070403062302-40155-cbbdd9f7c2dd1540de4a86ca6e8f0569eafb0676.gz

--- lib/ramaze/snippets/string/color.rb
+++ lib/ramaze/snippets/string/color.rb
@@ -1,9 +1,20 @@
 class String
 
   {
-    :red => 31,
-    :green => 32,
-    :yellow => 33,
+    :reset          =>  0,
+    :bold           =>  1,
+    :dark           =>  2,
+    :underline      =>  4,
+    :blink          =>  5,
+    :negative       =>  7,
+    :black          => 30,
+    :red            => 31,
+    :green          => 32,
+    :yellow         => 33,
+    :blue           => 34,
+    :magenta        => 35,
+    :cyan           => 36,
+    :white          => 37,
   }.each do |key, value|
     define_method key do
       "\e[#{value}m" + self + "\e[0m"
