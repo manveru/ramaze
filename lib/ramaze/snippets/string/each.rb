e9e083fdcc9e4b11fcf786132bf8fb9b97c12ce1
m.fellinger <m.fellinger@gmail.com>
2007-04-09 09:27:00 +0000

    better implementation of String#each (for 1.9 only)
    darcs-hash:20070409092724-40155-40941076e03e319b71f380da5d4651cf7f084324.gz

--- lib/ramaze/snippets/string/each.rb
+++ lib/ramaze/snippets/string/each.rb
@@ -2,9 +2,5 @@
 # All files in this distribution are subject to the terms of the Ruby license.
 
 class String
-  unless defined?(each)
-    def each(&block)
-      each_line(&block)
-    end
-  end
+  alias each each_line unless defined?(each)
 end
