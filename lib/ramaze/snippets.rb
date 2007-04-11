d37b1249b7bdbe9d8250f82d434bf73c8dcc3527
m.fellinger <m.fellinger@gmail.com>
2007-04-11 04:22:00 +0000

    replace the aquire with a verbose way... snippets were not added to the $:/$"
    darcs-hash:20070411042231-40155-0c84add3d9e73e963db1e43eff52934538ad9499.gz

--- lib/ramaze/snippets.rb
+++ lib/ramaze/snippets.rb
@@ -2,4 +2,10 @@
 # All files in this distribution are subject to the terms of the Ruby license.
 
 require 'ramaze/snippets/kernel/aquire'
-aquire 'ramaze/snippets/*'
+
+dir  = File.join(File.expand_path(File.dirname(__FILE__)), 'snippets')
+glob = File.join(dir, '**', '*.rb')
+
+Dir[glob].each do |snippet|
+  require snippet
+end
