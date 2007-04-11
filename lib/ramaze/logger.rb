fedba867f8dc102e898baac590443a54c54b5453
m.fellinger <m.fellinger@gmail.com>
2007-04-11 09:38:00 +0000

    fixing logger.rb by adding Informer
    darcs-hash:20070411093803-40155-faf661efdeff8547608475e9bcd3102fd42870b3.gz

--- lib/ramaze/logger.rb
+++ lib/ramaze/logger.rb
@@ -2,8 +2,9 @@
 # All files in this distribution are subject to the terms of the Ruby license.
 
 module Ramaze
-  autoload :Inform, "ramaze/logger/inform.rb"
   autoload :GlobalInformer, "ramaze/logger/global_informer.rb"
-  autoload :Analogger, "ramaze/logger/analogger.rb"
-  autoload :Syslog, "ramaze/logger/syslog.rb"
+  autoload :Analogger,      "ramaze/logger/analogger.rb"
+  autoload :Informer,       "ramaze/logger/global_informer.rb"
+  autoload :Inform,         "ramaze/logger/inform.rb"
+  autoload :Syslog,         "ramaze/logger/syslog.rb"
 end
