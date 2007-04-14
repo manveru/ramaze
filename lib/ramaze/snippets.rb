3ef3471d95e605ecabdb92ac3dfac3f4d8f27099
m.fellinger <m.fellinger@gmail.com>
2007-04-14 22:00:00 +0000

    get rid of the warning about redefinition of aquire
    darcs-hash:20070414220052-40155-79a73f4972caf591a6cbbc635cac6b9c75c8993e.gz

--- lib/ramaze/snippets.rb
+++ lib/ramaze/snippets.rb
@@ -1,11 +1,9 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
-require 'ramaze/snippets/kernel/aquire'
-
 dir  = File.join(File.expand_path(File.dirname(__FILE__)), 'snippets')
 glob = File.join(dir, '**', '*.rb')
 
 Dir[glob].each do |snippet|
-  require snippet
+  require(snippet)
 end
