dfef296934dcb16a70fe5ae12aec8b5228caddc2
m.fellinger <m.fellinger@gmail.com>
2007-04-29 10:32:00 +0000

    improving example for markaby a bit, it's not working yet though, have to talk with the markaby-folks
    darcs-hash:20070429103208-40155-c0e7e24d0474167de9e339957c8f1ad23f99decd.gz

--- examples/templates/template_markaby.rb
+++ examples/templates/template_markaby.rb
@@ -7,12 +7,17 @@ include Ramaze
 
 class MainController < Controller
   trait :engine => Template::Markaby
+  trait :map => '/'
+
+  helper :markaby
+
   def index
-    %{ #{link self.class} | #{link self.class, :internal} | #{link self.class, :external} }
+    %{ #{Rlink self.class} | #{Rlink self.class, :internal} | #{Rlink self.class, :external} }
   end
 
   def internal *args
     @args = args
+    @request = request
     mab do
       html do
         head do
@@ -21,16 +26,18 @@ class MainController < Controller
         body do
           h1 "The #{@action} Template for Markaby"
           a("Home", :href => R(:/))
-          p("Here you can pass some stuff if you like, parameters are just passed like this:") do
+          p do
+            text "Here you can pass some stuff if you like, parameters are just passed like this:"
             br
-            a("external/one", :href => R(self, @action, :one))
+            a("external/one", :href => Rs(@action, :one))
             br
-            a("external/one/two/three", :href => R(self, @action, :one, :two, :three))
+            a("external/one/two/three", :href => Rs(@action, :one, :two, :three))
             br
-            a("external/one?foo=bar", :href => R(self, @action, :one, :foo => :bar))
+            a("external/one?foo=bar", :href => Rs(@action, :one, :foo => :bar))
             br
           end
-          div "The arguments you have passed to this action are:" do
+          div do
+            text "The arguments you have passed to this action are:" 
             if @args.empty?
               "none"
             else
@@ -39,7 +46,7 @@ class MainController < Controller
               end
             end
           end
-          div request.params.inspect
+          div @request.params.inspect
         end
       end
     end.to_s
