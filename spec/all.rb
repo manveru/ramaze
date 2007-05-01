7e6da8b005980afe5ba66e08ed3a959ba38da97f
m.fellinger <m.fellinger@gmail.com>
2007-05-01 07:09:00 +0000

    update spec/all into a less hackish and easier configurable design, running all specs including examples
    darcs-hash:20070501070900-40155-e6f5faef0f966d19da76b965800565b115b11673.gz

--- spec/all.rb
+++ spec/all.rb
@@ -3,19 +3,54 @@
 
 require 'spec/helper/wrap'
 
-base = File.expand_path(File.dirname(__FILE__)/'ramaze')
+layout = {
+  'ramaze' => {
+    '.'         => '*',
+    'adapter'   => '*',
+    'helper'    => '*',
+    'inform'    => '*',
+    'request'   => '*',
+    'store'     => '*',
+    'template'  => '*',
+    },
+  'examples' => '*'
+}
 
-files = Dir[base/'*.rb']
-files += %w[helper template store inform].map{|dir| Dir[base/dir/'*.rb']}.flatten
-files.dup.each do |file|
-  dirname = base/File.basename(file, '.rb')
-  if File.directory?(dirname)
-    files.delete(file)
-    files += Dir[dirname/'*.rb']
-  else
-    file
+def gather_files(base, layout)
+  files = Set.new
+  base = File.expand_path(base)
+
+  layout.each do |key, value|
+    if value.is_a?(Hash)
+      files += gather_files(base/key, value)
+    else
+      glob = base/key/"#{value}.rb"
+      files += Dir[glob].map{|f| File.expand_path(f)}
+    end
   end
+
+  files.reject{|f| File.directory?(f)}
 end
+
+def clean_files(files, layout)
+  layout.each do |key, value|
+    if value.is_a?(Hash)
+      clean_files(files, value)
+    else
+      files.dup.each do |file|
+        name = File.basename(file, File.extname(file))
+        dir = file.gsub(File.extname(file), '')
+        if name == key and File.directory?(dir)
+          files.delete(file)
+        end
+      end
+    end
+  end
+  files
+end
+
+files = gather_files(File.dirname(__FILE__), layout)
+clean_files(files, layout)
 
 wrap = SpecWrap.new(files)
 wrap.run
