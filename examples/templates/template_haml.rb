e4f2cbc03e87009ab58a83492d8799b8722aa2bc
Michael Fellinger <m.fellinger@gmail.com>
2007-05-03 18:42:00 +0000

    remove the old template from the haml-example
    darcs-hash:20070503184247-cbc08-a15b3b344eec58c686a76ba28858af37f4442082.gz

--- examples/templates/template_haml.rb
+++ examples/templates/template_haml.rb
@@ -18,30 +18,6 @@ class MainController < Controller
     @title = "The #@action Template for Haml"
 
     %q{
-      %html
-        %head
-          %title= "Template::Haml internal"
-        %body
-          %h1= @title
-          = link( R(:/), :title => 'Home')
-          %p
-            = "Here you can pass some stuff if you like, parameters are just passed like this:"
-            %br/
-            = link( R(self, @action, :one), :title => "#@action/one")
-            %br/
-            = link( R(self, @action, :one, :two, :three), :title => "#@action/one/two/three")
-            %br/
-            = link( R(self, @action, :one, :foo => :bar), :title => "#@action/one?foo=bar")
-          %div= "The arguments you have passed to this action are:"
-            - if @args.empty?
-              = "none"
-            - else
-              -  @args.each do |arg|
-                 %span= arg
-          %div= request.params.inspect
-    }
-
-    %q{
 %html
   %head
     %title Template::Haml internal
