bcd3a1d67ed165de9e164f47dcc590fa7036446d
Michael Fellinger <m.fellinger@gmail.com>
2007-05-10 05:30:00 +0000

    intelligent logging of errors in Dispatcher::Error, omitting repeated backtraces
    darcs-hash:20070510053034-cbc08-f25e0cf6395bfca4bad883c34a1591999f7c3a5e.gz

--- lib/ramaze/dispatcher/error.rb
+++ lib/ramaze/dispatcher/error.rb
@@ -5,28 +5,37 @@ module Ramaze
   module Dispatcher
     class Error
       class << self
-        def process error
-          Inform.error(error)
-          Inform.debug("handle_error(#{error.inspect})")
-          Thread.current[:exception] = error
+        trait :last_error => nil
 
+        def process error
+          log_error(error)
+
+          Thread.current[:exception] = error
           handle_error = Dispatcher.trait[:handle_error]
 
           key = error.class.ancestors.find{|a| handle_error[a]}
           status, path = *handle_error[key || Exception]
 
-          error_in_error = error.message =~ /`#{path.split('/').last}'/
-
-          Response.current.status = status
-
-          unless error_in_error
+          unless error.message =~ %r(`#{path.split('/').last}')
+            Response.current.status = status
             return Dispatcher.dispatch_to(path) if path and Global.error_page
           end
 
           Dispatcher.build_response(error.message, status)
         rescue Object => ex
           Inform.error(ex)
-          Dispatcher.build_response(ex.message, status)
+          Dispatcher.build_response(ex.message, status || 500)
+        end
+
+        def log_error error
+          error_message = error.message
+
+          if trait[:last_error] == error_message
+            Inform.error(error_message)
+          else
+            trait[:last_error] = error_message
+            Inform.error(error)
+          end
         end
       end
     end
