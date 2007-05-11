3ffb2a2cc82dc98c710bcff84d2a9f0d34168c79
Jonathan Buch <jo@helloit.fi>
2007-05-11 11:30:00 +0000

    Update localization tool
    darcs-hash:20070511113011-21a65-816d2660e537ff838509e20b79fda823a9413b34.gz

--- lib/ramaze/tool/localize.rb
+++ lib/ramaze/tool/localize.rb
@@ -1,10 +1,8 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
-#Ramaze::Global.languages ||= {
-#  :en => { :hello => 'Hello, World!' },
-#  :de => { :hello => 'Hallo, Welt!' },
-#}
+$KCODE = 'UTF-8'
+require 'ya2yaml'
 
 class Ramaze::Tool::Localize
 
@@ -35,30 +33,27 @@ class Ramaze::Tool::Localize
 
     def call(response, options = {})
       return response unless trait[:enable]
+      return response if response.body.nil?
+      return response if response.body.respond_to?(:read)
       response.body = localize_body(response.body, options)
       response
     end
 
     def localize_body(body, options)
+      locale = session[:LOCALE] || set_session_locale
+      
       body.gsub!(trait[:regex]) do
-        localize($1)
+        localize($1, locale)
       end
+      
+      store(locale, trait[:default_language])
 
       body
     end
 
-    def localize(str)
-      locale = session[:LOCALE] || set_session_locale
-
+    def localize(str, locale)
       Ramaze::Inform.debug "localizing: #{locale} => '#{str}'"
 
-      translate(locale, str)
-    rescue ex
-      Ramaze::Inform.error(ex)
-      str
-    end
-
-    def translate(locale, str)
       trans = nil
       default_language = trait[:default_language]
       dict = dictionary
@@ -67,32 +62,28 @@ class Ramaze::Tool::Localize
         #
       elsif trans = dict[default_language][str]
         dict[locale][str] = str
-
-        store(locale) # 'long' running operation
       else
         dict[locale][str] = str
         dict[default_language][str] = str
-
-        store(locale, default_language) # 'long' running operation
       end
 
       trans || str
+    rescue Object => ex
+      Ramaze::Inform.error(ex)
+      str
     end
 
     def set_session_locale
       session[:LOCALE] = trait[:default_language]
-      accepted_languages = request.http_accept_language rescue nil
-
-      return session[:LOCALE] unless accepted_languages
+      accepted_langs = request.http_accept_language rescue 'en'
 
       mapping = trait[:mapping]
       dict = dictionary
-      accepted_languages =
-        accepted_languages.split(/[;,]/).delete_if {|l| l =~ /q=/ }
+      accepted_langs = accepted_langs.scan(/([^,;]+)(?:;q=[^,]+)?/m)[0]
 
-      accepted_languages.each do |language|
+      accepted_langs.each do |language|
         language = mapping[language] || language
-        if dict.has_key?(language)
+        if dict.key?(language)
           session[:LOCALE] = language
           break
         end
@@ -111,21 +102,27 @@ class Ramaze::Tool::Localize
       dict = trait[:dictionary] || {}
 
       locales.each do |locale|
-        dict[locale] = YAML.load_file(trait[:file] % locale)
+        begin
+          dict[locale] = YAML.load_file(trait[:file] % locale)
+        rescue Errno::ENOENT
+          dict[locale] = {}
+        end
       end
 
       trait[:dictionary] = dict
     end
 
     def store(*locales)
-      locales.uniq.each do |locale|
+      locales.uniq.compact.each do |locale|
         Ramaze::Inform.debug "saving localized to: #{trait[:file] % locale}"
-        data = YAML::dump(dictionary[locale])
+        data = dictionary[locale].ya2yaml
         file = trait[:file] % locale
         File.open(file, File::CREAT|File::TRUNC|File::WRONLY) do |fd|
           fd.write data
         end
       end
+    rescue Errno::ENOENT
+      # bleargh
     end
   end
 end
