cd165ab8e238d35152cca22ea85a6bacf16fcc1d
Michael Fellinger <m.fellinger@gmail.com>
2007-05-19 18:10:00 +0000

    don't create a new instance of the adapter for every request, we use singleton-methods only - may improve performance :)
    darcs-hash:20070519181019-cbc08-c387b7c11241c8faeb772129cba860aae82407ea.gz

--- lib/ramaze/adapter/base.rb
+++ lib/ramaze/adapter/base.rb
@@ -28,33 +28,29 @@ module Ramaze
         end
 
         def call(env)
-          new.call(env)
-        end
-      end
+          if Ramaze::Global.benchmarking
+            time = Benchmark.measure{ respond env }
+            Inform.debug("request took #{time.real}s")
+          else
+            respond env
+          end
 
-      def call(env)
-        if Ramaze::Global.benchmarking
-          time = Benchmark.measure{ respond env }
-          Inform.debug("request took #{time.real}s")
-        else
-          respond env
+          finish
         end
 
-        finish
-      end
+        def finish
+          response = Thread.current[:response]
 
-      def finish
-        response = Thread.current[:response]
-
-        response.finish
-      end
-
-      def respond env
-        request, response = Request.new(env), Response.new
-        if filter = Global.record
-          Record << request if filter[request]
+          response.finish
         end
-        Dispatcher.handle(request, response)
+
+        def respond env
+          request, response = Request.new(env), Response.new
+          if filter = Global.record
+            Record << request if filter[request]
+          end
+          Dispatcher.handle(request, response)
+        end
       end
     end
   end
