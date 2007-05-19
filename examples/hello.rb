7fb60e61a83f63be1bfb1f0ddae07dabf49fa12f
Michael Fellinger <m.fellinger@gmail.com>
2007-05-19 18:08:00 +0000

    improve examples/hello
    darcs-hash:20070519180858-cbc08-fa262c9fb89f9b20e10c47e00293e245d94f5623.gz

--- examples/hello.rb
+++ examples/hello.rb
@@ -3,18 +3,15 @@
 
 require 'ramaze'
 
-# This is named MainController to automagically map it to '/'
-#   Global.mapping['/'] = MainController
-# would do the same thing.
 # you can access it now with http://localhost:7000/
 # This should output
 # Hello, World!
 # in your browser
 
 class MainController < Ramaze::Controller
-  map '/'
-
   def index
     "Hello, World!"
   end
 end
+
+Ramaze.start
