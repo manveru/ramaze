e6cdb1f26252b403bd604405c895abd6704a34bb
Michael Fellinger <m.fellinger@gmail.com>
2007-05-20 07:13:00 +0000

    add some (c) for the controller split
    darcs-hash:20070520071333-cbc08-10dac603fa22885dbdecd5f5044a6214c99b94de.gz

--- lib/ramaze/controller/error.rb
+++ lib/ramaze/controller/error.rb
@@ -1,3 +1,6 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
 module Ramaze
   class Controller
     # The default error-page handler. you can overwrite this method
