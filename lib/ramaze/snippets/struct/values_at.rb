3a262488287811362d966090c4e2bc36e4ce62cf
Michael Fellinger <m.fellinger@gmail.com>
2007-05-21 22:03:00 +0000

    add Struct#values_at
    darcs-hash:20070521220315-cbc08-e5e8ba5d96098e99fe8929e631404c9249625d2e.gz

---
+++ lib/ramaze/snippets/struct/values_at.rb
@@ -0,0 +1,14 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
+class Struct
+
+  # Point = Struct.new(:x, :y)
+  # point = Point.new(15, 10)
+  # point.values_at(:y, :x)
+  # # => [10, 15]
+
+  def values_at(*keys)
+    keys.map{|k| self[k.to_sym] }
+  end
+end
