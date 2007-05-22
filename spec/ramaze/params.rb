088bb62d359b94cdb835a98ffbe320e4e3b801e8
Michael Fellinger <m.fellinger@gmail.com>
2007-05-22 12:14:00 +0000

    speed up params spec
    darcs-hash:20070522121402-cbc08-369971bbb3bf018eb5822554b8f363c38486c350.gz

--- spec/ramaze/params.rb
+++ spec/ramaze/params.rb
@@ -4,6 +4,8 @@
 require 'spec/helper'
 
 class TCParamsController < Ramaze::Controller
+  map '/'
+
   def index
     "index"
   end
@@ -42,94 +44,103 @@ class TCParamsController < Ramaze::Controller
 end
 
 class TCParamsController2 < Ramaze::Controller
+  map '/jo'
+
   def add(one, two = nil, three = nil)
     "#{one}:#{two}:#{three}"
   end
 end
 
 class TCParamsController3 < Ramaze::Controller
+  map '/ma'
+
   def index(*args)
     request.params['foo'].inspect
   end
 end
 
 describe "Simple Parameters" do
-  ramaze(
-    :mapping => {
-        '/' => TCParamsController,
-        '/jo' => TCParamsController2,
-        '/ma' => TCParamsController3
-      }
-  )
+  before :all do
+    ramaze
+  end
+
+  def handle(*url)
+    Ramaze::Controller.handle(*url)
+  end
 
   it "Should respond to no parameters given" do
-    get('/no_params').body.should == "no params"
+    handle('/no_params').should == "no params"
   end
 
   it "Should respond to only / with the index" do
-    get('/').body.should == "index"
+    handle('/').should == "index"
   end
 
   it "call /bar though index doesn't take params" do
-    get('/bar').status.should == 404
+    lambda{ handle('/bar') }.
+      should raise_error(Ramaze::Error::NoAction)
   end
 
   it "action that takes a single param" do
-    get('/single_param/foo').body.should == "single param (foo)"
+    handle('/single_param/foo').should == "single param (foo)"
   end
 
   it "action that takes two params" do
-    get('/double_param/foo/bar').body.should == "double param (foo, bar)"
+    handle('/double_param/foo/bar').should == "double param (foo, bar)"
   end
 
   it "action that takes two params but we give only one" do
-    get('/double_param/foo').status.should == 404
+    lambda{ handle('/double_param/foo') }.
+      should raise_error(Ramaze::Error::NoAction)
   end
 
   it "action that takes all params" do
-    get('/all_params/foo/bar/foobar').body.should == "all params (foo, bar, foobar)"
+    handle('/all_params/foo/bar/foobar').should == "all params (foo, bar, foobar)"
   end
 
   it "action that takes all params but needs at least one" do
-    get('/at_least_one/foo/bar/foobar').body.should == "at least one (foo, bar, foobar)"
+    handle('/at_least_one/foo/bar/foobar').should == "at least one (foo, bar, foobar)"
   end
 
   it "action that takes all params but needs at least one (not given here)" do
-    get('/at_least_one').status.should == 500
+    lambda{ handle('/at_least_one') }.
+      should raise_error(ArgumentError)
   end
 
   it "one default" do
-    get('/one_default').body.should == "one_default (default)"
+    handle('/one_default').should == "one_default (default)"
   end
 
   it "one default" do
-    get('/one_default/my_default').body.should == "one_default (my_default)"
+    handle('/one_default/my_default').should == "one_default (my_default)"
   end
 
   it "double underscore lookup" do
-    get('/cat1/cat11').body.should == 'cat1: cat11'
+    handle('/cat1/cat11').should == 'cat1: cat11'
   end
 
   it "double double underscore lookup" do
-    get('/cat1/cat11/cat111').body.should == 'cat1: cat11: cat111'
+    handle('/cat1/cat11/cat111').should == 'cat1: cat11: cat111'
   end
 
 
   it "jo/add should raise with 0 parameters" do
-    get('/jo/add').status.should == 500
+    lambda{ handle('/jo/add') }.
+      should raise_error(ArgumentError)
   end
 
   it "add should raise with 4 parameters" do
-    get('/jo/add/1/2/3/4').status.should == 500
+    lambda{ handle('/jo/add/1/2/3/4') }.
+      should raise_error(ArgumentError)
   end
 
   it "add should not raise with 1-3 parameters" do
-    get('/jo/add/1').body.should == '1::'
-    get('/jo/add/1/2').body.should == '1:2:'
-    get('/jo/add/1/2/3').body.should == '1:2:3'
+    handle('/jo/add/1').should == '1::'
+    handle('/jo/add/1/2').should == '1:2:'
+    handle('/jo/add/1/2/3').should == '1:2:3'
   end
 
-  it '' do
+  it 'params should have no content without params' do
     get('/ma').body.should == 'nil'
   end
 end
