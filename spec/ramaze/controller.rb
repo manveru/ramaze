dc9a86dd6fbed1a27312a8b38da92bd310bbb14f
Michael Fellinger <m.fellinger@gmail.com>
2007-05-22 12:14:00 +0000

    speed up controller spec
    darcs-hash:20070522121414-cbc08-6c8332c364d268ca05c95b27a872b3a27a75574c.gz

--- spec/ramaze/controller.rb
+++ spec/ramaze/controller.rb
@@ -28,152 +28,141 @@ class TCControllerController < Ramaze::Controller
 end
 
 describe "Controller" do
-  ramaze :error_page => false
+  def handle(*args)
+    Ramaze::Controller.handle(*args)
+  end
 
-  describe 'dry specs' do
-    describe 'pattern_for' do
-      hash = {
-        '/' => [
-          ["/", 'index', []]
-        ],
+  def resolve(*args)
+    Ramaze::Controller.resolve(*args)
+  end
 
-        '/foo' => [
-          ["/foo", 'index',[]],
-          ['/',    'foo',  []],
-          ["/",    'index', ["foo"]],
-        ],
+  def values(*url)
+    resolve(*url).last.values_at(:method, :params, :template)
+  end
 
-        '/foo/bar' => [
-          ["/foo__bar", "index",    []],
-          ["/foo/bar",  "index",    []],
+  before :all do
+    ramaze :error_page => false
+    @hash = {
+      '/' => [
+        ["/", 'index', []]
+      ],
 
-          ["/foo",      "bar",      []],
-          ["/foo",      "index",    ["bar"]],
+      '/foo' => [
+        ["/foo", 'index',[]],
+        ['/',    'foo',  []],
+        ["/",    'index', ["foo"]],
+      ],
 
-          ["/",         "foo__bar", []],
-          ["/",         "foo",      ["bar"]],
-          ["/",         "index",    ["foo", "bar"]],
-        ],
+      '/foo/bar' => [
+        ["/foo__bar", "index",    []],
+        ["/foo/bar",  "index",    []],
 
-        '/foo/bar/baz' => [
+        ["/foo",      "bar",      []],
+        ["/foo",      "index",    ["bar"]],
 
-          ['/foo__bar__baz', 'index',         []],
-          ['/foo/bar/baz',   'index',         []],
+        ["/",         "foo__bar", []],
+        ["/",         "foo",      ["bar"]],
+        ["/",         "index",    ["foo", "bar"]],
+      ],
 
-          ['/foo__bar',      'baz',           []],
-          ['/foo__bar',      'index',         ['baz']],
+      '/foo/bar/baz' => [
 
-          ['/foo/bar',       'baz',           []],
-          ['/foo/bar',       'index',         ['baz']],
+        ['/foo__bar__baz', 'index',         []],
+        ['/foo/bar/baz',   'index',         []],
 
-          ['/foo',           'bar__baz',      []],
-          ['/foo',           'bar',           ['baz']],
-          ['/foo',           'index',         ['bar', 'baz']],
+        ['/foo__bar',      'baz',           []],
+        ['/foo__bar',      'index',         ['baz']],
 
-          ['/',              'foo__bar__baz', []],
-          ['/',              'foo__bar',      ['baz']],
-          ['/',              'foo',           ['bar', 'baz']],
-          ['/',              'index',         ['foo', 'bar', 'baz']],
-        ],
+        ['/foo/bar',       'baz',           []],
+        ['/foo/bar',       'index',         ['baz']],
 
-        '/foo/bar/baz/oof' => [
-          ['/foo__bar__baz__oof', 'index',               []],
+        ['/foo',           'bar__baz',      []],
+        ['/foo',           'bar',           ['baz']],
+        ['/foo',           'index',         ['bar', 'baz']],
 
-          ['/foo/bar/baz/oof',    'index',               []],
+        ['/',              'foo__bar__baz', []],
+        ['/',              'foo__bar',      ['baz']],
+        ['/',              'foo',           ['bar', 'baz']],
+        ['/',              'index',         ['foo', 'bar', 'baz']],
+      ],
 
-          ['/foo__bar__baz',      'oof',                 []],
-          ['/foo__bar__baz',      'index',               ['oof']],
+      '/foo/bar/baz/oof' => [
+        ['/foo__bar__baz__oof', 'index',               []],
 
-          ['/foo/bar/baz',        'oof',                 []],
-          ['/foo/bar/baz',        'index',               ['oof']],
+        ['/foo/bar/baz/oof',    'index',               []],
 
-          ['/foo__bar',           'baz__oof',            []],
-          ['/foo__bar',           'baz',                 ['oof']],
-          ['/foo__bar',           'index',               ['baz', 'oof']],
+        ['/foo__bar__baz',      'oof',                 []],
+        ['/foo__bar__baz',      'index',               ['oof']],
 
-          ['/foo/bar',            'baz__oof',            []],
-          ['/foo/bar',            'baz',                 ['oof']],
-          ['/foo/bar',            'index',               ['baz', 'oof']],
+        ['/foo/bar/baz',        'oof',                 []],
+        ['/foo/bar/baz',        'index',               ['oof']],
 
-          ['/foo',                'bar__baz__oof',       []],
-          ['/foo',                'bar__baz',            ['oof']],
-          ['/foo',                'bar',                 ['baz', 'oof']],
-          ['/foo',                'index',               ['bar', 'baz', 'oof']],
+        ['/foo__bar',           'baz__oof',            []],
+        ['/foo__bar',           'baz',                 ['oof']],
+        ['/foo__bar',           'index',               ['baz', 'oof']],
 
-          ['/',                   'foo__bar__baz__oof',  []],
-          ['/',                   'foo__bar__baz',       ['oof']],
-          ['/',                   'foo__bar',            ['baz', 'oof']],
-          ['/',                   'foo',                 ['bar', 'baz', 'oof']],
-          ['/',                   'index',               ['foo', 'bar', 'baz', 'oof']],
-        ]
-      }
+        ['/foo/bar',            'baz__oof',            []],
+        ['/foo/bar',            'baz',                 ['oof']],
+        ['/foo/bar',            'index',               ['baz', 'oof']],
 
-      hash.each do |path, correct|
-        describe path do
-          patterns = Ramaze::Controller.pattern_for(path)
+        ['/foo',                'bar__baz__oof',       []],
+        ['/foo',                'bar__baz',            ['oof']],
+        ['/foo',                'bar',                 ['baz', 'oof']],
+        ['/foo',                'index',               ['bar', 'baz', 'oof']],
 
-          describe path do
-            it(path){ patterns.should == correct }
-            correct.zip(patterns).each do |(cc,cm,cp),(pc,pm,pp)|
-              {cc,pc,cm,pm,cp,pp}.each{|a,b| a.should == b}
-            end
-          end
-        end
-      end
+        ['/',                   'foo__bar__baz__oof',  []],
+        ['/',                   'foo__bar__baz',       ['oof']],
+        ['/',                   'foo__bar',            ['baz', 'oof']],
+        ['/',                   'foo',                 ['bar', 'baz', 'oof']],
+        ['/',                   'index',               ['foo', 'bar', 'baz', 'oof']],
+      ]
+    }
+  end
+
+  it "dry pattern_for" do
+    @hash.each do |path, correct|
+      patterns = Ramaze::Controller.pattern_for(path)
+      patterns.should == correct
     end
   end
 
-  describe 'resolve' do
-    def resolve(path)
-      TCControllerController.resolve(path)
-    end
+  it '/' do
+    values('/').should ==
+      ['index', [], 'spec/ramaze/template/ezamar/index.zmr']
+  end
 
-    def values(*url)
-      resolve(*url).last.values_at(:method, :params, :template)
-    end
+  it '/sum/1/2' do
+    values('/sum/1/2').should ==
+      ['sum', ['1', '2'],'spec/ramaze/template/ezamar/sum.zmr']
+  end
 
-    it '/' do
-      values('/').should ==
-        ['index', [], 'spec/ramaze/template/ezamar/index.zmr']
-    end
-
-    it '/sum/1/2' do
-      values('/sum/1/2').should ==
-        ['sum', ['1', '2'],'spec/ramaze/template/ezamar/sum.zmr']
-    end
-
-    it '/another/long/action' do
-      values('/another/long/action').should ==
-        ['another__long__action', [], 'spec/ramaze/template/ezamar/another/long/action.zmr']
-    end
-    it '/some/long/action' do
-      values('/some/long/action').should ==
-        ['some__long__action', [], 'spec/ramaze/template/ezamar/some__long__action.zmr']
-    end
+  it '/another/long/action' do
+    values('/another/long/action').should ==
+      ['another__long__action', [], 'spec/ramaze/template/ezamar/another/long/action.zmr']
+  end
+  it '/some/long/action' do
+    values('/some/long/action').should ==
+      ['some__long__action', [], 'spec/ramaze/template/ezamar/some__long__action.zmr']
   end
 
   it "simple request to index" do
-    get('/').body.should == 'Hello, World!'
+    handle('/').should == 'Hello, World!'
   end
 
   it "summing two values" do
-    get('/sum/1/2').body.should == '3'
+    handle('/sum/1/2').should == '3'
   end
 
   it "double underscore in templates" do
-    get('/some/long/action').body.should == 'some long action'
-    get('/another/long/action').body.should == 'another long action'
+    handle('/some/long/action').should == 'some long action'
+    handle('/another/long/action').should == 'another long action'
   end
 
-  describe "should not respond to private methods" do
-    TCControllerController.private_methods.sort.each do |action|
-      next if action =~ /\?$/ or action == '`'
-      it action do
-        path = "/#{action}"
-        response = get(path)
-        response.body.should =~ %r(No Action found for `#{path}' on TCControllerControlle)
-        response.status.should == 404
-      end
+  TCControllerController.private_methods.sort.each do |action|
+    it action do
+      path = "/#{action}"
+      message = "No Action found for `#{path}' on TCControllerController"
+      lambda{ resolve(path) }.should raise_error(Ramaze::Error::NoAction, message)
     end
   end
 end
