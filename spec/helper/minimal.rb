627cc2908adcb3034495f1277b150e47bb3b63a1
Michael Fellinger <m.fellinger@gmail.com>
2007-05-30 08:37:00 +0000

    make the life for people who want to run specs without RUBYOPT easier
    darcs-hash:20070530083719-cbc08-cf3e8228bdbaa0a20424aa4f9fde30fa0d095808.gz

--- spec/helper/minimal.rb
+++ spec/helper/minimal.rb
@@ -1,5 +1,10 @@
 require 'timeout'
 
+begin
+  require 'rubygems'
+rescue LoadError => ex
+end
+
 base = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
 $:.unshift base.gsub(/^#{Dir.pwd}/, '.')
 
@@ -8,13 +13,8 @@ $context_runner = false
 
 require 'ramaze'
 
-begin
-  require 'rubygems'
-rescue LoadError => ex
-end
-
 require 'spec'
-if Spec::VERSION::FULL_VERSION < "0.9.1 (r1880)"
-  puts "please update rspec >= 0.9.1"
+if Spec::VERSION::FULL_VERSION < "1.0.3 (r2035)"
+  puts "please update rspec >= 1.0.3"
   exit 1
 end
