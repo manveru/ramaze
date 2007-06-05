3bb25a1e71d4175c6d47e3b0995546fc12aab5bb
rff.rff <m.fellinger@gmail.com>
2007-06-05 10:58:00 +0000

    mapping explanation in proto
    darcs-hash:20070605105858-16231-fa48555713f0a7239947a879a450c5851d699c7f.gz

--- lib/proto/src/controller/main.rb
+++ lib/proto/src/controller/main.rb
@@ -1,12 +1,22 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
+# Default url mappings are:
+#  a controller called Main is mapped on the root of the site: /
+#  a controller called Something is mapped on: /something
+# If you want to override this, add a line like this inside the class
+#  map '/otherurl'
+# this will force the controller to be mounted on: /otherurl
+
 class MainController < Controller
+  
   # the index action is called automatically when no other action is specified
   def index
     @welcome = "Welcome to Ramaze!"
   end
+
   def notemplate
     "there is no template associated with this action"
   end
+
 end
