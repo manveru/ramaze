c48fca2cafbe139f8d430f2ed550d21f0ea059e6
Michael Fellinger <m.fellinger@gmail.com>
2007-07-26 10:09:00 +0000

    Add another spec for Action()
    darcs-hash:20070726100958-cbc08-55e0879676ae549859d2701d667b8d5975501d48.gz

--- spec/ramaze/action/render.rb
+++ spec/ramaze/action/render.rb
@@ -1,8 +1,18 @@
 require 'spec/helper'
 
 class TCActionOne < Ramaze::Controller
+  map '/'
+
   def index
     'Hello, World!'
+  end
+
+  def foo
+    Ramaze::Action(:controller => self.class, :method => :bar).render
+  end
+
+  def bar
+    "yo from bar"
   end
 end
 
@@ -14,5 +24,9 @@ describe 'Action rendering' do
   it 'should render' do
     action = Ramaze::Action(:method => :index, :controller => TCActionOne)
     action.render.should == 'Hello, World!'
+  end
+
+  it 'should render inside the controller' do
+    get('/foo').body.should == 'yo from bar'
   end
 end
