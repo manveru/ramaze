567b5813c87ecea736ee61b5819204de02873452
Michael Fellinger <m.fellinger@gmail.com>
2007-08-02 04:42:00 +0000

    Add examples/layout.rb
    darcs-hash:20070802044218-cbc08-ce58e33ad37a8848b550c1edd043804c0b2d2a7f.gz

---
+++ examples/layout.rb
@@ -0,0 +1,41 @@
+require 'ramaze'
+include Ramaze
+
+class SideBar < Ezamar::Element
+  def render
+    %{
+     <div class="sidebar">
+       <a href="http://something.com">something</a>
+     </div>
+     }
+  end
+end
+
+class MainController < Controller
+  map '/'
+  layout :page
+
+  def index
+    @title = "Test"
+    %{
+    <SideBar />
+    <p>Hello, World!</p>
+    }
+  end
+
+  def page
+    %{
+<html>
+  <head>
+    <title>examples/element</title>
+  </head>
+  <body>
+    <h1>#@title</h1>
+    #@content
+  </body>
+</html>
+    }
+  end
+end
+
+Ramaze.start
