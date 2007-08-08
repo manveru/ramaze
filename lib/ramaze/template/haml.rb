3cc0c0492f4d550d9689fc7b8768658d13053f37
Michael Fellinger <m.fellinger@gmail.com>
2007-08-08 11:12:00 +0000

    Add detailed error-message if haml 1.7.0 is installed but no activesupport.
    darcs-hash:20070808111239-cbc08-25629fd2894c46ee3f57242c286cad99d3962c99.gz

--- lib/ramaze/template/haml.rb
+++ lib/ramaze/template/haml.rb
@@ -1,6 +1,28 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
+if not ENV['FORCE_HAML'] and defined?(gem)
+  begin
+    gem 'haml', '=1.5.2'
+  rescue Gem::Exception => ex
+    match_error = /can't activate haml \(= 1\.5\.2\), already activated haml-(.*?)\]/
+    version = ex.message[match_error, 1]
+    if version == "1.7.0"
+      begin
+        require 'activesupport'
+      rescue LoadError => ex
+        puts ex,
+        "You seem to use a haml version incompatible with Ramaze.",
+        "Either make sure the version 1.5.2 of haml is installed,",
+        "or additionally install activesupport to use this version.",
+        "If you think this check is wrong, set FORCE_HAML=1 before or on",
+        "starting Ramaze."
+        exit 1
+      end
+    end
+  end
+end
+
 require 'haml/engine'
 
 module Ramaze
