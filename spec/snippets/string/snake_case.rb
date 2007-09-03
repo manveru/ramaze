f6b9986b8308dfa511375edc1cb4f059188e489f
rff.rff <m.fellinger@gmail.com>
2007-09-03 10:41:00 +0000

    spec for String#snake_case
    darcs-hash:20070903104157-16231-202012303caedb1c8b32a0c2f70d7be54fa9bb8c.gz

---
+++ spec/snippets/string/snake_case.rb
@@ -0,0 +1,17 @@
+require 'spec/helper'
+
+describe "String#snake_case" do
+  
+  it 'should snake_case a camelCase' do
+    'CamelCase'.snake_case.should == 'camel_case'
+  end
+
+  it 'should snake_case a CamelCaseLong' do
+    'CamelCaseLong'.snake_case.should == 'camel_case_long'
+  end
+
+  it 'will keep existing _' do
+    'Camel_Case'.snake_case.should == 'camel__case'
+  end
+
+end
