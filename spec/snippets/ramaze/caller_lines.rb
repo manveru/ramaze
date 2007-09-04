4d764601bd27d76693f4bc374a85d6cdbba02585
rff.rff <m.fellinger@gmail.com>
2007-09-04 12:07:00 +0000

    spec for Ramaze#caller_lines
    darcs-hash:20070904120759-16231-6ca5b1bdc1f0ff352d79d4679ba575241b1590fb.gz

---
+++ spec/snippets/ramaze/caller_lines.rb
@@ -0,0 +1,27 @@
+require 'spec/helper'
+
+describe "Ramaze#caller_info" do
+  
+  it 'should show  line numbers' do
+   res = Ramaze.caller_lines('/usr/lib/ruby/1.8/debug.rb', 122, 2) 
+   res.size.should == 5
+   res.map {|e| e[0]}.should == (120..124).to_a
+  end
+
+  it 'should show which line we asked for' do
+   res = Ramaze.caller_lines('/usr/lib/ruby/1.8/debug.rb', 122, 2) 
+   res.size.should == 5
+   res.map {|e| e[2]}.should == [false,false,true,false,false]
+  end
+
+  it 'should show the code' do
+   res = Ramaze.caller_lines(__FILE__, __LINE__, 1)
+   res.size.should == 3
+   res.map {|e| e[1].strip}.should == [
+      "it 'should show the code' do",
+      "res = Ramaze.caller_lines(__FILE__, __LINE__, 1)",
+      "res.size.should == 3"
+   ]
+  end
+
+end
