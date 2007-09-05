4474e3106f95ee997b8cb71265c1b6f8bf9e3739
rff.rff <m.fellinger@gmail.com>
2007-09-05 10:07:00 +0000

    spec for Action#to_hash
    darcs-hash:20070905100709-16231-b62338c93c5239b3ea7c788a109fada71430145a.gz

--- spec/ramaze/action/basics.rb
+++ spec/ramaze/action/basics.rb
@@ -16,4 +16,21 @@ describe 'Action() basics' do
     action.params.should == ['1', '2', ' ']
     action.method.should == 'foo'
   end
+
+  it 'should be transformable in an hash' do
+    # we need to supply a :controller because internal accessor methods 
+    # will use it to build some defaults. 
+    # TODO: in Action.new() raise on init :controller or use a default
+    hsh = Ramaze::Action(:controller=>Object).to_hash
+    hsh[:method].should == nil
+    hsh[:binding].should be_an_instance_of(Binding)
+    hsh[:controller].should == Object
+    hsh[:engine].should == Ramaze::Template::Ezamar
+    hsh[:params].should == []
+    hsh[:path].should == nil
+    hsh[:instance].should be_an_instance_of(Object)
+    hsh[:template].should == nil
+  end
+
+
 end
