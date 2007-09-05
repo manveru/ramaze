c044ff5d7dabcde64fb3da0b8591aeb20f29b30a
Michael Fellinger <m.fellinger@gmail.com>
2007-09-05 15:41:00 +0000

    Fixing directory listing and adding spec
    darcs-hash:20070905154147-cbc08-20a22b0ca00e49968d5cef126e5bb9c886494b42.gz

--- lib/ramaze/dispatcher/directory.rb
+++ lib/ramaze/dispatcher/directory.rb
@@ -13,26 +13,103 @@ module Ramaze
         # Just a forwarder to build_listing, automatticly exiting if there is
         # an error (defined by returning false in build_listing)
         def process(path)
-          return unless html = build_listing(path)
-          Dispatcher.build_response(html, Ramaze::STATUS_CODE['OK'])
+          if Global.list_directories
+            response = build_listing(path)
+            Response.current.build(*response) if response
+          end
         end
 
         # Makes a request for http://yourserver/dirlist/path and returns the
         # result. Due to this method, you can overwrite the action and create your
         # own page. See Ramaze::Controller::Directory for more.
         def build_listing(path)
-          return false unless Global.list_directories
-          dir = Global.public_root/path
+          dir = ::File.expand_path(Global.public_root/::File.expand_path(path, '/'))
 
           if ::File.directory?(dir)
-            response = Response.current
-            response['Content-Type'] = "text/html"
             Inform.debug("Serving directory listing: #{dir}")
-            response.body = Controller.resolve("/dirlist/#{dir}")
+
+            status = Ramaze::STATUS_CODE['OK']
+            header = {'Content-Type' => "text/html"}
+            body = list_for(dir)
+            [body, status, header]
           end
+        end
+
+        def list_for(path)
+          display = path.gsub(/^#{Regexp.escape(Global.public_root)}\/?/, '/')
+          wrapper =
+%(<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <title>Directory listing of #{display}</title>
+
+    <style type="text/css"> /*<![CDATA[*/ a, a:active {text-decoration: none; color: blue;} a:visited {color: #48468F;} a:hover, a:focus {text-decoration: underline; color: red;} body {background-color: #F5F5F5;} h2 {margin-bottom: 12px;} table {margin-left: 12px;} th, td { font-family: "Courier New", Courier, monospace; font-size: 10pt; text-align: left;} th { font-weight: bold; padding-right: 14px; padding-bottom: 3px;} td {padding-right: 14px;} td.s, th.s {text-align: right;} div.list { background-color: white; border-top: 1px solid #646464; border-bottom: 1px solid #646464; padding-top: 10px; padding-bottom: 14px;} div.foot { font-family: "Courier New", Courier, monospace; font-size: 10pt; color: #787878; padding-top: 4px;} /*]]>*/ </style>
+  </head>
+  <body>
+    <h2>Directory listing of #{display}</h2>
+    <div class="list">
+      <table summary="Directory Listing" cellpadding="0" cellspacing="0">
+        <thead>
+          <tr>
+            <th class="n">Name</th>
+            <th class="m">Last Modified</th>
+            <th class="s">Size</th>
+            <th class="t">Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="n"><a href="#{display/'..'}/">Parent Directory</a>/</td>
+            <td class="m">&nbsp;</td>
+            <td class="s">- &nbsp;</td>
+            <td class="t">Directory</td>
+          </tr>
+          %s
+          %s
+        </tbody>
+      </table>
+    </div>
+    <div class="foot">%s</div>
+  </body>
+</html>
+)
+
+          dirs, files = Dir[path/'*'].partition{|file| ::File.directory?(file) }
+          dir_body, file_body = [], []
+
+          dirs.each do |dir|
+            basename = ::File.basename(dir)
+            dir_body << %[<tr>
+            <td class="n"><a href="#{display/basename}">#{basename}/</a></td>
+            <td class="m">&nbsp;</td>
+            <td class="s">- &nbsp;</td>
+            <td class="t">Directory</td>
+          </tr>]
+          end
+
+          time_format = "%Y-%b-%d %H:%M:%S"
+          files.each do |file|
+            basename = ::File.basename(file)
+            time = ::File.mtime(file).strftime(time_format)
+            size = ::File.size(file)
+            mime = Tool::MIME.type_for(file)
+            file_body << %[<tr>
+            <td class="n"><a href="#{display/basename}">#{basename}</a></td>
+            <td class="m">#{time}</td>
+            <td class="s">#{size}</td>
+            <td class="t">#{mime}</td>
+          </tr>]
+          end
+
+          version = "ramaze/#{Ramaze::VERSION}"
+          wrapper % [
+            dir_body. join("\n          "),
+            file_body.join("\n          "),
+            version
+          ]
         end
       end
     end
   end
 end
-
---
+++ spec/ramaze/dispatcher/directory.rb
@@ -0,0 +1,53 @@
+require 'spec/helper'
+
+testcase_requires 'hpricot'
+
+describe 'Dispatcher::Directory' do
+  before :all do
+    ramaze :public_root => __DIR__/:public
+    @hierarchy = %w[
+    /test/deep/hierarchy/one.txt
+    /test/deep/hierarchy/two.txt
+    /test/deep/three.txt
+    /test/deep/four.txt
+    /test/five.txt
+    /test/six.txt ]
+
+    @hierarchy.each do |path|
+      FileUtils.mkdir_p(__DIR__/:public/File.dirname(path))
+      FileUtils.touch(__DIR__/:public/path)
+    end
+  end
+
+  def build_listing(path)
+    Ramaze::Dispatcher::Directory.build_listing(path)
+  end
+
+  it 'should dry serve root directory' do
+    body, status, header = build_listing('/')
+    status.should == 200
+    header['Content-Type'].should == 'text/html'
+    doc = Hpricot(body)
+    doc.at(:title).inner_text.should == 'Directory listing of /'
+    files = doc.search("//td[@class='n']")
+    links = files.map{|td| a = td.at(:a); [a['href'], a.inner_text]}
+    links.should == [["/../", "Parent Directory"], ["/test", "test/"],
+      ["/favicon.ico", "favicon.ico"], ["/test_download.css", "test_download.css"]]
+  end
+
+  it 'should serve hierarchies' do
+    body, status, header = build_listing('/test')
+    status.should == 200
+    header['Content-Type'].should == 'text/html'
+    doc = Hpricot(body)
+    doc.at(:title).inner_text.should == 'Directory listing of /test'
+    files = doc.search("//td[@class='n']")
+    links = files.map{|td| a = td.at(:a); [a['href'], a.inner_text]}
+    links.should == [["/test/../", "Parent Directory"], ["/test/deep", "deep/"],
+      ["/test/five.txt", "five.txt"], ["/test/six.txt", "six.txt"]]
+  end
+
+  after :all do
+    FileUtils.rm_rf(__DIR__/:public/:test)
+  end
+end
