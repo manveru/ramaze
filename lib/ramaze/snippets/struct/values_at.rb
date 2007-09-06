00027e7279a58956d40a1bff1b360e1ccaa29310
Michael Fellinger <m.fellinger@gmail.com>
2007-09-06 13:51:00 +0000

    Make Struct#values_at backwards-compatible but remove Symbol#to_int (deprecated by ruby1.9)
    darcs-hash:20070906135122-cbc08-4bc481eb9ad9a22e027b43e3df68a8407a3771e9.gz

--- lib/ramaze/snippets/struct/values_at.rb
+++ lib/ramaze/snippets/struct/values_at.rb
@@ -3,14 +3,25 @@
 
 # Extensions for Struct
 
+class Symbol
+  undef_method :to_int
+end
+
 class Struct
 
-  # Point = Struct.new(:x, :y)
-  # point = Point.new(15, 10)
-  # point.values_at(:y, :x)
-  # # => [10, 15]
+  # Example:
+  #  Point = Struct.new(:x, :y)
+  #  point = Point.new(15, 10)
+  #  point.values_at(:y, :x)
+  #  # => [10, 15]
+  #  point.values_at(0, 1)
+  #  # => [15, 10]
 
   def values_at(*keys)
-    keys.map{|k| self[k] }
+    if keys.all?{|key| key.respond_to?(:to_int) }
+      keys.map{|key| values[key.to_int] }
+    else
+      keys.map{|k| self[k.to_sym] }
+    end
   end
 end
