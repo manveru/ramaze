74c44bb8c21d93cda17be351dbbaa7e493590e41
Michael Fellinger <m.fellinger@gmail.com>
2007-10-21 04:50:00 +0000

    Require fileutils for Ramaze::Action::render
    darcs-hash:20071021045025-cbc08-0b9699c15114ce326e4c05870103245b64d93f03.gz

--- lib/ramaze.rb
+++ lib/ramaze.rb
@@ -1,6 +1,8 @@
 #          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
+require 'fileutils'
+
 begin
   require 'rubygems'
 rescue LoadError
