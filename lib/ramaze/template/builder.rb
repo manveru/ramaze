0f9430c36b2cd758d2bdac3309b808195c365bfc
Aman Gupta <aman@ramaze.net>
2008-01-13 22:05:00 +0000

    Add Builder template engine for generating XML
    darcs-hash:20080113220510-569a4-b7a585bb5c1b54379df568a0ef455ba31422d9bc.gz

---
+++ lib/ramaze/template/builder.rb
@@ -0,0 +1,25 @@
+require 'builder'
+
+module Ramaze
+  module Template
+
+      class Builder < Template
+        ENGINES[self] = %w[ builder rxml ]
+
+        class << self
+          def transform action
+            Response.current['Content-Type'] = "application/xml"
+            template = wrap_compile(action)
+            action.instance.instance_eval(template, action.template || __FILE__)
+          end
+
+          def compile action, template
+            "xml = ::Builder::XmlMarkup.new(:indent => 2)\n" +
+            template +
+            "\nxml.target!\n"
+          end
+        end
+      end
+
+  end
+end
---
+++ spec/ramaze/template/builder.rb
@@ -0,0 +1,55 @@
+require 'spec/helper'
+
+spec_require 'builder'
+
+class TCTemplateBuilder < Ramaze::Controller
+  template_root 'spec/ramaze/template/builder'
+  map '/'
+  engine :Builder
+
+  def external
+    @frameworks = { 'ramaze' => 'ruby',
+                    'symfony' => 'php',
+                    'django' => 'python' }
+  end
+
+  def internal
+    external
+    %q[
+      @frameworks.each do |name, lang|
+        xml.framework {|f| f.name(name); f.language(lang) }
+      end
+    ]
+  end
+end
+
+describe "Builder" do
+  behaves_like 'http'
+  ramaze
+
+  @xml = "<framework>
+            <name>symfony</name>
+            <language>php</language>
+          </framework>
+          <framework>
+            <name>ramaze</name>
+            <language>ruby</language>
+          </framework>
+          <framework>
+            <name>django</name>
+            <language>python</language>
+          </framework>"
+
+  @xml.gsub!(/\s/,'')
+
+  it "should render xml from files" do
+    r = get('/external')
+    r.status.should == 200
+    r.body.gsub(/\s/,'').should == @xml
+    r.headers['Content-Type'].should == 'application/xml'
+  end
+
+  it 'should render internal templates' do
+    get('/internal').body.gsub(/\s/,'').should == @xml
+  end
+end
