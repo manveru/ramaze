c346b8c11bc6bb89336e752ca3c09fd5e2e2249b
rff.rff <m.fellinger@gmail.com>
2008-01-13 23:05:00 +0000

    tests for if-modified-since and etags
    darcs-hash:20080113230508-16231-b5863b7f3b598c152b78ba5bdde925b4380a148c.gz

--- spec/ramaze/dispatcher/file.rb
+++ spec/ramaze/dispatcher/file.rb
@@ -21,4 +21,35 @@ describe 'Dispatcher::File' do
     end
     get('/favicon.ico').body.should == original
   end
+
+  it 'should send ETag' do
+    res = get '/test_download.css'
+    res.headers['ETag'].should.not.be == nil
+    res.headers['ETag'].length.should == 34  # "32 hash" 
+  end
+
+  it 'should send Last-Modified' do
+    res = get '/test_download.css'
+    res.headers['Last-Modified'].should.not.be == nil
+    res.headers['Last-Modified'].should == File.stat(@public_root/'test_download.css').mtime.httpdate
+  end
+
+  it 'should respect ETag with IF_NONE_MATCHES' do
+    res = get '/test_download.css'
+    etag = res.headers['ETag']
+    etag.should.not.be == nil
+    res = get '/test_download.css', :if_none_match=>etag
+    res.status.should == 304
+    res.body.should == ''
+  end
+
+  it 'should respect If-Modified' do
+    res = get '/test_download.css'
+    mtime = res.headers['Last-Modified']
+    mtime.should.not.be == nil
+    res = get '/test_download.css', :if_modified_since=>mtime
+    res.status.should == 304
+    res.body.should == ''
+  end
+
 end
