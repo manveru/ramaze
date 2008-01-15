9af0d9f16444c0910b4d766e9eb778015cae3b2d
jesusisramazing.10.pistos <m.fellinger@gmail.com>
2008-01-15 03:18:00 +0000

    Refactor of EmailHelper.
    darcs-hash:20080115031836-f2a08-080480260626250b807c32851be578c6d4ea8a0f.gz

--- lib/ramaze/contrib/email.rb
+++ lib/ramaze/contrib/email.rb
@@ -5,22 +5,22 @@
 #   require 'ramaze/contrib/email'
 #
 #   # Set the required traits:
-#   EmailHelper.trait[ :smtp_server ] = 'smtp.your-isp.com'
-#   EmailHelper.trait[ :smtp_helo_domain ] = "originating-server.com"
-#   EmailHelper.trait[ :smtp_username ] = 'username'
-#   EmailHelper.trait[ :smtp_password ] = 'password'
-#   EmailHelper.trait[ :sender_address ] = 'no-reply@your-domain.com'
+#   Ramaze::EmailHelper.trait[ :smtp_server ] = 'smtp.your-isp.com'
+#   Ramaze::EmailHelper.trait[ :smtp_helo_domain ] = "originating-server.com"
+#   Ramaze::EmailHelper.trait[ :smtp_username ] = 'username'
+#   Ramaze::EmailHelper.trait[ :smtp_password ] = 'password'
+#   Ramaze::EmailHelper.trait[ :sender_address ] = 'no-reply@your-domain.com'
 #
 #   # Optionally, set some other traits:
-#   EmailHelper.trait[ :smtp_auth_type ] = :login
-#   EmailHelper.trait[ :bcc_addresses ] = [ 'admin@your-domain.com' ]
-#   EmailHelper.trait[ :sender_full ] = 'MailBot <no-reply@your-domain.com>'
-#   EmailHelper.trait[ :id_generator ] = lambda { "<#{Time.now.to_i}@your-domain.com>" }
-#   EmailHelper.trait[ :subject_prefix ] = "[SiteName]"
+#   Ramaze::EmailHelper.trait[ :smtp_auth_type ] = :login
+#   Ramaze::EmailHelper.trait[ :bcc_addresses ] = [ 'admin@your-domain.com' ]
+#   Ramaze::EmailHelper.trait[ :sender_full ] = 'MailBot <no-reply@your-domain.com>'
+#   Ramaze::EmailHelper.trait[ :id_generator ] = lambda { "<#{Time.now.to_i}@your-domain.com>" }
+#   Ramaze::EmailHelper.trait[ :subject_prefix ] = "[SiteName]"
 #
 # To send an e-mail:
 #
-#   EmailHelper.send(
+#   Ramaze::EmailHelper.send(
 #     "foo@foobarmail.com",
 #     "Your fooness",
 #     "Hey, you are very fooey!"
@@ -28,14 +28,15 @@
 
 require 'net/smtp'
 
-class EmailHelper
+module Ramaze
+  class EmailHelper
     # Required to be set
     trait :smtp_server => 'smtp.your-isp.com'
     trait :smtp_helo_domain => 'your.helo.domain.com'
     trait :smtp_username => 'no-username-set'
     trait :smtp_password => ''
     trait :sender_address => 'no-reply@your-domain.com'
-    
+
     # Optionally set
     trait :smtp_port => 25
     trait :smtp_auth_type => :login
@@ -43,40 +44,38 @@ class EmailHelper
     trait :sender_full => nil
     trait :id_generator => lambda { "<" + Time.now.to_i.to_s + "@" + trait[ :smtp_helo_domain ] + ">" }
     trait :subject_prefix => ""
-    
-    def EmailHelper.send( recipient, subject, message )
-        sender = trait[ :sender_full ] || "#{trait[:sender_address]} <#{trait[:sender_address]}>"
-        if recipient
-            email = %{From: #{sender}
+
+    class << self
+      def send(recipient, subject, message)
+        {:recipient => recipient, :subject => subject, :message => message}.each do |k,v|
+          raise(ArgumentError, "EmailHelper error: Missing or invalid #{k}: #{v.inspect}")
+        end
+        sender = trait[:sender_full] || "#{trait[:sender_address]} <#{trait[:sender_address]}>"
+        subject = [trait[:subject_prefix], subject].join(' ').strip
+        id = trait[:id_generator].call
+        email = %{From: #{sender}
 To: <#{recipient}>
 Date: #{Time.now.rfc2822}
-Subject: #{ ( trait[ :subject_prefix ] + " " + subject ).strip }
-Message-Id: #{trait[ :id_generator ].call}
+Subject: #{subject}
+Message-Id: #{id}
 
 #{message}
-            }
-            
-            begin
-                Net::SMTP.start(
-                    trait[ :smtp_server ],
-                    trait[ :smtp_port ],
-                    trait[ :smtp_helo_domain ],
-                    trait[ :smtp_username ],
-                    trait[ :smtp_password ],
-                    trait[ :smtp_auth_type ]
-                ) do |smtp|
-                    smtp.send_message(
-                        email,
-                        trait[ :sender_address ],
-                        [ recipient ] + trait[ :bcc_addresses ]
-                    )
-                    Ramaze::Inform.info "E-mail sent to #{recipient} - '#{subject}'"
-                end
-            rescue Exception => e
-                Ramaze::Inform.error "Failed to send e-mail to #{recipient}: #{e.message}\n" + e.backtrace[ 0..9 ].join( "\n\t" )
-            end
-        else
-            Ramaze::Inform.error "EmailHelper error: Missing or invalid recipient: #{recipient.inspect}.\n" + caller[ 0..9 ].join( "\n\t" )
+}
+
+        send_smtp(email)
+      end
+
+      def send_smtp(email, recipient, subject, message)
+        options = trait.values_at(:smtp_server, :smtp_port, :smtp_helo_domain,
+                                  :smtp_username, :smtp_password, :smtp_auth_type)
+        Net::SMTP.start(*options) do |smtp|
+          smtp.send_message(email, sender_address, Array[recipient, *bcc_addresses])
+          Inform.info "E-mail sent to #{recipient} - '#{subject}'"
         end
+      rescue => e
+        Inform.error "Failed to send e-mail to #{recipient}"
+        Inform.error e
+      end
     end
+  end
 end
