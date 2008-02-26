0e7c80ba3c441cb5deb3d30103ec9b34068cbe6a
ara.t.howard <m.fellinger@gmail.com>
2008-02-26 16:20:00 +0000

    Handle informing on exceptions where ex.backtrace==nil.
    darcs-hash:20080226162000-4585f-317f4e756054c10ba8059b406a8e995e98e2734b.gz

--- lib/ramaze/inform/informing.rb
+++ lib/ramaze/inform/informing.rb
@@ -59,7 +59,7 @@ module Ramaze
 
     def error(ex)
       if ex.respond_to?(:exception)
-        message = ex.backtrace[0..Global.backtrace_size]
+        message = (ex.backtrace || [])[0..Global.backtrace_size]
         message.map{|m| m.gsub!(/^#{Regexp.escape(Dir.pwd)}/, '.') }
         message.unshift(ex.inspect)
       else
