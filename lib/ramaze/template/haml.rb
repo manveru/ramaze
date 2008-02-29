a56e978dff933890c23a56064d2dd3b70627b356
ara.t.howard <m.fellinger@gmail.com>
2008-02-29 17:15:00 +0000

    Fixes local handling for latest haml version.
    Haml no longer supports passing locals to Engine new, this patch extracts that
    info (trait[:haml_options]) and passes it in at the right moment.  It also
    extracts local vars from the passed binding and automatically passes them to
    the render call, which is the new api for passing Haml locals.  Hopefully this
    can be gutted when local support to render_template is removed but, for now,
    this just makes tests pass and supports latest Haml api.
    
    darcs-hash:20080229171550-4585f-e12a34d6c9d4d3c727f3373b711ea2893bbb2a75.gz

--- lib/ramaze/template/haml.rb
+++ lib/ramaze/template/haml.rb
@@ -19,13 +19,18 @@ module Ramaze
 
         def transform action
           haml = wrap_compile(action)
-          haml.to_html(action.instance)
+          binding = action.binding
+          lvars = eval('local_variables', binding)
+          locals = lvars.inject({}){|h,v| h.update v => eval(v, binding)}
+          haml.render(action.instance, @locals.merge(locals))
         end
 
         # Instantiates Haml::Engine with the template and haml_options trait from
         # the controller.
 
         def compile(action, template)
+          haml_options = trait[:haml_options] || {} 
+          @locals = haml_options.delete(:locals) || haml_options.delete('locals') || {} 
           ::Haml::Engine.new(template, action.controller.trait[:haml_options] || {})
         end
       end
