24b8919e8d78307a1e2b58ecbabc5c07d0cf966e
ara.t.howard <m.fellinger@gmail.com>
2008-02-29 21:14:00 +0000

    Add Binding#locals to return a hash of local vars and their values.
    darcs-hash:20080229211409-4585f-6411eb509f7b5af469075e8d0df5e469ca15b4d9.gz

---
+++ lib/ramaze/snippets/binding/locals.rb
@@ -0,0 +1,13 @@
+class Binding
+
+  # returns a hash of localvar/localvar-values from binding, useful for
+  # template engines that do not accept bindings and force passing locals via
+  # hash
+  #   usage: x = 42; p binding.locals #=> {'x'=> 42}
+  def locals
+    eval '
+      local_variables.inject({}){|h,v| h.update v => eval(v)}
+    ', self
+  end
+
+end
