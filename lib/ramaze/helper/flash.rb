3c2d446e89bcce9c285c545c1455c7e5dce9b91e
Michael Fellinger <m.fellinger@gmail.com>
2008-03-06 13:28:00 +0000

    Doc for Helper::Flash#flashbox
    darcs-hash:20080306132817-cbc08-721ab9f233d69c85bd6a9e6f9373cd8a5eb72690.gz

--- lib/ramaze/helper/flash.rb
+++ lib/ramaze/helper/flash.rb
@@ -30,6 +30,24 @@ module Ramaze
       Session.current.flash
     end
 
+    # Use in your template to display all flash messages that may be stored.
+    # For example, given you stored:
+    #
+    #   flash # => { :error => 'Pleae enter your name'
+    #                :info => 'Do you see the fnords?' }
+    #
+    # Then a flashbox would display:
+    #
+    #   <div class='flash' id='flash_error'>Please enter your name</div>
+    #   <div class='flash' id='flash_info'>Do you see the fnords?</div>
+    #
+    # This is designed to be customized permanently or per usage:
+    #
+    #   flashbox("<div class='flash_%key'>%value</div>")
+    #
+    # Where any occurrence of %key and %value will be replaced by the actual
+    # contents of each element of flash
+
     def flashbox(tag = Helper::Flash.trait[:tag])
       flash.sort.map{|(key, value)|
         tag.gsub(/%key/, key).gsub(/%value/, value)
