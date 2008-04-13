5b069e1886ec1c7cca261fd1b0f16f17096b450a
Michael Fellinger <m.fellinger@gmail.com>
2008-04-13 08:15:00 +0000

    Beautify the rest example
    darcs-hash:20080413081538-cbc08-89bc6363ae60de0bb1b6c9b80a9823eecfa73de8.gz

--- examples/rest.rb
+++ examples/rest.rb
@@ -5,25 +5,21 @@ class MainController < Ramaze::Controller
   helper :rest
 
   on_get
-  
   def index
     'Hello, World'
   end
 
   on_post
-
   def create
     p request.params
   end
 
   on_delete
-
   def delete
     'Deleted stuff'
   end
 
   on_put
-
   def insert
     'Inserted stuff'
   end
