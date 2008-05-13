4aeca5bdf4cbbb29defd8fb1eae6dc71e833ea67
Ryan Grove <ryan@wonko.com>
2008-05-12 21:48:03 -0700

    When Global.compile == true, only cache actual templates, not the output of templateless actions.

--- lib/ramaze/template.rb
+++ lib/ramaze/template.rb
@@ -60,18 +60,19 @@ module Ramaze
           action.instance.__send__(method, *action.params)
         end
 
-        # This is a wrapper to use Global.compile without even thinking about it.
-        # Don't use it if your engine is not based on a compile/eval principle.
+        # This is a wrapper to use Global.compile without even thinking about
+        # it. Don't use it if your engine is not based on a compile/eval
+        # principle.
 
         def wrap_compile(action, template = nil)
           template ||= reaction_or_file(action).to_s
           caching_compile(action, template)
         end
 
-        # If Global.compile is enabled try t find the action in the cache,
-        # otherwise give a new compiled object back
+        # If Global.compile is enabled try to find the action in the cache,
+        # otherwise give a new compiled object back.
         def caching_compile(action, template)
-          if Global.compile
+          if Global.compile && action.template
             Cache.compiled[action.relaxed_hash] ||= compile(action, template)
           else
             compile(action, template)
