fc81b0b032a983e23b4dbf9eba8735ce57f26beb
Michael Fellinger <m.fellinger@gmail.com>
2008-05-15 17:37:41 +0900

    A couple of style changes to helper/httpdigest

--- lib/ramaze/helper/httpdigest.rb
+++ lib/ramaze/helper/httpdigest.rb
@@ -4,10 +4,7 @@ require 'md5'
 module Ramaze
   module Helper
     module HttpDigest
-
-      private
-
-      def httpdigest uid, realm, &block
+      def httpdigest(uid, realm)
         session_opaque = "authentication_digest_opaque_#{uid}"
         session_nonce = "authentication_digest_nonce"
 
@@ -16,30 +13,32 @@ module Ramaze
         authorized = false
 
         if session[session_nonce] and request.env['HTTP_AUTHORIZATION']
-          
+
           auth_split = request.env['HTTP_AUTHORIZATION'].split
           authentication_type = auth_split[0]
           authorization = Rack::Auth::Digest::Params.parse( auth_split[1..-1].join(' ') )
+          response, username, nonce, nc, cnonce, qop =
+            authorization.values_at(*%w[response username nonce nc cnonce qop])
 
           if authentication_type == 'Digest'
+            if nonce == session[session_nonce]
+              ha1 = yield(username)
+              ha2 = MD5.hexdigest("#{request.request_method}:#{request.fullpath}")
+              md5 = MD5.hexdigest([ha1, nonce, nc, cnonce, qop, ha2].join(':'))
 
-            if authorization["nonce"] == session[session_nonce]
-
-              ha1 = block.call( authorization["username"] )
-              ha2 = MD5.new( "#{request.request_method}:#{request.fullpath}" )
-
-              authorized = ( authorization["response"] == MD5.new( "#{ha1}:#{authorization["nonce"]}:#{authorization["nc"]}:#{authorization["cnonce"]}:#{authorization["qop"]}:#{ha2}" ).to_s )
-
+              authorized = response == md5
             end
-
           end
-
         end
 
         unless authorized
           session[session_nonce] = UUID.new
-          response['WWW-Authenticate'] = %|Digest realm="#{realm}",qop="auth,auth-int",nonce="#{session[session_nonce]}",opaque="#{session[session_opaque]}"|
-          respond 'Unauthorized', 401
+          response['WWW-Authenticate'] =
+            %|Digest realm="#{realm}",| +
+            %|qop="auth,auth-int",| +
+            %|nonce="#{session[session_nonce]}",| +
+            %|opaque="#{session[session_opage]}"|
+          respond('Unauthorized', 401)
         end
 
         authorization["username"]
