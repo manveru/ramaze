0638d0f10ce43c8e6b474fc4f62ae063010949bc
Michael Fellinger <m.fellinger@gmail.com>
2008-05-23 19:02:31 +0900

    Builder spec shouldn't rely on hash order

--- spec/ramaze/template/builder.rb
+++ spec/ramaze/template/builder.rb
@@ -1,6 +1,11 @@
 require 'spec/helper'
 
-spec_require 'builder'
+spec_require 'builder', 'hpricot'
+
+FRAMEWORKS = {
+  'ramaze' => 'ruby',
+  'symfony' => 'php',
+  'django' => 'python' }
 
 class TCTemplateBuilder < Ramaze::Controller
   view_root 'spec/ramaze/template/builder'
@@ -8,9 +13,7 @@ class TCTemplateBuilder < Ramaze::Controller
   engine :Builder
 
   def external
-    @frameworks = { 'ramaze' => 'ruby',
-                    'symfony' => 'php',
-                    'django' => 'python' }
+    @frameworks = FRAMEWORKS
   end
 
   def internal
@@ -27,29 +30,22 @@ describe "Builder" do
   behaves_like 'http'
   ramaze
 
-  @xml = "<framework>
-            <name>symfony</name>
-            <language>php</language>
-          </framework>
-          <framework>
-            <name>ramaze</name>
-            <language>ruby</language>
-          </framework>
-          <framework>
-            <name>django</name>
-            <language>python</language>
-          </framework>"
-
-  @xml.gsub!(/\s/,'')
+  def check(string)
+    doc = Hpricot(string)
+    got = (doc/:framework).map{|f|
+      [ f.at(:name).inner_text,
+        f.at(:language).inner_text ] }
+    Hash[*got.flatten].should == FRAMEWORKS
+  end
 
   it "should render xml from files" do
     r = get('/external')
     r.status.should == 200
-    r.body.gsub(/\s/,'').should == @xml
     r.headers['Content-Type'].should == 'application/xml'
+    check(r.body)
   end
 
   it 'should render internal templates' do
-    get('/internal').body.gsub(/\s/,'').should == @xml
+    check get('/internal').body
   end
 end
