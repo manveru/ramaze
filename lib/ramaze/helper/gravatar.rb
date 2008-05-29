66931e1c111715f26dd324ec0a92d67fac773736
Michael Fellinger <m.fellinger@gmail.com>
2008-05-29 23:11:14 +0900

    Fix fallback and escaping for Helper::Gravatar

--- lib/ramaze/helper/gravatar.rb
+++ lib/ramaze/helper/gravatar.rb
@@ -4,9 +4,9 @@ module Ramaze
       def gravatar(email, size = 32, fallback_path = "/images/gravatar_default.jpg")
         emailhash = Digest::MD5.hexdigest(email)
 
-        fallback = request.domain
+        fallback = Request.current.domain
         fallback.path = fallback_path
-        default = CGI.escape(fallback.to_s)
+        default = h(fallback.to_s)
 
         return "http://www.gravatar.com/avatar.php?gravatar_id=#{emailhash}&default=#{default}&size=#{size}"
       end
