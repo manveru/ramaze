dfd9501296ec96c21cc78bcfe918c8a106b58307
Michael Fellinger <m.fellinger@gmail.com>
2008-06-01 16:15:39 +0900

    Adding css_for/js_for in Helper::XHTML

--- lib/ramaze/helper/xhtml.rb
+++ lib/ramaze/helper/xhtml.rb
@@ -8,8 +8,16 @@ module Ramaze::Helper
       end
     end
 
+    def css_for(*names)
+      names.flatten.map{|name| css(name) }.join("\n")
+    end
+
     def js(name)
       "<script src='/js/#{name}.js' type='text/javascript'></script>"
+    end
+
+    def js_for(*names)
+      names.flatten.map{|name| js(name) }.join("\n")
     end
   end
 end
