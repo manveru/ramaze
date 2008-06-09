6a2c0bac45ccf6dec30eb984ecffba91dc1b24d3
Riku Raisaenen <riku@helloit.fi>
2008-06-09 21:23:54 +0300

    examples/sequel_scaffolding.rb: Added MainController for guidance. Reloading no longer tries to create database twice.

--- examples/misc/sequel_scaffolding.rb
+++ examples/misc/sequel_scaffolding.rb
@@ -12,13 +12,23 @@ class User < Sequel::Model(:user)
     text :description
   end
 
-  create_table
+  create_table unless User.table_exists?
 end
 
 ScaffoldingExtensions.all_models = [User]
 
 class UserController < Ramaze::Controller
   scaffold_all_models :only => [User]
+end
+
+class MainController < Ramaze::Controller
+  def index
+    %{Scaffolding extension enabled for
+      <a href="http://sequel.rubyforge.org/classes/Sequel/Model.html">
+      Sequel::Model
+      </a> User.
+      You can access the scaffolded Model at #{A('/user')}}
+  end
 end
 
 Ramaze.start
