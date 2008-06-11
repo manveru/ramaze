0513aa249da17b7ae2d6a5569c4d08131b7fe262
Michael Fellinger <m.fellinger@gmail.com>
2008-06-11 16:54:16 +0900

    Version 2008.06

--- lib/ramaze/version.rb
+++ lib/ramaze/version.rb
@@ -1,14 +1,6 @@
 #          Copyright (c) 2008 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
-module Ramaze #:nodoc:
-  module Version #:nodoc:
-    MAJOR = 0
-    MINOR = 3
-    TINY  = 9.5
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-
-  VERSION = Version::STRING
+module Ramaze
+  VERSION = "2008.06"
 end
