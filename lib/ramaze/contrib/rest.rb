e0b938be4169c4343c297e57c1485f5c56449071
Clinton R. Nixon <clinton.nixon@viget.com>
2008-06-13 15:45:05 -0400

    Added ability to fake out PUT and DELETE through POST and a hidden 'method' variable in contrib/rest.rb

--- lib/ramaze/contrib/rest.rb
+++ lib/ramaze/contrib/rest.rb
@@ -1,14 +1,17 @@
-Ramaze::Route['REST dispatch'] = lambda{|path, request|
-  case request.request_method
-  when 'GET'
-    path << 'show/'
-  when 'POST'
-    path << 'create/'
-  when 'PUT'
-    path << 'update/'
-  when 'DELETE'
-    path << 'destroy/'
-  else
-    path
+Ramaze::Route['REST dispatch'] = lambda do |path, request|
+  path << '/' unless path[-1] == '/'
+
+  method = if request.request_method == 'POST' and request.params.has_key?('method')
+             request.params['method'].upcase
+           else
+             request.request_method
+           end
+  
+  case method
+  when 'GET' then path << 'show/'
+  when 'POST' then path << 'create/'
+  when 'PUT' then path << 'update/'
+  when 'DELETE' then path << 'destroy/'
+  else path
   end
-}
+end
