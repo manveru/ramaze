27ec88093abf759015551de275c0c9d191c9a960
Michael Fellinger <m.fellinger@gmail.com>
2008-08-27 23:45:41 +0900

    Remove check from Symbol#to_proc

--- lib/ramaze/snippets/symbol/to_proc.rb
+++ lib/ramaze/snippets/symbol/to_proc.rb
@@ -7,22 +7,20 @@ module Ramaze
     # Extensions for Symbol
 
     module Symbol
-      unless :to_proc.respond_to?(:to_proc)
 
-        # Turns the symbol into a simple proc, which is especially useful for enumerations. Examples:
-        #
-        #   # The same as people.collect { |p| p.name }
-        #   people.collect(&:name)
-        #
-        #   # The same as people.select { |p| p.manager? }.collect { |p| p.salary }
-        #   people.select(&:manager?).collect(&:salary)
-        #
-        #   [1, 2, 3].map(&:to_s)    # => ['1', '2', '3']
-        #   %w[a b c].map(&:to_sym)  # => [:a, :b, :c]
+      # Turns the symbol into a simple proc, which is especially useful for enumerations. Examples:
+      #
+      #   # The same as people.collect { |p| p.name }
+      #   people.collect(&:name)
+      #
+      #   # The same as people.select { |p| p.manager? }.collect { |p| p.salary }
+      #   people.select(&:manager?).collect(&:salary)
+      #
+      #   [1, 2, 3].map(&:to_s)    # => ['1', '2', '3']
+      #   %w[a b c].map(&:to_sym)  # => [:a, :b, :c]
 
-        def to_proc
-          Proc.new{|*args| args.shift.__send__(self, *args) }
-        end
+      def to_proc
+        Proc.new{|*args| args.shift.__send__(self, *args) }
       end
     end
 
