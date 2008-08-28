5e843f1239d70762c5b9edfe2a562f6c7575bbe7
Michael Fellinger <m.fellinger@gmail.com>
2008-08-28 23:26:03 +0900

    Make Rake::Deflater not a default middleware

--- lib/ramaze/adapter/base.rb
+++ lib/ramaze/adapter/base.rb
@@ -8,7 +8,7 @@ module Ramaze
     MIDDLEWARE = OrderedSet[
       Rack::ShowExceptions,
       Rack::ShowStatus,
-      Rack::Deflater,
+      # Rack::Deflater,
       Ramaze::Reloader,
       Ramaze::Current,
       Ramaze::Dispatcher,
