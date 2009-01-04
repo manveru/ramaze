ef8a665b144d1c9ff68a0463e3f7026c7e6cd8ca
Michael Fellinger <m.fellinger@gmail.com>
2009-01-04 12:25:07 +0900

    First rough cut of Ramaze on Innate

--- lib/ramaze.rb
+++ lib/ramaze.rb
@@ -1,6 +1,46 @@
 #          Copyright (c) 2008 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
+# Namespace for Ramaze
+#
+# THINK:
+#   * for now, we don't extend this with Innate to keep things clean. But we
+#     should eventually do it for a simple API, or people always have to find
+#     out whether something is in Innate or Ramaze.
+#     No matter which way we go, we should keep references point to the
+#     original location to avoid too much confusion for core developers.
+module Ramaze
+  ROOT = File.dirname(File.expand_path(__FILE__))
+
+  unless $LOAD_PATH.any?{|lp| File.expand_path(lp) == ROOT }
+    $LOAD_PATH.unshift(ROOT)
+  end
+end
+
+begin
+  require 'rubygems'
+rescue LoadError
+end
+
+# dependencies
+require 'innate'
+
+# stdlib
+
+# bootstrap
+require 'ramaze/version'
+require 'ramaze/snippets'
+require 'ramaze/helper'
+require 'ramaze/controller'
+
+module Ramaze
+  def self.start(*args)
+    Innate.start(*args)
+  end
+end
+
+__END__
+
 # StdLib
 require 'abbrev'
 require 'cgi'
--- lib/ramaze/controller.rb
+++ lib/ramaze/controller.rb
@@ -1,6 +1,28 @@
 #          Copyright (c) 2008 Michael Fellinger m.fellinger@gmail.com
 # All files in this distribution are subject to the terms of the Ruby license.
 
+module Ramaze
+  class Controller
+    include Innate::Node
+
+    # Assign default mapping
+
+    def self.inherited(into)
+      into.map_smart
+      into.provide(:html => :erb)
+    end
+
+    def self.map_smart
+      name = self.to_s.gsub('Controller', '').gsub('::', '/')
+      return if name.empty?
+      mapping = name == 'Main' ? '/' : "/#{name.snake_case}"
+      map(mapping)
+    end
+  end
+end
+
+__END__
+
 require 'ramaze/helper'
 require 'ramaze/template'
 require 'ramaze/action'
