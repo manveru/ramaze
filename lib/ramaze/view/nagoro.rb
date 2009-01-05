42d7c03ff04f84f56065fe6a7fd96b9c5e53feed
Michael Fellinger <m.fellinger@gmail.com>
2009-01-05 22:04:02 +0900

    Add Nagoro::Pipe::RenderPartial and make pipes optional

--- lib/ramaze/view/nagoro.rb
+++ lib/ramaze/view/nagoro.rb
@@ -1,17 +1,53 @@
 require 'nagoro'
+require 'ramaze/helper/partial'
 
 module Ramaze
   module View
     module Nagoro
+      DEFAULT_PIPES = [
+        :Element, :Morph, :Include, :RenderPartial, :Instruction, :Compile
+      ]
+
+      OPTIONS = { :pipes => DEFAULT_PIPES }
+
       def self.render(action, string = nil)
         string ||= action.view
 
-        # action.options[:pipes] ||= []
+        action.options[:pipes] ||= OPTIONS[:pipes]
         action.options[:filename] ||= action.view
         action.options[:binding] = action.binding
         action.options[:variables] = action.variables
 
         ::Nagoro.render(string.to_s, action.options)
+      end
+    end
+  end
+end
+
+module Nagoro
+  module Pipe
+    # Pipe that transforms <render /> tags.
+    #
+    # the src parameter in the render tag will be used as first parameter to
+    # render_partial, all other paramters are passed on as +options+.
+    #
+    # Example calling render_partial('/hello'):
+    #   <render src="/hello" />
+    #
+    # Example calling render_partial('/hello', 'tail' => 'foo'):
+    #   <render src="/hello" tail="foo" />
+    #
+    class RenderPartial < Base
+      include Ramaze::Helper::Partial
+
+      def tag_start(tag, attrs)
+        if tag == 'render' and src = attrs.delete('src')
+          append(render_partial(src, attrs))
+        end
+      end
+
+      def tag_end(tag)
+        super unless tag == 'render'
       end
     end
   end
