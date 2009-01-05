a9543a92edba8e67f1947f866a042707e272c97a
Michael Fellinger <m.fellinger@gmail.com>
2009-01-05 21:10:54 +0900

    adapt basic/linking example

--- examples/basic/linking.rb
+++ examples/basic/linking.rb
@@ -5,24 +5,24 @@ class LinkingController < Ramaze::Controller
   map '/'
 
   def index
-    %{simple link <br/> <a href="#{Rs(:help)}">Help?</a>}
+    %{simple link <br/> <a href="#{r(:help)}">Help?</a>}
   end
-  
+
   def new
     "something new!"
   end
-  
+
   def help
-    %{you have help <br/> <a href="#{R(LinkToController, :another)}">A Different Controller</a>}
+    %{you have help <br/> <a href="#{LinkToController.r(:another)}">A Different Controller</a>}
   end
 
 end
 
 class LinkToController < Ramaze::Controller
   map '/link_to'
-  
+
   def another
-    %{<a href="#{R(LinkingController, :index)}">Back to Original Controller</a>}
+    %{<a href="#{LinkingController.r(:index)}">Back to Original Controller</a>}
   end
 end
 
