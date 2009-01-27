d90bf2a053743c71f00f23ebe6f3f1414c309be3
Michael Fellinger <m.fellinger@gmail.com>
2009-01-27 22:43:57 +0900

    Better mapping of controllers

--- lib/ramaze/controller.rb
+++ lib/ramaze/controller.rb
@@ -5,18 +5,36 @@ module Ramaze
   class Controller
     include Innate::Node
 
-    # Assign default mapping
+    # we are no mapped node
+    Innate::Node::LIST.delete(self)
+
+    LIST = Set.new
 
     def self.inherited(into)
-      into.map_smart
-      into.provide(:html => :erb)
+      Innate::Node.included(into)
+      LIST << into
     end
 
-    def self.map_smart
-      name = self.to_s.gsub('Controller', '').gsub('::', '/')
+    def self.setup
+      LIST.each{|controller| controller.mapping }
+    end
+
+    # if trait[:automap] is set and controller is not in Global.mapping yet
+    # this will build a new default mapping-point, MainController is put
+    # at '/' by default. For other Class names, String#snake_case is called,
+    # e.g. FooBarController is mapped at '/foo_bar'.
+
+    def self.mapping
+      existing_mapping = Innate.to(self)
+      return existing_mapping if existing_mapping
+
+      automap if ancestral_trait[:automap] && self.to_s !~ /#<Class:/
+    end
+
+    def automap
+      name = self.to_s.gsub('Controller', '').gsub('::', '/').clone
       return if name.empty?
-      mapping = name == 'Main' ? '/' : "/#{name.snake_case}"
-      map(mapping)
+      name == 'Main' ? '/' : "/#{name.snake_case}"
     end
 
     def self.engine(name)
