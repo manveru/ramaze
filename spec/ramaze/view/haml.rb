0c9950fcf431eda81f341998b32b33db7234cfb9
Michael Fellinger <m.fellinger@gmail.com>
2009-01-28 22:38:19 +0900

    Haml spec should look like the others

--- spec/ramaze/view/haml.rb
+++ spec/ramaze/view/haml.rb
@@ -1,38 +1,86 @@
+#          Copyright (c) 2009 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
 require 'spec/helper'
+
+Innate.options.app.root = __DIR__
+Innate.options.app.view = 'haml'
 
 class SpecHaml < Ramaze::Controller
   map '/'
   provide :html => :haml
 
   def index
-    '.hello'
+    '%h1 Haml Index'
   end
 
-  def with_helper
-    '%a{:href => r(:with_helper)} me'
+  def links
+    '
+    %ul
+      %li
+        %a{:href => r(:index)} Index page
+      %li
+        %a{:href => r(:internal)} Internal template
+      %li
+        %a{:href => r(:external)} External remplate
+    '.ui
   end
 
-  def with_instance_variable
-    @name = 'manveru'
-    '.hello= @name'
+  def sum(num1, num2)
+    @num1, @num2 = num1.to_i, num2.to_i
   end
 end
 
-describe 'Innate::View::Haml' do
+describe Ramaze::View::Haml do
   behaves_like :mock
 
   should 'render' do
-    get('/').body.
-      should == "<div class='hello'></div>\n"
+    got = get('/')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/html'
+    got.body.strip.should == "<h1>Haml Index</h1>"
   end
 
-  should 'render with helper methods' do
-    get('/with_helper').body.
-      should == "<a href='/with_helper'>me</a>\n"
+  should 'use other helper methods' do
+    got = get('/links')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/html'
+    got.body.strip.should ==
+"<ul>
+  <li>
+    <a href='/index'>Index page</a>
+  </li>
+  <li>
+    <a href='/internal'>Internal template</a>
+  </li>
+  <li>
+    <a href='/external'>External remplate</a>
+  </li>
+</ul>"
   end
 
-  should 'render with instance variable' do
-    get('/with_instance_variable').body.
-      should == "<div class='hello'>manveru</div>\n"
+  should 'render external template' do
+    got = get('/external')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/html'
+    got.body.strip.should ==
+"<html>
+  <head>
+    <title>Haml Test</title>
+  </head>
+  <body>
+    <h1>Haml Template</h1>
+  </body>
+</html>"
+  end
+
+  should 'render external template with instance variables' do
+    got = get('/sum/1/2')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/html'
+    got.body.strip.should ==
+"<div>
+  3
+</div>"
   end
 end
