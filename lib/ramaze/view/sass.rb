0b5363ba891d370ab6fdb20915a0461d16d2634b
Michael Fellinger <m.fellinger@gmail.com>
2009-01-28 22:20:40 +0900

    Improve Sass, respect sass_options, port spec

--- lib/ramaze/view/sass.rb
+++ lib/ramaze/view/sass.rb
@@ -5,7 +5,15 @@ module Ramaze
     module Sass
       def self.render(action, string = nil)
         string ||= action.view
-        sass = ::Sass::Engine.new(string, action.options)
+
+        Current.response['Content-Type'] = 'text/css'
+
+        options = action.options
+        if sass_options = action.instance.ancestral_trait[:sass_options]
+          options = options.merge(sass_options)
+        end
+
+        sass = ::Sass::Engine.new(string, options)
         sass.to_css
       end
     end
---
+++ spec/ramaze/view/sass.rb
@@ -0,0 +1,73 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
+require 'spec/helper'
+
+Innate.options.app.root = __DIR__
+Innate.options.app.view = 'sass'
+
+class SpecSass < Ramaze::Controller
+  map '/'
+  provide :css => :sass
+
+  def style
+%{
+body
+  :margin 1em
+
+  #content
+    :text-align center
+}
+  end
+end
+
+class SpecSassOptions < Ramaze::Controller
+  map '/options'
+  provide :css => :sass
+  trait :sass_options => { :style => :compact }
+
+  def test
+%{
+body
+  margin: 1em
+
+  #content
+    font:
+      family: monospace
+      size: 10pt
+}
+  end
+end
+
+describe Ramaze::View::Sass do
+  behaves_like :mock
+
+  should 'render inline' do
+    got = get('/style.css')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/css'
+    got.body.strip.should ==
+"body {
+  margin: 1em; }
+  body #content {
+    text-align: center; }"
+  end
+
+  should 'render from file' do
+    got = get('/file.css')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/css'
+    got.body.strip.should ==
+"body {
+  margin: 1em; }
+  body #content {
+    text-align: center; }"
+  end
+
+  should 'use sass options' do
+    got = get('/options/test.css')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/css'
+    got.body.should.not =~ /^ +/
+  end
+end
