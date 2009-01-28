c385741dd606f9f2e0c3ee92044e2ba62981ca28
Michael Fellinger <m.fellinger@gmail.com>
2009-01-28 21:19:23 +0900

    Helper::DEFAULT is removed

--- lib/ramaze/helper.rb
+++ lib/ramaze/helper.rb
@@ -10,9 +10,3 @@ module Ramaze
   Innate::HelpersHelper.add_path('')
   Innate::HelpersHelper.add_pool(Ramaze::Helper)
 end
-
-# Require default helpers as far as we can find them.
-# This is pure magic and way too DRY, anyone got a dispel handy?
-Dir[Innate::HelpersHelper.glob].each do |file|
-  require file if File.read(file) =~ /^\s*DEFAULT/
-end
