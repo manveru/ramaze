c6b7851a9adac5700d2d1307eea45ce2d521abfd
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-01-30 12:45:45 +0800

    Partial work to get erubis specs passing.  Not done yet.
    Signed-off-by: Michael Fellinger <m.fellinger@gmail.com>

---
+++ spec/ramaze/view/erubis.rb
@@ -0,0 +1,82 @@
+#          Copyright (c) 2006 Michael Fellinger m.fellinger@gmail.com
+# All files in this distribution are subject to the terms of the Ruby license.
+
+require 'spec/helper'
+
+Innate.options.app.root = __DIR__
+Innate.options.app.view = 'erubis'
+
+class SpecErubis < Ramaze::Controller
+  map '/'
+  provide :html => :erubis
+
+  def index
+    '<h1>Erubis Index</h1>'
+  end
+
+  def links
+    '<ul>
+      <li><a href="<%=r(:index)%>">Index page</a></li>
+      <li><a href="<%=r(:internal)%>">Internal template</a></li>
+      <li><a href="<%=r(:external)%>">External template</a></li>
+    </ul>'.ui
+  end
+
+  def sum(num1, num2)
+    @num1, @num2 = num1.to_i, num2.to_i
+  end
+
+  # def inline *args
+    # @args = args
+    # "<%= @args.inspect %>"
+  # end
+end
+
+describe "Ramaze::View::Erubis" do
+  behaves_like :mock
+
+  should 'render' do
+    got = get('/')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/html'
+    got.body.strip.should == "<h1>Erubis Index</h1>"
+  end
+
+  should 'use other helper methods' do
+    got = get('/links')
+    got.status.should == 200
+    got['Content-Type'].should == 'text/html'
+    got.body.strip.should ==
+'<ul>
+  <li><a href="/index">Index page</a></li>
+  <li><a href="/internal">Internal template</a></li>
+  <li><a href="/external">External template</a></li>
+</ul>'
+  end
+#
+  # should 'render external template' do
+    # got = get('/external')
+    # got.status.should == 200
+    # got['Content-Type'].should == 'text/html'
+    # got.body.strip.should ==
+# "<html>
+  # <head>
+    # <title>Haml Test</title>
+  # </head>
+  # <body>
+    # <h1>Haml Template</h1>
+  # </body>
+# </html>"
+  # end
+#
+  # should 'render external template with instance variables' do
+    # got = get('/sum/1/2')
+    # got.status.should == 200
+    # got['Content-Type'].should == 'text/html'
+    # got.body.strip.should ==
+# "<div>
+  # 3
+# </div>"
+  # end
+
+end
