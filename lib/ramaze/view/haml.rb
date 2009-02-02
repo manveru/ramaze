9fe58c70fcb090723714982e9ca56148e59db3c6
Michael Fellinger <m.fellinger@gmail.com>
2009-02-02 17:55:28 +0900

    Don't forget to copy instance variables in haml

--- lib/ramaze/view/haml.rb
+++ lib/ramaze/view/haml.rb
@@ -6,6 +6,7 @@ module Ramaze
       def self.render(action, string = nil)
         string ||= action.view
         action.options[:filename] ||= (action.view || '(haml)')
+        action.copy_variables
         haml = ::Haml::Engine.new(string.to_s, action.options)
         haml.to_html(action.instance, action.variables)
       end
