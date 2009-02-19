eb22b3787f4d3ce7e9e895192b9304baa1ea1eb6
Michael Fellinger <m.fellinger@gmail.com>
2009-02-19 14:11:07 +0900

    Add spec_precondition to deal with specs that require running memcached and the like

--- lib/ramaze/spec.rb
+++ lib/ramaze/spec.rb
@@ -2,3 +2,19 @@ require 'ramaze'
 require 'bacon'
 
 require 'innate/spec'
+
+def spec_requires(*libs)
+  spec_precondition 'require' do
+    libs.each{|lib| require(lib) }
+  end
+end
+
+def spec_precondition(name)
+  yield
+rescue LoadError => ex
+  puts "Spec require: %p failed: %p" % [name, ex.message]
+  exit 0
+rescue Exception => ex
+  puts "Spec precondition: %p failed: %p" % [name, ex.message]
+  exit 0
+end
