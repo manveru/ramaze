3ef2ce1852b9ba296a2c3e5853195846d9c4c0de
Michael Fellinger <m.fellinger@gmail.com>
2009-02-19 17:25:47 +0900

    Add spec for integration with Rack::Directory

--- spec/ramaze/dispatcher/directory.rb
+++ spec/ramaze/dispatcher/directory.rb
@@ -2,53 +2,69 @@ require 'spec/helper'
 
 spec_require 'hpricot'
 
-describe 'Dispatcher::Directory' do
+module Ramaze
+  # minimal middleware, no exception handling
+  middleware!(:innate){|m|
+    m.run m.directory(__DIR__('public'))
+  }
+end
+
+describe 'Directory listing' do
+  behaves_like :mock
+
   @hierarchy = %w[
-  /test/deep/hierarchy/one.txt
-  /test/deep/hierarchy/two.txt
-  /test/deep/three.txt
-  /test/deep/four.txt
-  /test/five.txt
-  /test/six.txt ]
+    /test/deep/hierarchy/one.txt
+    /test/deep/hierarchy/two.txt
+    /test/deep/three.txt
+    /test/deep/four.txt
+    /test/five.txt
+    /test/six.txt
+  ]
 
   @hierarchy.each do |path|
     FileUtils.mkdir_p(__DIR__(:public, File.dirname(path)))
     FileUtils.touch(__DIR__(:public, path))
   end
 
+  Ramaze.options.app.root = __DIR__
+  Ramaze.options.app.public = '/public'
+  Ramaze.map('/', lambda{|env| [404, {}, ['not found']]})
+
   def build_listing(path)
-    Ramaze::Dispatcher::Directory.build_listing(path)
+    get('path').body
   end
 
   def check(url, title, list)
-    body, status, header = build_listing(url)
-    status.should == 200
-    header['Content-Type'].should == 'text/html'
+    got = get(url)
+    got.status.should == 200
+    got['Content-Type'].should == 'text/html; charset=utf-8'
 
-    doc = Hpricot(body)
+    doc = Hpricot(got.body)
     doc.at(:title).inner_text.should == title
-    doc.search("//td[@class='n']").map{|td|
-      a = td.at(:a)
-      [ a[:href], a.inner_text ]
-    }.should == list
+
+    (doc/'td.name/a').map{|a| [a[:href], a.inner_text] }.should == list
   end
 
-  it 'should dry serve root directory' do
+  should 'dry serve root directory' do
    files = [
-     ["/../", "Parent Directory"], ["/test", "test/"],
-     ["/favicon.ico", "favicon.ico"], ["/file name.txt", "file name.txt"],
+     ["../", "Parent Directory"],
+     ["/favicon.ico", "favicon.ico"],
+     ["/file name.txt", "file name.txt"],
+     ["/test", "test"],
      ["/test_download.css", "test_download.css"]
    ]
 
-    check '/', 'Directory listing of /', files
+    check '/', '/', files
   end
 
-  it 'should serve hierarchies' do
+  should 'serve hierarchies' do
     files = [
-      ["/test/../", "Parent Directory"], ["/test/deep", "deep/"],
-      ["/test/five.txt", "five.txt"], ["/test/six.txt", "six.txt"]
+      ["../", "Parent Directory"],
+      ["/test/deep", "deep"],
+      ["/test/five.txt", "five.txt"],
+      ["/test/six.txt", "six.txt"]
     ]
-    check '/test', 'Directory listing of /test', files
+    check '/test', '/test', files
   end
 
   FileUtils.rm_rf(__DIR__('public/test'))
