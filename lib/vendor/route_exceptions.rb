f9c0054290ef4050b2e1a669d43d2a1e2628b378
Michael Fellinger <m.fellinger@gmail.com>
2009-02-24 22:29:39 +0900

    change response to returned in the route_exceptions to avoid confusion

--- lib/vendor/route_exceptions.rb
+++ lib/vendor/route_exceptions.rb
@@ -4,30 +4,30 @@ module Rack
 
     ROUTE_EXCEPTIONS_PATH_INFO = 'rack.route_exceptions.path_info'.freeze
     ROUTE_EXCEPTIONS_EXCEPTION = 'rack.route_exceptions.exception'.freeze
-    ROUTE_EXCEPTIONS_RESPONSE = 'rack.route_exceptions.response'.freeze
+    ROUTE_EXCEPTIONS_RETURNED  = 'rack.route_exceptions.returned'.freeze
 
     def initialize(app)
       @app = app
     end
 
     def call(env, try_again = true)
-      response = @app.call(env)
+      returned = @app.call(env)
     rescue Exception => exception
       raise(exception) unless try_again
 
       ROUTES.each do |klass, to|
         next unless klass === exception
-        return route(to, env, response, exception)
+        return route(to, env, returned, exception)
       end
 
       raise(exception)
     end
 
-    def route(to, env, response, exception)
+    def route(to, env, returned, exception)
       hash = {
         ROUTE_EXCEPTIONS_PATH_INFO => env['PATH_INFO'],
         ROUTE_EXCEPTIONS_EXCEPTION => exception,
-        ROUTE_EXCEPTIONS_RESPONSE => response
+        ROUTE_EXCEPTIONS_RETURNED => returned
       }
       env.merge!(hash)
 
@@ -40,5 +40,7 @@ module Rack
       ROUTES.delete_if{|k,v| k == exception }
       ROUTES << [exception, to]
     end
+
+    alias []= route
   end
 end
