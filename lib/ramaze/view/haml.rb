0b7847aac7605f764fe6a0fda6dd14e9818caaa0
Michael Fellinger <m.fellinger@gmail.com>
2009-02-28 17:11:42 +0900

    Fix bug where the name of the haml template was wrong for layouts

--- lib/ramaze/view/haml.rb
+++ lib/ramaze/view/haml.rb
@@ -3,9 +3,8 @@ require 'haml/engine'
 module Ramaze
   module View
     module Haml
-      def self.render(action, string = nil)
-        string ||= action.view
-        action.options[:filename] ||= (action.view || '(haml)')
+      def self.render(action, string)
+        action.options[:filename] = (action.view || '(haml)')
         action.copy_variables
         haml = ::Haml::Engine.new(string.to_s, action.options)
         haml.to_html(action.instance, action.variables)
