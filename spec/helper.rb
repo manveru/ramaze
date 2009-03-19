44cbafd8727b6dcfdbf709f035750eece4f371b6
Michael Fellinger <m.fellinger@gmail.com>
2009-03-19 21:52:30 +0900

    Improve spec/helper.rb so specs can be loaded from anywhere

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,4 +1,10 @@
-$0 = caller.last[/^[^:]+/] unless caller.empty?
+if caller_line = caller.grep(%r!spec/ramaze/!).first
+  caller_file = caller_line.split(':', 2).first
+  caller_root = File.dirname(caller_file)
+  $0 = caller_file
+end
 
 require File.expand_path(File.join(File.dirname(__FILE__), "../lib/ramaze"))
 require 'ramaze/spec'
+
+Ramaze.options.roots = [caller_root]
