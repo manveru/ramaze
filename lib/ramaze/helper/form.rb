070ccc94cb2bbea5a4ca6e8aeaed0a74305beb69
Michael Fellinger <m.fellinger@gmail.com>
2009-03-23 20:16:26 +0900

    Add the new Helper::Form, i carried it around for too long, move the old Helper::Form to Helper::SequelForm as it never supported another ORM

--- lib/ramaze/helper/form.rb
+++ lib/ramaze/helper/form.rb
@@ -1,284 +1,74 @@
+require 'ramaze/gestalt'
+
 module Ramaze
   module Helper
     module Form
-      # Pass it an object for your ORM and options for the <form> tag
-      # Usage:
-      #   form_for(User, :action => '/create')
-      #   form_for(Tag, :action => '/find', :method => 'post')
-      def form_for(object, options = {})
-        Ramaze::Form.pick(object, options)
+      def form_text(label, name, value = nil)
+        form_input(label, :type => :text, :name => name, :value => value)
       end
-    end
-  end
 
-  class Form
-    attr_accessor :object, :options
-
-    YEARS, MONTHS, DAYS, HOURS, MINUTES, SECONDS =
-      (1900..2100), (1..12), (1..31), (0..23), (0..59), (0..59)
-
-    DATE_GENERIC = [
-      [ :day,   DAYS ],
-      [ :month, MONTHS ],
-      [ :year,  YEARS ] ]
-
-    TIME_GENERIC = [
-      [ :day,   DAYS ],
-      [ :month, MONTHS ],
-      [ :year,  YEARS ],
-      [ :hour,  HOURS ],
-      [ :min,   MINUTES ],
-      [ :sec,   SECONDS ] ]
-
-    # TODO:
-    #   How _elegant_ ...
-    #   Tries to find the right module for extending the Form instance.
-    #   It's problematic since the boundaries of what an model instance or model
-    #   class looks like is very fuzzy, also a problem is that the ORM may not be
-    #   available/required.
-    #
-    #   Maybe we can abstract that a bit by going through an array of procs for
-    #   testing?
-    def self.pick(object, options = {})
-      if defined?(Sequel::Model)
-        if object.is_a?(Sequel::Model)
-          options[:layer] ||= Layer::Sequel
-          InstanceForm.new(object, options)
-        elsif object.ancestors.include?(Sequel::Model)
-          options[:layer] ||= Layer::Sequel
-          ClassForm.new(object, options)
-        end
-      else
-        raise "Unknown ORM for: %p" % object
+      def form_checkbox(label, name, checked = false)
+        hash = {:type => :checkbox, :name => name}
+        hash[:checked] = 'checked' if checked
+        form_input(label, hash)
       end
-    end
 
-    # Create new instance of Form plus the layer for the ORM
-    def initialize(object, options = {})
-      @object, @options = object, options
-      if layer = options.delete(:layer)
-        extend layer
+      def form_password(label, name)
+        form_input(label, :type => :password, :name => name)
       end
-    end
 
-    # Generate and return the final form
-    def to_s
-      out = "<form #{form_attributes}>"
-      out << "<fieldset>"
-      out << generate
-      out << "</fieldset>"
-      out << '<input type="submit" />'
-      out << '<input type="reset" />'
-      out << "</form>"
-    end
+      def form_textarea(label, name, value = nil)
+        form_build(:textarea, label, :name => name){ value }
+      end
 
-    # Decide on the strucuture of the tag based on the hash
-    def field_for(hash)
-      return if hash[:primary_key]
-      args = args_for(hash)
+      def form_file(label, name)
+        form_input(label, :type => :file, :name => name)
+      end
 
-      inner =
-        case type = hash[:type]
-        when :integer
-          field_integer(*args)
-        when :boolean
-          field_boolean(*args)
-        when :text
-          field_textarea(*args)
-        when :varchar
-          field_input(*args)
-        when :date
-          field_date(*args)
-        when :time
-          field_time(*args)
-        else
-          Log.warn "Unknown field: %p" % hash
-          field_input(*args)
-        end
+      def form_input(label, hash)
+        form_build(:input, label, hash)
+      end
 
-      "<label>#{args.first}: </label>\n#{inner}"
-    end
+      def form_build(tag_name, label, hash, &block)
+        form_id = "form-#{hash[:name]}"
+        opts = hash.merge(form_tabindex.merge(:id => form_id))
+        errors = form_errors
 
-    private
-
-    # inject to attributes for the <form>
-    def form_attributes
-      options.inject([]){|s,(k,v)| s << "#{k}='#{v}'" }.join(' ')
-    end
-
-    # Start tag with name and attributes
-    def start_tag(name, hash)
-      hash.inject("<#{name}"){|s,(k,v)| s << " #{k}='#{v}'" }
-    end
-
-    # Make a closed tag with name and attributes
-    def closed_tag(name, hash)
-      start_tag(name, hash) << ' />'
-    end
-
-    # Textarea with attributes from hash and the value from @object
-    def textarea(value, hash = {})
-      start_tag(:textarea, hash) << ">#{value}</textarea>"
-    end
-
-    # <input> with optional attributes from hash
-    def input(hash = {})
-      closed_tag(:input, hash)
-    end
-
-    # <input type="checkbox" with optional attributes from hash.
-    def checkbox(hash = {})
-      hash[:type] = :checkbox
-      input(hash)
-    end
-
-    # <option value="value"> with optional attributes from hash
-    def option(value, hash = {})
-      start_tag(:option, hash) << ">#{value}</option>"
-    end
-
-    # Yield method names and values for the Date instance
-    def field_date_generic
-      DATE_GENERIC.map{|(sel, range)|
-        yield(sel, range).join
-      }.join("\n")
-    end
-
-    # Yield method names and values for the Time/DateTime instance
-    def field_time_generic
-      TIME_GENERIC.map{|(sel, range)|
-        yield(sel, range).join
-      }.join("\n")
-    end
-
-    # Here go all the layers that are extended for specific ORMs
-    module Layer
-      # Layer for Sequel, only generate needs to be implemented, may change in
-      # future if we abstract more for different ORMs
-      module Sequel
-        # A bit nasty, get the @columns of the object and generate its
-        # field_for
-        def generate
-          columns = object_class.schema.instance_variable_get('@columns')
-          columns.map{|hash| field_for(hash) }.flatten.join("<br />\n")
+        Ramaze::Gestalt.build do
+          tr do
+            td do
+              label(:for => form_id){ "#{label}:" }
+              if error = errors[opts[:name]]
+                div(:class => "error"){ error }
+              end
+            end
+            td do
+              tag(tag_name, opts, &block)
+            end
+          end
         end
       end
-    end
-  end
 
-  # Form for the model class itself, very similar to an empty instance.
-  class ClassForm < Form
-    # <input name="name" />
-    def field_input(name)
-      input :name => name
-    end
+      def form_tabindex
+        @tabindex ||= 0
+        @tabindex += 1
 
-    # <textarea name="name"></textarea>
-    def field_textarea(name)
-      textarea '', :name => name
-    end
-
-    # <input name="name" />
-    def field_integer(name)
-      input :name => name
-    end
-
-    # <input type="checkbox" name="name" />
-    def field_boolean(name)
-      checkbox :name => name
-    end
-
-    # <select> with lots of <option>s
-    def field_date(name)
-      field_date_generic{|sel, range|
-        [ "<select name='#{name}[#{sel}]'>",
-          range.map{|d| option(d, :value => d) },
-          "</select>" ]
-      }
-    end
-
-    # <select> with lots of <option>s
-    def field_time(name)
-      field_time_generic{|sel, range|
-        [ "<select name='#{name}[#{sel}]'>",
-          range.map{|d| option(d, :value => d) },
-          "</select>" ]
-      }
-    end
-
-    # picks the :name
-    def args_for(hash)
-      [ hash[:name] ]
-    end
-
-    # Should be that way, at least for Sequel
-    def object_class
-      @object
-    end
-  end
-
-  # Form for instances of the model class
-  class InstanceForm < Form
-    # returns <input type='text' name='name' value='value' />
-    def field_input(name, value)
-      "<input type='text' name='#{name}' value='#{value}'/>"
-    end
-
-    # returns <textarea name='name'>#{value}</textarea>
-
-    def field_textarea(name, value)
-      "<textarea name='#{name}'>#{value}</textarea>"
-    end
-
-    # returns <input type="text" name="name" value="value" />
-
-    def field_integer(name, value)
-      field_input(name, value)
-    end
-
-    # <input type="checkbox" ...
-    def field_boolean(name, value)
-      if value
-        checkbox :name => name, :value => value, :checked => :checked
-      else
-        checkbox :name => name, :value => value
+        {:tabindex => @tabindex}
       end
-    end
 
-    def field_date(name, value)
-      field_date_generic do |sel, range|
-        [ "<select name='#{name}[#{sel}]'>",
-          option_range_selected(range, value.send(sel)),
-          "</select>" ]
+      def form_error(name, message)
+        form_errors[name.to_s] = message.to_s
       end
-    end
 
-    def field_time(name, value)
-      field_time_generic do |sel, range|
-        [ "<select name='#{name}[#{sel}]'>",
-          option_range_selected(range, value.send(sel)),
-          "</select>" ]
+      def form_errors
+        @form_errors ||= {}
       end
-    end
 
-    def option_range_selected(range, value)
-      range.map do |r|
-        if r == value
-          option(r, :value => r, :selected => :selected)
-        else
-          option(r, :value => r)
+      def form_errors_from_model(obj)
+        obj.errors.each do |key, value|
+          form_error(key, value.first % key)
         end
       end
-    end
-
-    def args_for(hash)
-      name = hash[:name]
-      [ name, @object.send(name) ]
-    end
-
-    # Class for @object, atm Sequel specific?
-    def object_class
-      @object.class
     end
   end
 end
--- spec/ramaze/helper/form.rb
+++ spec/ramaze/helper/form.rb
@@ -1,90 +1,170 @@
 require 'spec/helper'
 
-require 'hpricot'
-require 'sequel'
+spec_requires 'hpricot'
 
-DB = Sequel.sqlite
-
-class User < Sequel::Model(:user)
-  set_schema do
-    primary_key :id
-
-    boolean :online
-    varchar :name
-    integer :level
-    text :description
-    date :birthday
-    time :created
-  end
-end
-
-User.create_table
-
-class FormController < Ramaze::Controller
+class SpecHelperForm < Ramaze::Controller
   map '/'
   helper :form
 
-  def new
-    form_for(User).to_s
+  def text
+    form_text('Username', :username, request[:username])
   end
 
-  def new_with_options
-    form_for(User, :method => :post, :action => r(:create)).to_s
+  def checkbox
+    form_checkbox('Administrator', :admin, request[:admin])
   end
 
-  def edit(id)
-    form_for(User[id])
+  def password
+    form_password('Password', :password)
   end
 
-  def edit_with_options(id)
-    form_for(User[id], :method => :post, :action => r(:create)).to_s
+  def textarea
+    form_textarea('Text', :text, request[:text])
+  end
+
+  def file
+    form_file('File', :file)
   end
 end
 
 describe Ramaze::Helper::Form do
   behaves_like :mock
 
-  def hget(uri)
-    got = get(uri)
-    got.status.should == 200
-    Hpricot(got.body)
+  it 'provides empty text input' do
+    got = get('/text')
+
+    doc = Hpricot(got.body)
+    label, input = doc.at(:label), doc.at(:input)
+
+    label[:for].should == 'form-username'
+    label.inner_text.should == 'Username:'
+
+    input[:id].should == 'form-username'
+    input[:type].should == 'text'
+    input[:name].should == 'username'
+    input[:value].should == ''
+    input[:checked].should.be.nil
+    input[:tabindex].should == '1'
   end
 
-  it 'provides forms for model class' do
-    form = hget('/new').at(:form)
-    (form/:input).size.should == 5
-    (form/:input).map{|i| i[:name] }.compact.sort.should == %w[level name online]
-    form.at(:textarea)[:name].should == 'description'
+  it 'provides filled text input' do
+    got = get('/text?username=manveru')
+
+    doc = Hpricot(got.body)
+    label, input = doc.at(:label), doc.at(:input)
+
+    label[:for].should == 'form-username'
+    label.inner_text.should == 'Username:'
+
+    input[:id].should == 'form-username'
+    input[:type].should == 'text'
+    input[:name].should == 'username'
+    input[:value].should == 'manveru'
+    input[:checked].should.be.nil
+    input[:tabindex].should == '1'
   end
 
-  it 'provides form for model class with options' do
-    form = hget('/new_with_options').at(:form)
-    form.raw_attributes.should == {'action' => '/create', 'method' => 'post'}
+  it 'provides unchecked checkbox input' do
+    got = get('/checkbox')
+
+    doc = Hpricot(got.body)
+    label, input = doc.at(:label), doc.at(:input)
+
+    label[:for].should == 'form-admin'
+    label.inner_text.should == 'Administrator:'
+
+    input[:id].should == 'form-admin'
+    input[:type].should == 'checkbox'
+    input[:name].should == 'admin'
+    input[:value].should.be.nil
+    input[:checked].should.be.nil
+    input[:tabindex].should == '1'
   end
 
-  data = {
-    :name        => 'manveru',
-    :description => 'Ramaze dev',
-    :online      => true,
-    :level       => 2,
-    :birthday    => Time.now,
-    :created     => Time.now,
-  }
-  User.create(data)
+  it 'provides checked checkbox input' do
+    got = get('/checkbox?admin=true')
 
-  it 'provides form for instance of model' do
-    form = hget('/edit/1').at(:form)
-    form.at('input[@name=name]').raw_attributes.should ==
-      { 'name' => 'name', 'type' => 'text', 'value' => data[:name] }
-    form.at('input[@name=online]').raw_attributes.should ==
-      { "name" => "online", "type" => "checkbox", "checked" => "checked",
-        "value" => data[:online].to_s }
-    form.at('input[@name=level]').raw_attributes.should ==
-      { "name" => "level", "type" => "text", "value" => data[:level].to_s }
+    doc = Hpricot(got.body)
+    label, input = doc.at(:label), doc.at(:input)
+
+    label[:for].should == 'form-admin'
+    label.inner_text.should == 'Administrator:'
+
+    input[:id].should == 'form-admin'
+    input[:type].should == 'checkbox'
+    input[:name].should == 'admin'
+    input[:value].should.be.nil
+    input[:checked].should == 'checked'
+    input[:tabindex].should == '1'
   end
 
-  it 'provides form with options for instance of model' do
-    hget('/edit_with_options/1').at(:form).raw_attributes.should ==
-      {"action" => "/create", "method" => "post"}
+  it 'provides password input' do
+    got = get('/password')
+
+    doc = Hpricot(got.body)
+    label, input = doc.at(:label), doc.at(:input)
+
+    label[:for].should == 'form-password'
+    label.inner_text.should == 'Password:'
+
+    input[:id].should == 'form-password'
+    input[:type].should == 'password'
+    input[:name].should == 'password'
+    input[:value].should.be.nil
+    input[:checked].should.be.nil
+    input[:tabindex].should == '1'
+  end
+
+  it 'provides empty textarea' do
+    got = get('/textarea')
+
+    doc = Hpricot(got.body)
+    label, textarea = doc.at(:label), doc.at(:textarea)
+
+    label[:for].should == 'form-text'
+    label.inner_text.should == 'Text:'
+
+    textarea[:id].should == 'form-text'
+    textarea[:type].should.be.nil
+    textarea[:name].should == 'text'
+    textarea[:value].should.be.nil
+    textarea[:checked].should.be.nil
+    textarea[:tabindex].should == '1'
+    textarea.inner_text.should == ''
+  end
+
+  it 'provides filled textarea' do
+    got = get('/textarea?text=foobar')
+
+    doc = Hpricot(got.body)
+    label, textarea = doc.at(:label), doc.at(:textarea)
+
+    label[:for].should == 'form-text'
+    label.inner_text.should == 'Text:'
+
+    textarea[:id].should == 'form-text'
+    textarea[:type].should.be.nil
+    textarea[:name].should == 'text'
+    textarea[:value].should.be.nil
+    textarea[:checked].should.be.nil
+    textarea[:tabindex].should == '1'
+    textarea.inner_text.should == 'foobar'
+  end
+
+  it 'provides file upload input' do
+    got = get('/file')
+
+    doc = Hpricot(got.body)
+    label, input = doc.at(:label), doc.at(:input)
+
+    label[:for].should == 'form-file'
+    label.inner_text.should == 'File:'
+
+    input[:id].should == 'form-file'
+    input[:type].should == 'file'
+    input[:name].should == 'file'
+    input[:value].should.be.nil
+    input[:checked].should.be.nil
+    input[:tabindex].should == '1'
   end
 end
