3ba481bb02d182c09e43f4c4d86535a71226d3b4
Michael Fellinger <m.fellinger@gmail.com>
2009-03-25 23:34:08 +0900

    Improve intro in example/simple/hello.rb

--- examples/basic/hello.rb
+++ examples/basic/hello.rb
@@ -1,10 +1,9 @@
 require 'rubygems'
 require 'ramaze'
 
-# you can access it now with http://localhost:7000/
-# This should output
-# Hello, World!
-# in your browser
+# Start this example with `ruby hello.rb`.
+# After startup you will be able to access it at http://localhost:7000/
+# This should output "Hello, World!" in your browser.
 
 class MainController < Ramaze::Controller
   def index
