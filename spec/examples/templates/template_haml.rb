414f6f2188a7a66c15b83f50ee6d7fa03dbcdf4d
Clive Crous <clive@crous.co.za>
2009-03-25 19:50:50 +0200

    Remove excess whitespace from Haml

--- spec/examples/templates/template_haml.rb
+++ spec/examples/templates/template_haml.rb
@@ -4,7 +4,6 @@ require 'ramaze/spec/helper/template_examples'
 spec_require 'haml'
 require 'examples/templates/template_haml'
 
-
 describe 'Template Haml' do |describe|
   ::Ramaze::Spec::Examples::Templates.tests( describe, 'Haml' )
 end
