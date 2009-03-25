4e35cdd4417d8949d949272bb29048ab1a49d7ad
Clive Crous <clive@crous.co.za>
2009-03-25 22:00:26 +0200

    markaby and xslt are not yet implemented in ramaze/view

--- spec/examples/templates/template_markaby.rb
+++ spec/examples/templates/template_markaby.rb
@@ -1,9 +1,10 @@
 require 'spec/helper'
 require 'ramaze/spec/helper/template_examples'
 
-spec_require 'markaby'
-require 'examples/templates/template_markaby'
+#spec_require 'markaby'
+#require 'examples/templates/template_markaby'
 
 describe 'Template Markaby' do |describe|
-  ::Ramaze::Spec::Examples::Templates.tests( describe, 'Markaby' )
+  #::Ramaze::Spec::Examples::Templates.tests( describe, 'Markaby' )
+  it "isn't yet implemented"
 end
