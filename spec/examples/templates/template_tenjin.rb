fac24bfa56ee32088945f96e19d91fac3e345f66
Clive Crous <clive@crous.co.za>
2009-03-25 20:46:56 +0200

    updated spec and example for templates/tenjin

--- spec/examples/templates/template_tenjin.rb
+++ spec/examples/templates/template_tenjin.rb
@@ -1,28 +1,9 @@
 require 'spec/helper'
+require 'ramaze/spec/helper/template_examples'
 
 spec_require 'tenjin'
 require 'examples/templates/template_tenjin'
 
-describe 'Template Tenjin' do
-  behaves_like 'http'
-  ramaze
-
-  it '/' do
-    html = get('/').body.strip
-    html.should =~ %r(<a href=\"/\">Home</a>)
-    html.should =~ %r(<a href=\"/internal\">internal</a>)
-    html.should =~ %r(<a href=\"/external\">external</a>)
-  end
-
-  %w[/internal /external].each do |url|
-    it url do
-      name = url.gsub(/^\//, '')
-      response = get(url)
-      response.status.should == 200
-      html = response.body
-      html.should.not == nil
-      html.should =~ %r(<title>Template::Tenjin #{name}</title>)
-      html.should =~ %r(<h1>The #{name} Template for Tenjin</h1>)
-    end
-  end
+describe 'Template Tenjin' do |describe|
+  ::Ramaze::Spec::Examples::Templates.tests( describe, 'Tenjin' )
 end
