32efcd7b661557fe95e2352ec4ea37f389b748eb
Clive Crous <clive@crous.co.za>
2009-03-25 19:39:01 +0200

    Common tests for most template example specs

---
+++ lib/ramaze/spec/helper/template_examples.rb
@@ -0,0 +1,30 @@
+module Ramaze
+  module Spec
+    module Examples
+      module Templates
+        def self.tests( describe, spec_engine )
+
+            describe.behaves_like :mock
+
+            describe.it '/' do
+              get('/').body.strip.should ==
+                "<a href=\"/\">Home</a> | <a href=\"/internal\">internal</a> | <a href=\"/external\">external</a>"
+            end
+
+            %w[/internal /external].each do |url|
+              describe.it url do
+                html = get(url).body
+                html.should.not == nil
+                html.should =~ %r{<title>Template::#{spec_engine} (internal|external)</title>}
+                html.should =~ %r{<h1>The (internal|external) Template for #{spec_engine}</h1>}
+              end
+
+          end
+
+        end
+
+      end
+    end
+  end
+end
+Ramaze.options.views = '../../../examples/templates/view'
