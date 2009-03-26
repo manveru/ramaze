972559baf6b5b43e9873faeef90c6fe258bd2647
Michael Fellinger <m.fellinger@gmail.com>
2009-03-26 23:41:14 +0900

    Remove Amrita2 templating engine, it's defunct with current Hpricot and doesn't look like anybody maintains it

--- spec/examples/templates/template_amrita2.rb
+++
@@ -1,9 +0,0 @@
-require 'spec/helper'
-require 'ramaze/spec/helper/template_examples'
-
-spec_require 'amrita2'
-require 'examples/templates/template_amrita2'
-
-describe 'Template Amrita2' do |describe|
-  ::Ramaze::Spec::Examples::Templates.tests( describe, 'Amrita2' )
-end
