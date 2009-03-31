851e1bd5cf7674e8bd57e8526d33f7b241b6597a
Michael Fellinger <m.fellinger@gmail.com>
2009-04-01 01:21:56 +0900

    Version 2009.04.01

--- lib/ramaze/version.rb
+++ lib/ramaze/version.rb
@@ -1,6 +1,3 @@
-#          Copyright (c) 2008 Michael Fellinger m.fellinger@gmail.com
-# All files in this distribution are subject to the terms of the Ruby license.
-
 module Ramaze
-  VERSION = "2008.12"
+  VERSION = "2009.04.01"
 end
