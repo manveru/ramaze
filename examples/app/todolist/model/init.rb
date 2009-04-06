30fec68fc2838d973985fb4f0f6e048cb21d54bd
Michael Fellinger <m.fellinger@gmail.com>
2009-04-06 19:41:41 +0900

    Add information about, and usage of: Sequel::Model.plugin(:schema)

--- examples/app/todolist/model/init.rb
+++ examples/app/todolist/model/init.rb
@@ -1,4 +1,7 @@
 module TodoList
+  # yeah, we're lazy
+  Sequel::Model.plugin(:schema)
+
   # Uncomment the line for the DB you want to use.
 
   # Sqlite In memory, fastest, but cannot persist over restarts.
