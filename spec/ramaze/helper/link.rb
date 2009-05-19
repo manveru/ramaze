eba4734ce7a991c292371049d186384738364bcd
Antti Tuomi <antti.tuomi@tkk.fi>
2009-05-19 19:45:14 +0300

    spec to check if the link helper works properly with prefix

--- spec/ramaze/helper/link.rb
+++ spec/ramaze/helper/link.rb
@@ -12,6 +12,18 @@ class SpecHelperLinkTwo < Ramaze::Controller
   map '/two'
 end
 
+class SpecHelperApp < Ramaze::Controller
+  map '/', :other
+end
+
+class SpecHelperAppTwo < Ramaze::Controller
+  map '/two', :other
+end
+
+Ramaze::App[:other].location = '/other'
+
+
+
 describe Ramaze::Helper::Link do
   extend Ramaze::Helper::Link
 
@@ -45,5 +57,27 @@ describe Ramaze::Helper::Link do
       '<a href="/prefix/path/file/dir/listing/is">is</a>',
       '<a href="/prefix/path/file/dir/listing/is/cool">cool</a>'
     ].join('/')
+  end
+
+  it "builds routes and links to other applications" do
+    SpecHelperApp.r(:foo).to_s.should == '/other/foo'
+    SpecHelperApp.a(:foo, :bar).should == '<a href="/other/bar">foo</a>'
+    SpecHelperAppTwo.r(:foo).to_s.should == '/other/two/foo'
+    SpecHelperAppTwo.a(:foo, :bar).should == '<a href="/other/two/bar">foo</a>'
+  end
+  it "builds routes when Ramaze.options.prefix is present" do
+
+    SpecHelperLink.r(:foo).to_s.should == '/prfx/foo'
+    SpecHelperLinkTwo.r(:foo).to_s.should == '/prfx/two/foo'
+    SpecHelperApp.r(:foo).to_s.should == '/prfx/other/foo'
+    SpecHelperAppTwo.r(:foo).to_s.should == '/prfx/other/two/foo'
+
+  end
+  it "builds links when Ramaze.options.prefix is present" do
+    Ramaze.options.prefix = '/prfx'
+    SpecHelperLink.a(:foo, :bar).should == '<a href="/prfx/bar">foo</a>'
+    SpecHelperLinkTwo.a(:foo, :bar).should == '<a href="/prfx/two/bar">foo</a>'
+    SpecHelperApp.a(:foo, :bar).should == '<a href="/prfx/other/bar">foo</a>'
+    SpecHelperAppTwo.a(:foo, :bar).should == '<a href="/prfx/other/two/bar">foo</a>'
   end
 end
