2529786d618cc6d2864ed36542d268a6b6948fe6
Michael Fellinger <m.fellinger@gmail.com>
2009-05-28 09:15:19 +0900

    Put layout helper into controller singleton only, fix specs, improve style

--- lib/ramaze/helper/layout.rb
+++ lib/ramaze/helper/layout.rb
@@ -1,49 +1,46 @@
 module Ramaze
   module Helper
 
-    # Provides wrapper methods for a higher-level approach than the core layout method.
-
+    # Provides wrapper methods for a higher-level approach than the core layout
+    # method.
     module Layout
-
       def self.included(into)
-        into.extend self
-        into.extend Innate::Traited
+        into.extend SingletonMethods
       end
 
-      def set_layout(hash_or_the_layout)
-        if hash_or_the_layout.respond_to?(:to_hash)
-          f = hash_or_the_layout.first
-          trait :the_layout => f[0]
-          trait :layout_whitelist => f[1].map { |action| action.to_s }
-        else
-          trait :the_layout => hash_or_the_layout
+      module SingletonMethods
+        def set_layout(hash_or_the_layout)
+          if hash_or_the_layout.respond_to?(:to_hash)
+            f = hash_or_the_layout.first
+            the_layout = f[0]
+            whitelist = f[1].map{|action| action.to_s }
+          else
+            the_layout = hash_or_the_layout
+          end
+
+          layout do |path, wish|
+            if whitelist.nil? || whitelist.include?(path.to_s)
+              the_layout
+            end
+          end
         end
 
-        layout {|path,wish|
-          whitelist = trait[ :layout_whitelist ]
-          if whitelist.nil? || whitelist.include?(path.to_s)
-            trait[ :the_layout ]
+        def set_layout_except(hash_or_the_layout)
+          if hash_or_the_layout.respond_to?(:to_hash)
+            f = hash_or_the_layout.to_hash.first
+            the_layout = f[0]
+            blacklist = f[1].map{|action| action.to_s }
+          else
+            the_layout = hash_or_the_layout
           end
-        }
-      end
 
-      def set_layout_except(hash_or_the_layout)
-        if hash_or_the_layout.respond_to?(:to_hash)
-          f = hash_or_the_layout.first
-          trait :the_layout => f[0]
-          trait :layout_blacklist => f[1].map { |action| action.to_s }
-        else
-          trait :the_layout => hash_or_the_layout
+          layout do |path, wish|
+            if blacklist.nil? || !blacklist.include?(path.to_s)
+              the_layout
+            end
+          end
         end
-
-        layout {|path,wish|
-          blacklist = trait[ :layout_blacklist ]
-          if blacklist.nil? || ! blacklist.include?(path.to_s)
-            trait[ :the_layout ]
-          end
-        }
       end
-
     end
   end
 end
