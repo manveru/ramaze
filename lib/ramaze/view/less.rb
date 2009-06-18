0a4cbaf71f36507a3513c4d8b808df57ac53bf91
Michael Fellinger <m.fellinger@gmail.com>
2009-06-18 13:14:26 +0900

    Remove cruft from less

--- lib/ramaze/view/less.rb
+++ lib/ramaze/view/less.rb
@@ -4,16 +4,8 @@ module Ramaze
   module View
     module Less
       def self.call(action, string)
-        options = action.options
-
-        if sass_options = action.instance.ancestral_trait[:sass_options]
-          options = options.merge(sass_options)
-        end
-
         less = View.compile(string){|s| ::Less::Engine.new(s) }
-        css = less.to_css
-
-        return css, 'text/css'
+        return less.to_css, 'text/css'
       end
     end
   end
