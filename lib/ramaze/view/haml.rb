8d79efe6a9daab8d91a7b556bea064c952c0f1cc
Michael Fellinger <m.fellinger@gmail.com>
2009-08-05 21:58:10 +0200

    Provide support for :haml_options trait again

--- lib/ramaze/view/haml.rb
+++ lib/ramaze/view/haml.rb
@@ -5,8 +5,14 @@ module Ramaze
   module View
     module Haml
       def self.call(action, string)
+        options = action.options
+
+        if haml_options = action.instance.ancestral_trait[:haml_options]
+          options = options.merge(haml_options)
+        end
+
         action.options[:filename] = (action.view || '(haml)')
-        haml = View.compile(string){|s| ::Haml::Engine.new(s, action.options) }
+        haml = View.compile(string){|s| ::Haml::Engine.new(s, options) }
         html = haml.to_html(action.instance, action.variables)
 
         return html, 'text/html'
