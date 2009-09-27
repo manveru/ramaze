72684cb2ea3533ea465e90189af4e147b6a55cc8
Mario Gutierrez <mgutz@mgutz-mbp.local>
2009-09-27 16:39:33 -0700

    adding erector engine convenience methods

--- lib/ramaze/view/erector.rb
+++ lib/ramaze/view/erector.rb
@@ -3,14 +3,18 @@ require 'erector'
 module Ramaze
   module View
     module Erector
-      extend ::Erector::Mixin
-
       def self.call(action, string)
         return string, 'text/html' unless action.view
 
         controller = action.instance
 
         html = ::Erector.inline do
+          alias :raw! :rawtext
+
+          def inspect(elem)
+            text elem.inspect
+          end
+
           # copy instance variables into Erector context
           controller.instance_variables.each do |v|
             instance_variable_set(v, controller.instance_variable_get(v))
