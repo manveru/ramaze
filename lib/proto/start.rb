67eb0aa183286668c5c0d1cd72f3ebac383b3be0
Michael Fellinger <m.fellinger@gmail.com>
2009-11-17 02:14:18 +0900

    Nicer require

--- lib/proto/start.rb
+++ lib/proto/start.rb
@@ -3,6 +3,6 @@
 # All application related things should go into `app.rb`, this file is simply
 # for options related to running the application locally.
 
-require File.expand_path('app', File.dirname(__FILE__))
+require File.expand_path('../app', __FILE__)
 
 Ramaze.start(:adapter => :webrick, :port => 7000, :file => __FILE__)
